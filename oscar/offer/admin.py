77d2f9016f2bb7d66cbae8360e9c31c3034b0a50
David Winterbottom <david.winterbottom@gmail.com>
2010-12-10 21:43:07 +0000

    Admin defined for offers package

---
+++ oscar/offer/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from oscar.offer.models import *
+
+
+admin.site.register(ConditionalOffer)
+admin.site.register(Condition)
+admin.site.register(Benefit)
+admin.site.register(Range)
+admin.site.register(Voucher)
