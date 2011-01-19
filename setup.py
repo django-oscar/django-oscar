67f6779adafbdd8718298b58b21c8bb8fd05328b
David Winterbottom <david.winterbottom@gmail.com>
2011-01-19 09:18:04 +0000

    Restructured project folders to make oscar sit as a standalone app

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='django-oscar',
+        version='0.1.0',
+        url='https://github.com/codeinthehole/django-oscar',
+        description="Flexible ecommerce applications for Django",
+        author="David Winterbottom",
+        author_email="david.winterbottom@tangentlabs.co.uk",
+        package_dir={'': '.'},
+        classifiers = [
+            "Intended Audience :: Developers",
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: eCommerce',
+            ]
+        )
