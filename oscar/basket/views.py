2c498234b38bdb334236c3a4cb40f8c8fd6b7165
David Winterbottom <david.winterbottom@gmail.com>
2011-01-24 13:26:35 +0000

    Enabled signed-in users to add to their baskets

--- oscar/basket/views.py
+++ oscar/basket/views.py
@@ -18,38 +18,48 @@ COOKIE_KEY_HASH = 'basket_hash'
 COOKIE_LIFETIME = 7*24*60*60
 
 def _get_user_basket(request):
+    """
+    Returns the basket for the current user
+    """
     b = None
     if request.user.is_authenticated():
-        # @todo user baskets
-        pass
+        b = basket_models.Basket.open.get(owner=request.user)
     else:
-        # If user is anonymous, their basket ID (if they have one) will be
-        # stored in a cookie together with a hash which verifies it and prevents
-        # it from being spoofed.
-        if request.COOKIES.has_key(COOKIE_KEY_ID) and request.COOKIES.has_key(COOKIE_KEY_HASH):
-            basket_id = request.COOKIES[COOKIE_KEY_ID]
-            basket_hash = request.COOKIES[COOKIE_KEY_HASH]
-            if basket_hash == _get_basket_hash(basket_id):
-                try:
-                    b = basket_models.Basket.open.get(pk=basket_id)
-                except basket_models.Basket.DoesNotExist, e:
-                    b = None
+        b = _get_anon_user_basket(request)
     return b    
+
+def _get_anon_user_basket(request):
+    """
+    Looks for a basket that has its ID stored in the user's cookies
+    """
+    b = None
+    # If user is anonymous, their basket ID (if they have one) will be
+    # stored in a cookie together with a hash which verifies it and prevents
+    # it from being spoofed.
+    if request.COOKIES.has_key(COOKIE_KEY_ID) and request.COOKIES.has_key(COOKIE_KEY_HASH):
+        basket_id = request.COOKIES[COOKIE_KEY_ID]
+        basket_hash = request.COOKIES[COOKIE_KEY_HASH]
+        if basket_hash == _get_basket_hash(basket_id):
+            try:
+                b = basket_models.Basket.open.get(pk=basket_id)
+            except basket_models.Basket.DoesNotExist, e:
+                b = None
+    return b      
 
 def _get_or_create_basket(request, response):
     """
     Loads or creates a basket object
-    
-    If the user is anonymous, the id of the basket is stored in
-    a cookie.
     """
-    b = _get_user_basket(request)
-    if not b and not request.user.is_authenticated():
-        # No valid basket found so we create a new one and store the id
-        # and hash in a cookie
-        b = basket_models.Basket.objects.create()
-        response.set_cookie(COOKIE_KEY_ID, b.pk, max_age=COOKIE_LIFETIME)
-        response.set_cookie(COOKIE_KEY_HASH, _get_basket_hash(b.pk), max_age=COOKIE_LIFETIME)
+    if request.user.is_authenticated():
+        b,_ = basket_models.Basket.open.get_or_create(owner=request.user)
+    else:
+        b = _get_anon_user_basket(request)
+        if not b:
+            # No valid basket found so we create a new one and store the id
+            # and hash in a cookie.
+            b = basket_models.Basket.objects.create()
+            response.set_cookie(COOKIE_KEY_ID, b.pk, max_age=COOKIE_LIFETIME)
+            response.set_cookie(COOKIE_KEY_HASH, _get_basket_hash(b.pk), max_age=COOKIE_LIFETIME)
     return b    
 
 def _get_basket_hash(id):
@@ -58,6 +68,7 @@ def _get_basket_hash(id):
     variable defined in settings.py as a salt.
     """
     return str(zlib.crc32(str(id)+settings.SECRET_KEY))
+
 
 def index(request):
     """ 
