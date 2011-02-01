587cf7266406af36ec59e4741b368ff56686e37c
David Winterbottom <david.winterbottom@gmail.com>
2011-02-01 21:37:57 +0000

    Added a nifty decorator function to checkout.
    The decorator checks whether the steps are being completed in order by
    storing the URL-names in the session.

--- oscar/checkout/views.py
+++ oscar/checkout/views.py
@@ -4,6 +4,8 @@ from django.template import RequestContext
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.forms import ModelForm
+from django.contrib import messages
+from django.core.urlresolvers import resolve
 
 from oscar.services import import_module
 
@@ -31,6 +33,67 @@ class OrderTotalCalculator(object):
     def order_total_excl_tax(self, basket):
         return basket.total_excl_tax
 
+class ProgressChecker(object):
+    urls_for_steps = ['oscar-checkout-delivery-address',
+                      'oscar-checkout-delivery-method',
+                      'oscar-checkout-payment',
+                      'oscar-checkout-preview']
+    
+    def are_previous_steps_complete(self, request, url_name):
+        """
+        Checks whether the previous checkout steps have been completed.
+        
+        This uses the URL-name and the class-level list of required
+        steps.
+        """
+        complete_steps = request.session.get('checkout_complete_steps', [])
+        try:
+            current_step_index = self.urls_for_steps.index(url_name)
+            last_completed_step_index = len(complete_steps) - 1
+            return current_step_index <= last_completed_step_index + 1 
+        except ValueError:
+            # Can't find current step index - must be manipulation
+            return False
+        except IndexError:
+            # No complete steps - only allowed to be on first page
+            return current_step_index == 0
+        
+    def step_complete(self, request):
+        """
+        Record a checkout step as complete.
+        """
+        match = resolve(request.path)
+        url_name = match.url_name
+        complete_steps = request.session.get('checkout_complete_steps', [])
+        if not url_name in complete_steps:
+            # Only add name if this is the first time the step 
+            # has been completed. 
+            complete_steps.append(url_name)
+            request.session['checkout_complete_steps'] = complete_steps
+
+def prev_steps_must_be_complete(view_fn):
+    """
+    Decorator fn for checking that previous steps of the checkout
+    are complete.
+    
+    The URL-names are stored in the session
+    """
+    def _view_wrapper(request, *args, **kwargs):
+        checker = ProgressChecker()
+        # Extract the URL name from the path
+        match = resolve(request.path)
+        if not checker.are_previous_steps_complete(request, match.url_name):
+            messages.error(request, "You must complete the previous steps before this one")
+            return HttpResponseRedirect(reverse('oscar-checkout-index'))
+        return view_fn(request,*args, **kwargs)
+    return _view_wrapper
+
+def mark_step_as_complete(request):
+    """ 
+    Convenience function for marking a checkout page
+    as complete.
+    """
+    ProgressChecker().step_complete(request)
 
 def index(request):
     """
@@ -50,6 +113,7 @@ def delivery_address(request):
                 clean_data['user_id'] = request.user.id 
             request.session['delivery_address'] = clean_data
             
+            mark_step_as_complete(request)
             return HttpResponseRedirect(reverse('oscar-checkout-delivery-method'))
     else:
         if request.session.get('delivery_address', False):
@@ -58,23 +122,25 @@ def delivery_address(request):
             form = checkout_forms.DeliveryAddressForm()
     return render(request, 'checkout/delivery_address.html', locals())
     
-    
+@prev_steps_must_be_complete    
 def delivery_method(request):
     """
     Delivery methods are domain-specific and so need implementing in a 
     subclass of this class.
     """
+    mark_step_as_complete(request)
     return HttpResponseRedirect(reverse('oscar-checkout-payment'))
 
-
+@prev_steps_must_be_complete
 def payment(request):
     """
     Payment methods are domain-specific and so need implementing in a s
     subclass of this class.
     """
+    mark_step_as_complete(request)
     return HttpResponseRedirect(reverse('oscar-checkout-preview'))
 
-
+@prev_steps_must_be_complete
 def preview(request):
     """
     Show a preview of the order
@@ -90,9 +156,10 @@ def preview(request):
     calc = OrderTotalCalculator(request)
     order_total = calc.order_total_incl_tax(basket)
     
+    mark_step_as_complete(request)
     return render(request, 'checkout/preview.html', locals())
 
-
+@prev_steps_must_be_complete
 def submit(request):
     """
     Do several things then redirect to the thank-you page
