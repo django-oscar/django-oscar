9275500aaf26e60546a896d0cf812bdcef1b86c3
David Winterbottom <david.winterbottom@gmail.com>
2011-02-05 22:51:22 +0000

    Payment events can now be set within the order management

--- oscar/order_management/views.py
+++ oscar/order_management/views.py
@@ -7,7 +7,7 @@ from django.views.generic import ListView, DetailView
 
 from oscar.views import ModelView
 from oscar.services import import_module
-order_models = import_module('order.models', ['Order', 'BatchLine', 'ShippingEvent', 'ShippingEventType', 'PaymentEventType'])
+order_models = import_module('order.models', ['Order', 'BatchLine', 'ShippingEvent', 'ShippingEventType', 'PaymentEvent', 'PaymentEventType'])
 
 
 class OrderListView(ListView):
@@ -33,20 +33,25 @@ class OrderView(ModelView):
         self.response = render(self.request, self.template_file, locals())
         
     def handle_POST(self, order):
-        
-        self.response = HttpResponseRedirect(reverse('oscar-order-management-order', kwargs={'order_number': order.number}))
-        
         line_ids = self.request.POST.getlist('order_line')
+        lines = order.lines.in_bulk(line_ids)
         
         # Need to determine what kind of event update this is
         if self.request.POST['shipping_event']:
-            # Load event type obj
-            event_type = order_models.ShippingEventType.objects.get(code=self.request.POST['shipping_event'])
+            self.create_shipping_event(order, lines, self.request.POST['shipping_event'])
+        elif self.request.POST['payment_event']:
+            self.create_payment_event(order, lines, self.request.POST['payment_event'])
+        self.response = HttpResponseRedirect(reverse('oscar-order-management-order', kwargs={'order_number': order.number}))
+                
+    def create_shipping_event(self, order, lines, type_code):
+        event_type = order_models.ShippingEventType.objects.get(code=type_code)
+        for line in lines.values():
+            order_models.ShippingEvent.objects.create(order=order, line=line, 
+                                                      quantity=line.quantity, event_type=event_type)
             
-            # Need to load lines in a way that checks they are from the order 
-            lines = order_models.BatchLine.objects.in_bulk(line_ids)
-            for line in lines.values():
-                order_models.ShippingEvent.objects.create(order=order, line=line, quantity=line.quantity, event_type=event_type)
-        
-        
+    def create_payment_event(self, order, lines, type_code):
+        event_type = order_models.PaymentEventType.objects.get(code=type_code)
+        for line in lines.values():
+            order_models.PaymentEvent.objects.create(order=order, line=line, 
+                                                     quantity=line.quantity, event_type=event_type)
     
