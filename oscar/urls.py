91f59fbea1c917c14ec3383b7d84199fbd8464e0
David Winterbottom <david.winterbottom@gmail.com>
2011-02-09 15:35:04 +0000

    Added new shop to test modifying checkout and taking payment

--- oscar/urls.py
+++ oscar/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import *
 from oscar.views import home
 
 urlpatterns = patterns('',
-    (r'^$', home),                   
     (r'product/', include('oscar.product.urls')),
     (r'basket/', include('oscar.basket.urls')),
     (r'checkout/', include('oscar.checkout.urls')),
     (r'order-management/', include('oscar.order_management.urls')),
+    (r'^$', home),     
 )
