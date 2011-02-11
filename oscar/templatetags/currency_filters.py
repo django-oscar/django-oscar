cd2c6b8f331ae343efe0610bd4b2a572dceab69b
David Winterbottom <david.winterbottom@gmail.com>
2011-02-11 14:46:53 +0000

    Improved the currency filteR

--- oscar/templatetags/currency_filters.py
+++ oscar/templatetags/currency_filters.py
@@ -1,14 +1,15 @@
 import locale
 
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
 @register.filter(name='currency')
 def currency(value):
     try:
-        locale.setlocale(locale.LC_ALL, 'en_GB.utf8')
-    except:
+        locale.setlocale(locale.LC_ALL, settings.LOCALE)
+    except AttributeError:
         locale.setlocale(locale.LC_ALL, '')
     loc = locale.localeconv()
     return locale.currency(value, loc['currency_symbol'], grouping=True)
