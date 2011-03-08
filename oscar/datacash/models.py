f3c823e9da3851a35e21055d1bed9598c19d4d74
David Winterbottom <david.winterbottom@gmail.com>
2011-03-08 21:02:15 +0000

    Added basic skeleton of a few datacash classes

---
+++ oscar/datacash/models.py
@@ -0,0 +1,7 @@
+u"""Vanilla implementation of Datacash models"""
+from django.db import models
+
+from oscar.datacash.abstract_models import Transaction
+
+class Transaction(AbstractTransaction):
+    pass
