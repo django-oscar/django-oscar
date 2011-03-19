ef8b7c6d67512d4a94a34e1ca7e7f337f04b5cec
David Winterbottom <david.winterbottom@gmail.com>
2011-03-19 22:15:21 +0000

    Added tests for shipping methods and documentation

--- oscar/tests.py
+++ oscar/tests.py
@@ -9,6 +9,7 @@ from oscar.product.tests import *
 from oscar.stock.tests import *
 from oscar.checkout.tests import *
 from oscar.payment.tests import *
+from oscar.shipping.tests import *
 
 from oscar.services import import_module, AppNotFoundError
 
