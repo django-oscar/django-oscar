c4853b144a880f14f254ab8a65e3f159ab09d992
David Winterbottom <david.winterbottom@gmail.com>
2011-03-23 19:43:22 +0000

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='django-oscar',
       author="David Winterbottom",
       author_email="david.winterbottom@tangentlabs.co.uk",
       package_dir={'': '.'},
-      install_requires=['Django=1.3',
+      install_requires=['Django==1.3',
                 'MySQL-python==1.2.3',
                 'PIL==1.1.7']    
       )
