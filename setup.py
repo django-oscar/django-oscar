f4ef66481c83f8788af82a7f2b4df0b28574f0f9
David Winterbottom <david.winterbottom@gmail.com>
2011-03-23 19:41:44 +0000

    Tweaked setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
 from setuptools import setup
 
 setup(name='django-oscar',
-        version='0.1.0',
-        url='https://github.com/codeinthehole/django-oscar',
-        description="A flexible ecommerce application for Django",
-        author="David Winterbottom",
-        author_email="david.winterbottom@tangentlabs.co.uk",
-        package_dir={'': '.'},
-        requires=['Django=1.3',
-                  'MySQL-python==1.2.3',
-                  'PIL==1.1.7'],    
-        classifiers = [
-            "Intended Audience :: Developers",
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Software Development :: eCommerce',
-            ]
-        )
+      version='0.1.0',
+      url='https://github.com/codeinthehole/django-oscar',
+      description="A flexible ecommerce application for Django",
+      author="David Winterbottom",
+      author_email="david.winterbottom@tangentlabs.co.uk",
+      package_dir={'': '.'},
+      install_requires=['Django=1.3',
+                'MySQL-python==1.2.3',
+                'PIL==1.1.7']    
+      )
