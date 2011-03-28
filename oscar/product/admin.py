7be057c0e0abb3c60e9219e14fbcddf2a15d0f20
David Winterbottom <david.winterbottom@gmail.com>
2011-03-28 21:15:30 +0100

    Minor tidy up of product models

--- oscar/product/admin.py
+++ oscar/product/admin.py
@@ -14,9 +14,12 @@ class ItemAdmin(admin.ModelAdmin):
     list_display = ('get_title', 'upc', 'get_item_class', 'is_top_level', 'is_group', 'is_variant', 'attribute_summary', 'date_created')
     prepopulated_fields = {"slug": ("title",)}
     inlines = [AttributeInline]
+    
+class AttributeTypeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"code": ("name",)}
 
 admin.site.register(product_models.ItemClass, ItemClassAdmin)
 admin.site.register(product_models.Item, ItemAdmin)
-admin.site.register(product_models.AttributeType)
+admin.site.register(product_models.AttributeType, AttributeTypeAdmin)
 admin.site.register(product_models.ItemAttributeValue)
 admin.site.register(product_models.Option)
