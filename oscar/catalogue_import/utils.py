fc456e6e57ab1e882255cf5fce2767acf3249011
Kura <kura@deviling.net>
2011-04-15 11:35:09 +0100

    Revert a ton of pointless code. Make a simple CSV importer script

--- oscar/catalogue_import/utils.py
+++ oscar/catalogue_import/utils.py
@@ -2,7 +2,6 @@ import os
 import csv
 import sys
 from oscar.services import import_module
-from oscar.catalogue_import.csv_types import Simple
 
 
 catalogue_exception = import_module('catalogue_import.exceptions', ['CatalogueImportException'])
@@ -12,9 +11,6 @@ stock_models = import_module('stock.models', ['Partner', 'StockRecord'])
 class CatalogueImport(object):
     u"""A catalogue importer object"""
     
-    csv_types = {
-        'simple': Simple,
-    }
     flush = False
     item = []
     
@@ -22,15 +18,11 @@ class CatalogueImport(object):
         u"""Handles the actual import process"""
         if self.file is None:
             raise catalogue_exception.CatalogueImportException("You need to pass a file argument")
-        try:
-            self.mapper = self.csv_types[self.csv_type]()
-        except:
-            raise catalogue_exception.CatalogueImportException("Unable to load CSV mapper: %s" % (csv_type))
         self.catalogue_import_file = CatalogueImportFile()
         self.catalogue_import_file.file = self.file
         self.test_file()
         if self.flush is True:
-            self.flush()
+            self.flushdb()
         self.csv_content = self.load_csv()
         self.iterate()
     
@@ -40,71 +32,50 @@ class CatalogueImport(object):
         self.catalogue_import_file.is_file()
         self.catalogue_import_file.file_is_readable()
         
-    def flush(self):
+    def flushdb(self):
         u"""Flush out product and stock models"""
         product_models.ItemClass.objects.all().delete()
         product_models.Item.objects.all().delete()
         product_models.AttributeType.objects.all().delete()
         product_models.ItemAttributeValue.objects.all().delete()
         product_models.Option.objects.all().delete()
-        product_models.Partner.objects.all().delete()
-        product_models.StockRecord.objects.all().delete()
+        stock_models.Partner.objects.all().delete()
+        stock_models.StockRecord.objects.all().delete()
         
     def load_csv(self):
         u"""Load the CSV content"""
         csv_reader = CatalogueCsvReader()
         return csv_reader.get_csv_contents(self.file)
-
+    
     def iterate(self):
         u"""Iterate over rows, creating a complete list item"""
         for row in self.csv_content:
-            mapped_row = self.map(row)
-            print mapped_row
-            print 
-            self.item = [] # Create a new list, 'just in-case'
-            for item in mapped_row:
-                print item
-                self.current_item = item # Map to current_item, again 'just in-case'
-                self.item.append(item) # Finally append to current mapped item, again 'just in-case'
-            print
-            print self.item
-            print 
-            print self.mapper.model_fields
-            print
-            self.create()
-            print 'new row'
-            sys.exit()
+            upc, title, description, item_class, partner_name, partner_reference, price_excl_tax, num_in_stock = row
+            saved_item_class, _ = product_models.ItemClass.objects.get_or_create(name=item_class)
+            try:
+                saved_item = product_models.Item.objects.get(upc=upc)
+            except product_models.Item.DoesNotExist:
+                saved_item = product_models.Item()
+            saved_item.upc = upc
+            saved_item.title = title
+            saved_item.description = description
+            saved_item.item_class = saved_item_class
+            saved_item.save()
             
-    def create(self):
-        model_objects = []
-        for model, fields in self.mapper.model_fields:
-            print model
-            model_object = model()
-            model_objects.append({'name': model, 'object': model_object})
-            for field in fields:
-                for column in self.item:
-                    if column[0]['model'] == model and column[0]['field'] == field:
-                        print "%s = %s" % (field, column[1])
-                        print dir(model_object)
-                        vars(model_object)[field] = column[1]
-        self.save(model_objects)
-        
-    def save(self, objects):
-        for model in self.mapper.order:
-            for save_object in objects:
-                if save_object['name'] == model:
-                    for fk in self.mapper.fk:
-                        if fk['to'] == model:
-                            vars(save_object['object'])[fk['as']] = vars()[fk['as']]
-                    save_object['object'].save()
-                    for fk in self.mapper.fk:
-                        if fk['from'] == model:
-                            vars()[fk['as']] = save_object['object']
-
-    def map(self, row):
-        u"""Map CSV row against format"""
-        return zip(self.mapper.fields, row)
-
+            saved_partner, _ = stock_models.Partner.objects.get_or_create(name=partner_name)
+            
+            try:
+                saved_stock, _ = stock_models.StockRecord.objects.get(partner_reference=partner_reference)
+            except stock_models.StockRecord.DoesNotExist:
+                saved_stock = stock_models.StockRecord()
+            
+            saved_stock.product = saved_item
+            saved_stock.partner = saved_partner
+            saved_stock.partner_reference = partner_reference
+            saved_stock.price_excl_tax = price_excl_tax
+            saved_stock.num_in_stock = num_in_stock
+            saved_stock.save()
+ 
         
 class CatalogueImportFile(object):
     u"""A catalogue importer file object"""
