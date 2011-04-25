de14e850ad9f4eeb3d789b71a21541544600b4ce
David Winterbottom <david.winterbottom@gmail.com>
2011-04-25 21:54:27 +0100

    Reworked tests to be cleaner and more readable

--- oscar/apps/catalogue_import/tests/command.py
+++ oscar/apps/catalogue_import/tests/command.py
@@ -6,140 +6,79 @@ from oscar.apps.catalogue_import.utils import Importer
 from oscar.apps.catalogue_import.exceptions import CatalogueImportException
 from oscar.apps.product.models import ItemClass, Item
 from oscar.apps.stock.models import Partner, StockRecord
+from oscar.test.helpers import create_product
 
 
-TEST_CSV = os.path.join(os.path.dirname(__file__), '../fixtures/test.csv')
+TEST_CSV = os.path.join(os.path.dirname(__file__), '../fixtures/sample.csv')
 
 
-def setup():
-    importer = Importer()
-    importer.afile = TEST_CSV
-    importer.handle()
+class CommandEdgeCasesTest(TestCase):
 
-class FileArgumentTest(TestCase):
+    def setUp(self):
+        self.importer = Importer()
 
-    def test_sending_no_file_argument(self):
-        importer = Importer()
-        importer.afile = None
+    def test_sending_no_file_argument_raises_exception(self):
+        self.importer.afile = None
         with self.assertRaises(CatalogueImportException):
-            importer.handle()
+            self.importer.handle()
 
-
-class FileTypeTest(TestCase):
-    
-    def test_sending_directory_as_file(self):
-        importer = Importer()
-        importer.afile = "/tmp"
+    def test_sending_directory_as_file_raises_exception(self):
+        self.importer.afile = "/tmp"
         with self.assertRaises(CatalogueImportException):
-            importer.handle()
+            self.importer.handle()
 
 
-class ImportNonExistentFileTest(TestCase):
-    
-    def test_importing_file(self):
-        importer = Importer()
-        importer.afile = "/tmp/catalogue-import.zgvsfsdfsd"
+    def test_importing_nonexistant_file_raises_exception(self):
+        self.importer.afile = "/tmp/catalogue-import.zgvsfsdfsd"
         with self.assertRaises(CatalogueImportException):
-            importer.handle()
+            self.importer.handle()
 
 
-class ItemUpcDoesNotExistTest(TestCase):
-    
-    def test_upc_does_not_exist(self):
-        setup()
-        with self.assertRaises(Item.DoesNotExist):
-            Item.objects.get(upc="abcdupc1")
+class ImportSmokeTest(TestCase):
 
+    def setUp(self):
+        self.importer = Importer()
+        self.importer.afile = TEST_CSV
+        self.importer.handle()
 
-class ItemUpcDoesExistsTest(TestCase):
-    
-    def test_upc_exists(self):
-        setup()
-        upc = "abcdupc"
-        product = Item.objects.get(upc=upc)
-        self.assertEqual(product.upc, upc)
-
-
-class ItemClassNameDoesNotExistTest(TestCase):
-    
-    def test_item_class_name_does_not_exist(self):
-        setup()
-        with self.assertRaises(ItemClass.DoesNotExist):
-            ItemClass.objects.get(name="halo")
-
-
-class ItemClassNameExistsTest(TestCase):
-    
-    def test_item_class_name_exists(self):
-        setup()
-        name = "tshirt"
-        item_class = ItemClass.objects.get(name=name)
-        self.assertEqual(item_class.name, name)
-
-
-class PartnerNameDoesNotExistTest(TestCase):
-    
-    def test_item_class_name_does_not_exist(self):
-        setup()
-        with self.assertRaises(Partner.DoesNotExist):
-            Partner.objects.get(name="JEFF")
-
+    def test_item_is_created(self):
+        try:
+            Item.objects.get(upc="abcdupc")
+        except Item.DoesNotExist:
+            self.fail() 
             
-class PartnerNameExistsTest(TestCase):
-    
-    def test_item_class_name_exists(self):
-        setup()
-        name = "KURA"
-        partner = Partner.objects.get(name=name)    
-        self.assertEqual(partner.name, name)
+    def test_item_class_is_created(self):
+        try:
+            ItemClass.objects.get(name="tshirt")
+        except Item.DoesNotExist:
+            self.fail() 
 
+    def test_partner_is_created(self):
+        try:
+            Partner.objects.get(name="KURA")
+        except Item.DoesNotExist:
+            self.fail() 
+    
+    def test_stockrecord_is_created(self):
+        try:
+            StockRecord.objects.get(partner_reference="SKU999")
+        except Item.DoesNotExist:
+            self.fail()  
 
-class PartnerReferenceDoesNotExistTest(TestCase):
-    
-    def test_partner_reference_does_not_exist(self):
-        setup()
-        with self.assertRaises(StockRecord.DoesNotExist):
-            StockRecord.objects.get(partner_reference="1234")
         
-            
-class PartnerRefenceExistsTest(TestCase):
+class ImportWithFlushTest(TestCase):
     
-    def test_partner_reference_exists(self):
-        setup()
-        partner_reference = "SKU999"
-        stock_record = StockRecord.objects.get(partner_reference=partner_reference)
-        self.assertEqual(partner_reference, partner_reference)
+    def setUp(self):
+        self.importer = Importer()
+        self.importer.afile = TEST_CSV
+
+    def test_items_are_flushed_by_importer(self):
+        upc = "KURA9"
+        create_product(price=D('10.00'), upc=upc)
         
-class FlushTest(TestCase):
-    
-    item_class = "hotpants"
-    product_upc = "KURA9"
-    
-    def setup(self):
-        item_class, _ = ItemClass.objects.get_or_create(name=self.item_class)
+        self.importer.flush = True
+        self.importer.handle()
         
-        item = Item()
-        item.upc = self.product_upc
-        item.title = "Test item"
-        item.description = "Hello there"
-        item.item_class = item_class
-        item.save()
-        
-        partner, _ = Partner.objects.get_or_create(name="Kura")
-        
-        stock = StockRecord()
-        stock.product = item
-        stock.partner = partner
-        stock.partner_reference = "09090"
-        stock.price_excl_tax = D('10.00')
-        stock.num_in_stock = 5
-        stock.save()
-    
-    def test_flush(self):
-        self.setup()
-        importer = Importer()
-        importer.afile = TEST_CSV
-        importer.flush = True
-        importer.handle()
         with self.assertRaises(Item.DoesNotExist):
-            Item.objects.get(upc=self.product_upc)
+            Item.objects.get(upc=upc)
+   
--- oscar/test/helpers.py
+++ oscar/test/helpers.py
@@ -1,13 +1,13 @@
 from oscar.apps.product.models import ItemClass, Item
 from oscar.apps.stock.models import Partner, StockRecord
 
-def create_product(price=None):
+def create_product(price=None, upc="dummy_upc"):
     u"""
     Helper method for creating products that are used in 
     tests.
     """
     ic,_ = ItemClass._default_manager.get_or_create(name="Dummy item class")
-    item = Item._default_manager.create(title="Dummy product", item_class=ic)
+    item = Item._default_manager.create(title="Dummy product", item_class=ic, upc=upc)
     if price:
         partner,_ = Partner._default_manager.get_or_create(name="Dummy partner")
         sr = StockRecord._default_manager.create(product=item, partner=partner, price_excl_tax=price)
