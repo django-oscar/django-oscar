9acba79412df9aac01d66720bff525d46642f194
David Winterbottom <david.winterbottom@gmail.com>
2011-05-04 10:24:56 +0100

    Fixed dependency links in setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,7 @@ setup(name='django-oscar',
       author="David Winterbottom",
       author_email="david.winterbottom@tangentlabs.co.uk",
       package_dir={'': '.'},
-      install_requires=['Django==1.3',
+      install_requires=['Django>=1.3',
           'PIL>=1.1.7',
-          'django-haystack==1.2.0-beta'],
-      dependency_links = [
-          'https://github.com/toastdriven/django-haystack/tarball/master#egg=django-haystack-1.2.0-beta'
-          ]
+          'django-haystack>=1.2.0'],
       )
