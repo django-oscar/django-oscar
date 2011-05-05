652b189859d9d248284b786edd4dd402d0ae9d63
David Winterbottom <david.winterbottom@gmail.com>
2011-05-05 14:34:18 +0100

    Improved promotions package to allow arbitrary new types to be specified within a project

--- oscar/apps/promotions/templatetags/promotions.py
+++ oscar/apps/promotions/templatetags/promotions.py
@@ -1,5 +1,4 @@
-from django.template import Library, Node, Variable, Template, Context
-from django.db.models import get_model
+from django.template import Library, Node, Variable, Context
 from django.template.loader import select_template
      
 register = Library()
@@ -11,11 +10,12 @@ class MerchandisingBlockNode(Node):
     def render(self, context):
         linked_block = self.linked_block.resolve(context)
         template = select_template([linked_block.block.template_file, 'oscar/promotions/block_default.html'])
-        context = Context({'block': linked_block.block})
+        args = dict(block=linked_block.block, **linked_block.block.template_context())
+        context = Context(args)
         return template.render(context)
  
 def get_block_html(parser, token):
-    tag_name, linked_block = token.split_contents()
+    _, linked_block = token.split_contents()
     return MerchandisingBlockNode(linked_block)
 
 register.tag('render_merchandising_block', get_block_html)
