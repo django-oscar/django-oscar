f87456e68951031aa74decd387b5a99204d8fec8
Andrew Ingram <andrew.ingram@tangentlabs.co.uk>
2011-05-19 11:26:21 +0100

    Added attribute override to Application, any field can be overridden by a kwarg in the constructor.

--- oscar/core/application.py
+++ oscar/core/application.py
@@ -1,8 +1,10 @@
 class Application(object):
     name = None
     
-    def __init__(self, app_name=None):
+    def __init__(self, app_name=None, **kwargs):
         self.app_name = app_name
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
     
     def get_urls(self):
         u"""
