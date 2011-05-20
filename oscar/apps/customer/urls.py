20dbb041e9a5db84daca2c386fe4cde693dc9ca0
David Winterbottom <david.winterbottom@gmail.com>
2011-05-20 22:15:06 +0100

    Added redirect to customer app

--- oscar/apps/customer/urls.py
+++ oscar/apps/customer/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import login_required
+from django.views.generic.simple import redirect_to
 
 from oscar.core.decorators import class_based_view
 from oscar.core.loading import import_module
@@ -18,4 +19,5 @@ urlpatterns += patterns('oscar.apps.customer.views',
     url(r'^order/(?P<order_number>[\w-]*)/line/(?P<line_id>\w+)$', login_required(class_based_view(OrderLineView)), name='oscar-customer-order-line'),
     url(r'^address-book/$', AddressBookView.as_view(), name='oscar-customer-address-book'),
     url(r'^address/(?P<address_id>\d+)/$', login_required(class_based_view(AddressView)), name='oscar-customer-address'),
+    url(r'^$', redirect_to, {'url': 'profile/'})
 )
