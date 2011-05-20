bc4b4df4fa520aab0d900d8770352f09482c0cfa
David Winterbottom <david.winterbottom@gmail.com>
2011-05-20 20:49:39 +0100

    Moved settings file

--- settings_test.py
+++ settings_test.py
@@ -3,10 +3,8 @@ Special settings file for use when testing.  This specifies a SQLite
 database to use when running tests.
 
 Just make sure you run the tests and specify this file:
-> ./manage.py test --settings=test_settings
+> ./manage.py test --settings=settings_test
 """
-import sys
-sys.path.append("../..")
 
 from settings import *
 
@@ -18,5 +16,38 @@ DATABASES = {
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['null'],
+            'propagate': True,
+            'level':'INFO',
+        },
+        'django.request': {
+            'handlers': ['null'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'oscar.checkout': {
+            'handlers': ['null'],
+            'propagate': True,
+            'level':'INFO',
+        },
+        'django.db.backends': {
+            'handlers':['null'],
+            'propagate': False,
+            'level':'DEBUG',
+        },
     }
 }
