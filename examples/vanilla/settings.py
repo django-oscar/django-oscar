53f59d261e5bb5b2dc5c1ac8cb93a931324323a3
David Winterbottom <david.winterbottom@gmail.com>
2011-05-20 20:54:45 +0100

    Removed local settings file from git

--- examples/vanilla/settings.py
+++ examples/vanilla/settings.py
@@ -16,12 +16,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.', # Make sure you're using InnoDB
+        'NAME': 'oscar_vanilla',     
+        'USER': '',                           # Set these details in settings_local.py
+        'PASSWORD': '',                       # "
+        'HOST': '',                     
+        'PORT': '',                      
     }
 }
 
