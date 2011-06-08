d6567c73c15ae57b28da521332934eb32bfe8626
Andrew Ingram <andrew.ingram@tangentlabs.co.uk>
2011-06-08 18:14:15 +0100

    Removed import_module from basket admin

--- oscar/apps/basket/admin.py
+++ oscar/apps/basket/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-
-from oscar.core.loading import import_module
-import_module('basket.models', ['Basket', 'Line', 'LineAttribute'], locals())
+from django.db.models import get_model
 
 
 class BasketAdmin(admin.ModelAdmin):
     read_only_fields = ('date_merged', 'date_submitted')
 
 
-admin.site.register(Basket, BasketAdmin)
-admin.site.register(Line)
-admin.site.register(LineAttribute)
+admin.site.register(get_model('basket', 'basket'), BasketAdmin)
+admin.site.register(get_model('basket', 'line'))
+admin.site.register(get_model('basket', 'LineAttribute'))
