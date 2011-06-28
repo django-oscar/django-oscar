5d3f765191153b31654dccfccb0ba3a0541c3c95
Kura <kura@deviling.net>
2011-06-28 18:32:57 +0100

    Clean up docstrings

--- oscar/apps/search/forms.py
+++ oscar/apps/search/forms.py
@@ -5,24 +5,25 @@ from haystack.forms import FacetedSearchForm
 
 
 class SearchInput(Input):
-    '''
+    """
     Defining a search type widget
 
     This is an HTML5 thing and works nicely with Safari, other browsers default
     back to using the default "text" type
-    '''
+    """
     input_type = 'search'
 
 
 class MultiFacetedSearchForm(FacetedSearchForm):
-    '''
+    """
     An extension of the regular faceted search form to alow for multiple facets
-    '''
+    """
     q = forms.CharField(required=False, label=_('Search'), widget=SearchInput({"placeholder": _('Search')}))
+    
     def search(self):
-        '''
+        """
         Overriding the search method to allow for multiple facets
-        '''
+        """
         sqs = super(FacetedSearchForm, self).search().order_by('-score')
         if hasattr(self, 'cleaned_data') and 'selected_facets' in self.cleaned_data:
             for f in self.cleaned_data['selected_facets'].split("|"):
