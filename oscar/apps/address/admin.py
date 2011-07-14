3c6fb376fc86edd430b63e2ebcf9f3300c53d697
David Winterbottom <david.winterbottom@gmail.com>
2011-07-14 14:18:39 +0100

    Updated the user address admin file

--- oscar/apps/address/admin.py
+++ oscar/apps/address/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.db.models import get_model
 
-admin.site.register(get_model('address', 'useraddress'))
+class UserAddressAdmin(admin.ModelAdmin):
+    exclude = ('search_text', 'hash')
+    readonly_fields = ('num_orders',)
+
+
+admin.site.register(get_model('address', 'useraddress'), UserAddressAdmin)
 admin.site.register(get_model('address', 'country'))
