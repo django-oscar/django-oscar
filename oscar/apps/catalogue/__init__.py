2dac329efa337cad0dd029e3205f7cb79e1795be
Andrew Ingram <andrew.ingram@tangentlabs.co.uk>
2011-08-02 18:27:17 +0100

    Working on validating the new attribute api

--- oscar/apps/catalogue/__init__.py
+++ oscar/apps/catalogue/__init__.py
@@ -1,1 +1,19 @@
-from oscar.apps.catalogue import receivers
+from django.db.models.signals import post_init, pre_save, post_save
+from django.dispatch import receiver
+from oscar.apps.catalogue.abstract_models import ProductAttributesContainer
+
+@receiver(post_init)
+def attr_post_init(sender, **kwargs):
+    instance = kwargs['instance']
+    container = ProductAttributesContainer(product=instance)
+    instance.attr = container
+
+@receiver(pre_save)
+def attr_pre_save(sender, **kwargs):
+    instance = kwargs['instance']
+    instance.attr.validate_attributes()
+
+@receiver(post_save)
+def attr_post_save(sender, **kwargs):
+    instance = kwargs['instance']
+    instance.attr.save()                
