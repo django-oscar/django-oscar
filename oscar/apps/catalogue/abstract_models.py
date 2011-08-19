9e55315cad4c3ff4a38d2a139be579f38618b4b5
John Papanastasiou <johnpapps@hotmail.com>
2011-08-19 04:08:02 +0200

    Product Contributors should not require a Contributor Role to be specified, so modified AbstractProductContributor to allow this.

--- oscar/apps/catalogue/abstract_models.py
+++ oscar/apps/catalogue/abstract_models.py
@@ -136,7 +136,7 @@ class AbstractContributor(models.Model):
 class AbstractProductContributor(models.Model):
     product = models.ForeignKey('catalogue.Product')
     contributor = models.ForeignKey('catalogue.Contributor')
-    role = models.ForeignKey('catalogue.ContributorRole')
+    role = models.ForeignKey('catalogue.ContributorRole', blank=True, null=True)
     
     def __unicode__(self):
         return '%s <- %s - %s' % (self.item, self.role, self.contributor)
