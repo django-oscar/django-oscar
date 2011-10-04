87442527c8cd05cd96ca0bb76b6456012658df37
AndrewIngram <andy@andrewingram.net>
2011-10-04 12:49:33 +0200

    Catch and log IntegrityErrors in analytics to stop get_or_create breaking sites.

--- oscar/apps/analytics/receivers.py
+++ oscar/apps/analytics/receivers.py
@@ -1,4 +1,6 @@
 from django.dispatch import receiver
+from django.db import IntegrityError
+import logging
 
 from oscar.core.loading import import_module
 import_module('analytics.models', ['UserRecord', 'ProductRecord', 'UserProductView'], locals())
@@ -8,47 +10,68 @@ import_module('order.signals', ['order_placed'], locals())
 
 # Helpers
 
+logger = logging.getLogger('oscar.analytics')
+
 def _record_product_view(product):
-    record,_ = ProductRecord.objects.get_or_create(product=product)
-    record.num_views += 1
-    record.save()
+    try:
+        record,_ = ProductRecord.objects.get_or_create(product=product)
+        record.num_views += 1
+        record.save()
+    except IntegrityError:
+        # get_or_create sometimes fails due to MySQL's weird transactions, fail silently
+        logger.error("IntegrityError on ProductRecord.objects.get_or_create(product=product)")
     
 def _record_user_product_view(user, product):
     if user.is_authenticated():
         # Update user record
-        record,_ = UserRecord.objects.get_or_create(user=user)
-        record.num_product_views += 1
-        record.save()
+        try:
+            record,_ = UserRecord.objects.get_or_create(user=user)
+            record.num_product_views += 1
+            record.save()
+        except IntegrityError:
+            logger.error("IntegrityError on UserRecord.objects.get_or_create(user=user)")
         
         # Add user product view record
         UserProductView.objects.create(product=product, user=user)
-        
+
 def _record_basket_addition(product):
-    record,_ = ProductRecord.objects.get_or_create(product=product)
-    record.num_basket_additions += 1
-    record.save()
+    try:
+        record,_ = ProductRecord.objects.get_or_create(product=product)
+        record.num_basket_additions += 1
+        record.save()
+    except IntegrityError:
+        logger.error("IntegrityError on ProductRecord.objects.get_or_create(product=product)")
     
 def _record_user_basket_addition(user, product):
     if user.is_authenticated():
-        record,_ = UserRecord.objects.get_or_create(user=user)
-        record.num_basket_additions += 1
-        record.save()
+        try:
+            record,_ = UserRecord.objects.get_or_create(user=user)
+            record.num_basket_additions += 1
+            record.save()
+        except IntegrityError:
+            logger.error("IntegrityError on UserRecord.objects.get_or_create(user=user)")
 
 def _record_products_in_order(order):
     for line in order.lines.all():
-        record,_ = ProductRecord.objects.get_or_create(product=line.product)
-        record.num_purchases += line.quantity
-        record.save()
+        try:
+            record,_ = ProductRecord.objects.get_or_create(product=line.product)
+            record.num_purchases += line.quantity
+            record.save()
+        except IntegrityError:
+            logger.error("IntegrityError on ProductRecord.objects.get_or_create(product=product)")
 
 def _record_user_order(user, order):
     if user.is_authenticated():
-        record,_ = UserRecord.objects.get_or_create(user=user)
-        record.num_orders += 1
-        record.num_order_lines += order.num_lines
-        record.num_order_items += order.num_items
-        record.total_spent += order.total_incl_tax
-        record.date_last_order = order.date_placed
-        record.save()
+        try:
+            record,_ = UserRecord.objects.get_or_create(user=user)
+            record.num_orders += 1
+            record.num_order_lines += order.num_lines
+            record.num_order_items += order.num_items
+            record.total_spent += order.total_incl_tax
+            record.date_last_order = order.date_placed
+            record.save()
+        except IntegrityError:
+            logger.error("IntegrityError on UserRecord.objects.get_or_create(user=user)")
 
 def _record_user_product_search(user, query):
     if user.is_authenticated():
