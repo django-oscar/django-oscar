5e05c3bc4d73a753538cb9853d715ecbfbf0aeb7
David Winterbottom <david.winterbottom@gmail.com>
2011-11-09 10:32:44 +0000

    Re-enabled a commented out test, which passed without me needing to change anything

--- oscar/apps/offer/tests.py
+++ oscar/apps/offer/tests.py
@@ -268,15 +268,14 @@ class AbsoluteDiscountBenefitTest(OfferTest):
         self.benefit.max_affected_items = 1
         self.assertEquals(Decimal('5.00'), self.benefit.apply(self.basket))
 
-    # I'm not sure how to fix this
-    #def test_discount_can_only_be_applied_once(self):
-    #    # Add 3 items to make total 15.00
-    #    self.basket.add_product(self.item, 3)
-    #    first_discount = self.benefit.apply(self.basket)
-    #    self.assertEquals(Decimal('10.00'), first_discount)
-    #    
-    #    second_discount = self.benefit.apply(self.basket)
-    #    self.assertEquals(Decimal('5.00'), second_discount)
+    def test_discount_can_only_be_applied_once(self):
+        # Add 3 items to make total 15.00
+        self.basket.add_product(self.item, 3)
+        first_discount = self.benefit.apply(self.basket)
+        self.assertEquals(Decimal('10.00'), first_discount)
+        
+        second_discount = self.benefit.apply(self.basket)
+        self.assertEquals(Decimal('5.00'), second_discount)
         
         
 class MultibuyDiscountBenefitTest(OfferTest):
