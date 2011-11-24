328471c4a9eb47a5769c5114dfad00bf752ad1be
Patryk Zawadzki <patrys@room-303.com>
2011-11-24 16:09:33 +0100

    line_id has to be a number

--- oscar/apps/customer/app.py
+++ oscar/apps/customer/app.py
@@ -25,13 +25,13 @@ class CustomerApplication(Application):
         urlpatterns = patterns('django.contrib.auth.views',
             url(r'^logout/$', 'logout', name='logout', kwargs={'next_page': '/'}),
         )
-        
+
         urlpatterns += patterns('',
             url(r'^$', login_required(self.summary_view.as_view()), name='summary'),
             url(r'^login/$', self.login_view.as_view(), name='login'),
             url(r'^orders/$', login_required(self.order_history_view.as_view()), name='order-list'),
             url(r'^orders/(?P<order_number>[\w-]*)/$', login_required(self.order_detail_view.as_view()), name='order'),
-            url(r'^orders/(?P<order_number>[\w-]*)/(?P<line_id>\w+)$', login_required(self.order_line_view.as_view()), name='order-line'),
+            url(r'^orders/(?P<order_number>[\w-]*)/(?P<line_id>\d+)$', login_required(self.order_line_view.as_view()), name='order-line'),
             url(r'^addresses/$', login_required(self.address_list_view.as_view()), name='address-list'),
             url(r'^addresses/add/$', login_required(self.address_create_view.as_view()), name='address-create'),
             url(r'^addresses/(?P<pk>\d+)/$', login_required(self.address_update_view.as_view()), name='address-detail'),
