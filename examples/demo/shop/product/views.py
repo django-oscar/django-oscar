2a009a6300c3b466e716cb15ae177dc30a15f80f
David Winterbottom <david.winterbottom@gmail.com>
2011-12-22 22:46:34 +0000

    Removed the examples folder.
    Example implementations of oscar are now moved to their own repo.

--- examples/demo/shop/product/views.py
+++
@@ -1,4 +0,0 @@
-from oscar.apps.catalogue.views import ItemDetailView
-
-class MyItemDetailView(ItemDetailView):
-    template_name = "product/myitem.html"
--- examples/vanilla/README.md
+++
@@ -1,2 +0,0 @@
-This is an example project that uses oscar with all its default settings.  
-Nothing is overridden or customised.
