07ee43c9942942810220a38d807c622638668b32
David Winterbottom <david.winterbottom@gmail.com>
2012-01-17 09:25:13 +0000

    Fixed bug with basket template tag.
    The template tag was not passing the basket through to the AddToBasket
    form, causing a bug.  Not spotted in other projects as they define their
    own add-to-basket forms.

--- oscar/apps/basket/templatetags/basket_tags.py
+++ oscar/apps/basket/templatetags/basket_tags.py
@@ -5,25 +5,32 @@ from oscar.apps.basket.forms import AddToBasketForm
 register = template.Library()
 product_model = get_model('catalogue', 'product')
 
+
 @register.tag(name="basket_form")
 def do_basket_form(parse, token):
     tokens = token.split_contents()
     
-    if len(tokens) < 3 or tokens[2] != 'as':
-        raise template.TemplateSyntaxError("%r tag uses the following syntax: {% basket_form product_var as form_var %}" % tokens[0])
+    if len(tokens) < 4 or tokens[3] != 'as':
+        raise template.TemplateSyntaxError("%r tag uses the following syntax: {%% basket_form basket_var product_var as form_var %%}" % tokens[0])
     
-    product_var = tokens[1]
-    form_var = tokens[3]
+    basket_var = tokens[1]
+    product_var = tokens[2]
+    form_var = tokens[4]
     
-    return BasketFormNode(product_var, form_var)
+    return BasketFormNode(basket_var, product_var, form_var)
 
 
 class BasketFormNode(template.Node):
-    def __init__(self, product_var, form_var):
+    def __init__(self, basket_var, product_var, form_var):
+        self.basket_var = template.Variable(basket_var)
         self.product_var = template.Variable(product_var)
         self.form_var = form_var
         
     def render(self, context):
+        try:
+            basket = self.basket_var.resolve(context)
+        except template.VariableDoesNotExist:
+            return ''
         try:
             product = self.product_var.resolve(context)
         except template.VariableDoesNotExist:
@@ -33,5 +40,5 @@ class BasketFormNode(template.Node):
             initial = {
                 'product_id': product.id,
             }
-            context[self.form_var] = AddToBasketForm(instance=product,initial=initial)
+            context[self.form_var] = AddToBasketForm(basket, instance=product,initial=initial)
         return ''
