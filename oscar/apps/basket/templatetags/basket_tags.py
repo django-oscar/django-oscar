77b884cfe460ca5e845403333d0fa373074ffabb
David Winterbottom <david.winterbottom@gmail.com>
2012-01-17 09:31:46 +0000

    Minor tidy-up of the basket template tags module

--- oscar/apps/basket/templatetags/basket_tags.py
+++ oscar/apps/basket/templatetags/basket_tags.py
@@ -1,22 +1,24 @@
 from django import template
 from django.db.models import get_model
 
-from oscar.apps.basket.forms import AddToBasketForm
+from oscar.core.loading import import_module
+forms = import_module('basket.forms', ['AddToBasketForm'])
+Product = get_model('catalogue', 'product')
+
 register = template.Library()
-product_model = get_model('catalogue', 'product')
 
 
 @register.tag(name="basket_form")
 def do_basket_form(parse, token):
+    """
+    Template tag for adding the add-to-basket form to the
+    template context so it can be rendered.
+    """
     tokens = token.split_contents()
-    
     if len(tokens) < 4 or tokens[3] != 'as':
         raise template.TemplateSyntaxError("%r tag uses the following syntax: {%% basket_form basket_var product_var as form_var %%}" % tokens[0])
     
-    basket_var = tokens[1]
-    product_var = tokens[2]
-    form_var = tokens[4]
-    
+    basket_var, product_var, form_var = tokens[1], tokens[2], tokens[4]
     return BasketFormNode(basket_var, product_var, form_var)
 
 
@@ -29,16 +31,13 @@ class BasketFormNode(template.Node):
     def render(self, context):
         try:
             basket = self.basket_var.resolve(context)
-        except template.VariableDoesNotExist:
-            return ''
-        try:
             product = self.product_var.resolve(context)
         except template.VariableDoesNotExist:
             return ''
         
-        if isinstance(product, product_model):
+        if isinstance(product, Product):
             initial = {
                 'product_id': product.id,
             }
-            context[self.form_var] = AddToBasketForm(basket, instance=product,initial=initial)
+            context[self.form_var] = forms.AddToBasketForm(basket, instance=product,initial=initial)
         return ''
