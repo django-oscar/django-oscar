fb09859024c82ab8bbe48234aefb34365b790d32
Andrew Ingram <andrew.ingram@tangentlabs.co.uk>
2012-01-27 11:07:52 +0000

    added tests for ExtendedURLValidator

--- oscar/core/tests.py
+++ oscar/core/tests.py
@@ -3,6 +3,8 @@ import unittest
 from django.test import TestCase
 
 from oscar.core.loading import import_module, AppNotFoundError
+from oscar.core.validators import ExtendedURLValidator
+from django.core.exceptions import ValidationError
 
 class ImportAppTests(unittest.TestCase):
 
@@ -13,3 +15,24 @@ class ImportAppTests(unittest.TestCase):
     def test_unknown_apps_raise_exception(self):
         self.assertRaises(AppNotFoundError, import_module, 'banana', ['skin'])
 
+
+class ValidatorTests(unittest.TestCase):
+    
+    def test_validate_local_url(self):
+        v = ExtendedURLValidator(verify_exists=True)
+               
+        try:
+            v('/')
+        except ValidationError:
+            self.fail('ExtendedURLValidator raised ValidationError unexpectedly!')
+            
+        try:
+            v('/?q=test')  # Query strings shouldn't affect validation
+        except ValidationError:
+            self.fail('ExtendedURLValidator raised ValidationError unexpectedly!')
+
+        with self.assertRaises(ValidationError):
+            v('/invalid/')
+        
+        with self.assertRaises(ValidationError):
+            v('/invalid/?q=test')  # Query strings shouldn't affect validation
