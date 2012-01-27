5c6b4058eeeda1fee58e4b9b672f4377b3196655
David Winterbottom <david.winterbottom@gmail.com>
2012-01-27 09:21:35 +0000

    Renamed management commands to have a namespace.
    This seems cleaner overall and allows the common names such as
    "import_catalogue" to be defined by your application.

--- oscar/management/commands/oscar_calculate_scores.py
+++ oscar/management/commands/oscar_calculate_scores.py
@@ -5,16 +5,14 @@ from django.core.management.base import BaseCommand
 from oscar.core.loading import import_module
 import_module('analytics.utils', ['ScoreCalculator'], locals())
 
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
-    
     help = 'Calculate product scores based on analytics data'
     
     def handle(self, *args, **options):
-        
-        logger = logging.getLogger(__name__)
-        calculator = ScoreCalculator(logger)
-        calculator.run()
+        ScoreCalculator(logger).run()
 
         
         
