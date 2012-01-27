6851048780a70d6862e3ebe12b349324f55f9237
David Winterbottom <david.winterbottom@gmail.com>
2012-01-27 15:03:28 +0000

    Replaced import_module with a better class loading mechanism.
    This ones won't causes pep8 to complain.

--- oscar/core/tests.py
+++ oscar/core/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.conf import settings
 
 from oscar.core.loading import import_module, AppNotFoundError, \
-        get_classes
+        get_classes, get_class
 from oscar.core.validators import ExtendedURLValidator
-from django.core.exceptions import ValidationError
+from oscar.test import patch_settings
 
 
 class ImportAppTests(TestCase):
@@ -23,13 +25,42 @@ class ClassLoadingTests(TestCase):
         self.assertEqual('oscar.apps.catalogue.models', Product.__module__)
         self.assertEqual('oscar.apps.catalogue.models', Category.__module__)
 
+    def test_loading_oscar_class(self):
+        Product = get_class('catalogue.models', 'Product')
+        self.assertEqual('oscar.apps.catalogue.models', Product.__module__)
+
     def test_bad_appname_raises_exception(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(AppNotFoundError):
             Product, Category = get_classes('fridge.models', ('Product', 'Category'))
 
-    def _test_loading_local_project_class(self):
-        (Importer,) = get_classes('catalogue.utils', ('Importer',))
-        self.assertEqual('tests.catalogue.utils', Importer.__module__)
+
+class ClassLoadingWithLocalOverrideTests(TestCase):
+
+    def setUp(self):
+        self.installed_apps = list(settings.INSTALLED_APPS)
+        self.installed_apps[self.installed_apps.index('oscar.apps.shipping')] = 'tests.shipping'
+
+    def test_loading_class_defined_in_local_module(self):
+        with patch_settings(INSTALLED_APPS=self.installed_apps):
+            (Free,) = get_classes('shipping.methods', ('Free',))
+            self.assertEqual('tests.shipping.methods', Free.__module__)
+
+    def test_loading_class_which_is_not_defined_in_local_module(self):
+        with patch_settings(INSTALLED_APPS=self.installed_apps):
+            (FixedPrice,) = get_classes('shipping.methods', ('FixedPrice',))
+            self.assertEqual('oscar.apps.shipping.methods', FixedPrice.__module__)
+
+    def test_loading_class_from_module_not_defined_in_local_app(self):
+        with patch_settings(INSTALLED_APPS=self.installed_apps):
+            (Repository,) = get_classes('shipping.repository', ('Repository',))
+            self.assertEqual('oscar.apps.shipping.repository', Repository.__module__)
+
+    def test_loading_classes_defined_in_both_local_and_oscar_modules(self):
+        with patch_settings(INSTALLED_APPS=self.installed_apps):
+            (Free, FixedPrice) = get_classes('shipping.methods', ('Free', 'FixedPrice'))
+            self.assertEqual('tests.shipping.methods', Free.__module__)
+            self.assertEqual('oscar.apps.shipping.methods', FixedPrice.__module__)
+
 
 
 class ValidatorTests(TestCase):
