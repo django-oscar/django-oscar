b694272de31be46bccf795fd0c14e732147daf79
Eleni Lixourioti <inelegr88@gmail.com>
2012-01-31 16:05:02 +0000

    added BulkEditMixin for the dashboard

--- oscar/apps/dashboard/users/tests.py
+++ oscar/apps/dashboard/users/tests.py
@@ -9,14 +9,45 @@ from oscar.apps.dashboard.users.views import IndexView
 
 class IndexViewTests(ClientTestCase):
     is_staff = True
+    active_users_ids = []
+    inactive_users_ids = []
 
     def setUp(self):
         super(IndexViewTests, self).setUp()
-        for i in range(1, 50):
-            get(User)
+        for i in range(1, 25):
+            get(User, is_active=True)
+        for i in range(1, 25):
+            get(User, is_active=False)
+
+        user_queryset = User.objects.all()
+        self.active_users_ids = user_queryset.filter(is_active=True).values_list('id', flat=True)
+        self.inactive_users_ids = user_queryset.filter(is_active=False).values_list('id', flat=True)
 
     def test_user_list_view(self):
         response = self.client.get(reverse('dashboard:users-index'))
         self.assertInContext(response, 'user_list')
         self.assertEquals(len(response.context['user_list']), IndexView.paginate_by)
 
+    def test_make_active(self):
+        params = {'action': 'make_active',
+                  'selected_user': self.inactive_users_ids}
+        response = self.client.post(reverse('dashboard:users-index'), params)
+        ex_inactive = User.objects.get(id=self.inactive_users_ids[10])
+        self.assertIsRedirect(response)
+        self.assertTrue(ex_inactive.is_active)
+
+    def test_make_inactive(self):
+        params = {'action': 'make_inactive',
+                  'selected_user': self.active_users_ids}
+        response = self.client.post(reverse('dashboard:users-index'), params)
+        ex_active = User.objects.get(id=self.active_users_ids[10])
+        self.assertIsRedirect(response)
+        self.assertFalse(ex_active.is_active)
+
+class DetailViewTests(ClientTestCase):
+    is_staff = True
+
+    def test_user_detail_view(self):
+        response = self.client.get(reverse('dashboard:user-detail', kwargs={'pk': 1} ))
+        self.assertInContext(response, 'user')
+        self.assertIsOk(response)
--- oscar/apps/dashboard/users/views.py
+++ oscar/apps/dashboard/users/views.py
@@ -1,17 +1,39 @@
-from django.views.generic import ListView, DetailView
+from django.contrib import messages
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views.generic import ListView, DetailView
 
-class IndexView(ListView):
+from oscar.apps.dashboard.views import BulkEditMixin
+
+
+class IndexView(ListView, BulkEditMixin):
     template_name = 'dashboard/users/index.html'
     paginate_by = 25
     model = User
-
+    actions = ('make_active', 'make_inactive', )
+    current_view = 'dashboard:users-index'
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         return context
 
+    def make_inactive(self, request, users):
+        return self._change_users_active_status(users, False)
+
+    def make_active(self, request, users):
+        return self._change_users_active_status(users, True)
+
+    def _change_users_active_status(self, users, value):
+        for user in users:
+            if not user.is_superuser:
+                user.is_active = value
+                user.save()
+        messages.info(self.request, 'Users\' status successfully changed')
+        return HttpResponseRedirect(reverse(self.current_view))
+
 
 class UserDetailView(DetailView):
     template_name = 'dashboard/users/detail.html'
     model = User
+
