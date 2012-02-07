2089b12408b8122b07dea7425d7ecd7fc97839c3
David Winterbottom <david.winterbottom@gmail.com>
2012-02-07 17:37:51 +0000

    Extended test runner to generate coverage stats

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@ import sys
 import os
 import logging
 from optparse import OptionParser
+from coverage import coverage
 
 import tests.config
 
@@ -15,10 +16,24 @@ def run_tests(*test_args):
     test_runner = DjangoTestSuiteRunner(verbosity=1)
     if not test_args:
         test_args = ['oscar']
-    failures = test_runner.run_tests(test_args)
-    sys.exit(failures)
+    num_failures = test_runner.run_tests(test_args)
+    if num_failures:
+        sys.exit(num_failures)
 
 if __name__ == '__main__':
     parser = OptionParser()
+    parser.add_option('-c', '--coverage', dest='use_coverage', default=False,
+                      action='store_true', help="Generate coverage report")
     (options, args) = parser.parse_args()
-    run_tests(*args)
+
+    if options.use_coverage:
+        print 'Running tests with coverage'
+        c = coverage(source=['oscar'])
+        c.start()
+        run_tests(*args)
+        c.stop()
+        print 'Generate HTML reports'
+        c.html_report()
+    else:
+        print 'Running tests'
+        run_tests(*args)
