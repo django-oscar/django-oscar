fd4f9776ae8ff6a7b6b1e447be2e8de244fd9487
Pavel Vavruska <pavel.vavruska@tangentlabs.co.uk>
2012-02-21 17:30:25 +0000

    Cleaned up code

--- oscar/apps/dashboard/orders/models.py
+++ oscar/apps/dashboard/orders/models.py
@@ -7,8 +7,3 @@ class OrderSummary(models.Model):
 
     def __unicode__(self):
         return u"%s - %s : OrderSummary" % (self.date_from, self.date_to)
-
-
-#    date_formats = ('%d/%m/%Y',)
-#    date_from = forms.DateField(required=False, label="Date from", input_formats=date_formats)
-#    date_to = forms.DateField(required=False, label="Date to", input_formats=date_formats)
