4ccaf44f54d91b93a8396b7f1b5caeb1086e88fb
Sebastian Vetter <sebastian.vetter@tangentone.com.au>
2012-03-15 15:06:52 +1100

    added validator to check for non-existing URLs
    I added a new validator to oscar.core.validators that validates
    absolute and relative URL and checks if the URL already exists. We
    use this functionality when creating flatpages in the dashboard to
    check if the URL can be used.
    The validation includes *two* calls to ExtendedURLValidator to
    validate the URL in the first run and then check if it already
    exists in the second. This might not be the most efficient way
    of handling it. The reason to do it that way is that it reduces
    the amount of code that has to be replicated.

--- oscar/core/validators.py
+++ oscar/core/validators.py
@@ -1,3 +1,5 @@
+import re
+
 from django.core import validators
 from django.core.exceptions import ValidationError
 from django.core.urlresolvers import resolve
@@ -8,6 +10,7 @@ from django.db.models import get_model
 
 # FlatPages is None if not installed
 FlatPage = get_model('flatpages', 'FlatPage')
+
 
 class ExtendedURLValidator(validators.URLValidator):
     def __call__(self, value):
@@ -49,3 +52,41 @@ class ExtendedURLValidator(validators.URLValidator):
             value = value[:q_index]
 
         return value
+
+
+class URLDoesNotExistValidator(ExtendedURLValidator):
+
+    def __call__(self, value):
+        """
+        Validate that the URL in *value* does not already exist. The
+        URL will be verified first and raises ``ValidationError`` when
+        it is invalid. A valid URL is checked for existance and raises
+        ``ValidationError`` if the URL already exists. Setting attribute
+        ``verify_exists`` has no impact on validation.
+        This validation uses two calls to ExtendedURLValidator which can
+        be slow. Be aware of this, when you use it.
+
+        Returns ``None`` if URL is valid and does not exist.
+        """
+        self.verify_exists = False
+
+        ## calling ExtendedURLValidator twice instead of replicating its code
+        ## and invert the cases when a ValidationError is returned seemes to
+        ## be a much cleaner solution. Although this might not be the most
+        ## efficient way of handling this, it should have not much of an impact
+        ## due to its current application in flatpage creation.
+        try:
+            super(URLDoesNotExistValidator, self).__call__(value)
+        except ValidationError:
+            raise ValidationError(_('Specified page does already exist'),
+                                    code='invalid')
+
+        ## check if URL exists since it seems to be valid
+        try:
+            self.verify_exists = True
+            super(URLDoesNotExistValidator, self).__call__(value)
+        except ValidationError:
+            return
+
+        raise ValidationError(_('Specified page does already exist'),
+                                code='invalid')
