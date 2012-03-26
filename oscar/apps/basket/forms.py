02fda7c8b14a0f58e5644001b4bcd2021eb5b060
Eleni Lixourioti <inelegr88@gmail.com>
2012-03-26 14:21:28 +0100

    fixed missing basket field

--- oscar/apps/basket/forms.py
+++ oscar/apps/basket/forms.py
@@ -96,7 +96,7 @@ class AddToBasketForm(forms.Form):
         else:
             desired_qty = qty + line.quantity
 
-        is_available, reason = product.is_purchase_permitted(user=self.user, 
+        is_available, reason = product.is_purchase_permitted(user=self.user,
                                                              quantity=desired_qty)
         if not is_available:
             raise forms.ValidationError(reason)
@@ -145,4 +145,6 @@ class AddToBasketForm(forms.Form):
         """
         self.fields[option.code] = forms.CharField()
 
+class SimpleAddToBasketForm(AddToBasketForm):
+    quantity = forms.IntegerField(initial=1, min_value=1)
 
