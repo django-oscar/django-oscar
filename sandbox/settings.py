84069a4c0816d71a6997ce62ed8832b130e28ffe
David Winterbottom <david.winterbottom@gmail.com>
2012-04-02 09:49:38 +0100

    Updated settings for sandbox

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -51,7 +51,7 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = location("assets")
+MEDIA_ROOT = location("assets/media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
