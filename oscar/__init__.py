7c481761da56e303bcd6669fc7d0b77f43844ed3
David Winterbottom <david.winterbottom@gmail.com>
2012-04-23 12:28:52 +0100

    Added helper functions for dealing with oscar's long list of standard apps

--- oscar/__init__.py
+++ oscar/__init__.py
@@ -19,6 +19,7 @@ def get_version():
         version = '%s %s %s' % (version, VERSION[3], VERSION[4])
     return version
 
+
 # Cheeky setting that can be used to override templates and give them
 # the same name.  You can use:
 #
@@ -27,3 +28,55 @@ def get_version():
 # when you want to create a new template with path 'basket/basket.html'
 # Just add this setting to the end of your TEMPLATE_DIRS setting.
 OSCAR_PARENT_TEMPLATE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+OSCAR_CORE_APPS = [
+    'oscar',
+    'oscar.apps.analytics',
+    'oscar.apps.discount',
+    'oscar.apps.order',
+    'oscar.apps.checkout',
+    'oscar.apps.shipping',
+    'oscar.apps.catalogue',
+    'oscar.apps.catalogue.reviews',
+    'oscar.apps.basket',
+    'oscar.apps.payment',
+    'oscar.apps.offer',
+    'oscar.apps.address',
+    'oscar.apps.partner',
+    'oscar.apps.customer',
+    'oscar.apps.promotions',
+    'oscar.apps.search',
+    'oscar.apps.voucher',
+    'oscar.apps.dashboard',
+    'oscar.apps.dashboard.reports',
+    'oscar.apps.dashboard.users',
+    'oscar.apps.dashboard.orders',
+    'oscar.apps.dashboard.promotions',
+    'oscar.apps.dashboard.catalogue',
+]
+
+
+def get_core_apps(overrides=None):
+    """
+    Return a list of oscar's apps amended with any passed overrides
+    """
+    if overrides is None:
+        return OSCAR_CORE_APPS
+    def get_app_label(app_label, overrides):
+        pattern = app_label.replace('oscar.apps.', '')
+        for override in overrides:
+            if override.endswith(pattern):
+                return override
+        return app_label
+
+    apps = []
+    for app_label in OSCAR_CORE_APPS:
+        apps.append(get_app_label(app_label, overrides))
+    return apps
+
+
+
+
+
+    return OSCAR_CORE_APPS
