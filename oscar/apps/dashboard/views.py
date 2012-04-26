a0a8c665dbd96f6e483aedf86d982c332b58a400
Sebastian Vetter <sebastian.vetter@tangentone.com.au>
2012-04-26 17:42:11 +1000

    added extra stats for stock & offers

--- oscar/apps/dashboard/views.py
+++ oscar/apps/dashboard/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from decimal import Decimal as D
 
 from django.views.generic import FormView
@@ -5,6 +6,13 @@ from django.db.models.loading import get_model
 from django.db.models import Sum, Count
 
 from oscar.apps.dashboard import forms
+
+ConditionalOffer = get_model('offer', 'ConditionalOffer')
+Condition = get_model('offer', 'Condition')
+Benefit = get_model('offer', 'Benefit')
+
+StockAlert = get_model('partner', 'StockAlert')
+Product = get_model('catalogue', 'Product')
 
 Order = get_model('order', 'Order')
 Line = get_model('order', 'Line')
@@ -35,9 +43,40 @@ class IndexView(FormView):
         ctx.update(self.get_stats(filters))
         return ctx
 
+    def get_current_offers(self):
+        return ConditionalOffer.objects.filter(end_date__gt=datetime.now())
+
+    def get_expired_offers(self):
+        return ConditionalOffer.objects.filter(end_date__gt=datetime.now())
+
+    def get_offer_conditions(self):
+        conditions = {}
+        for id_, _ in Condition.TYPE_CHOICES:
+            conditions[id_] = Condition.objects.filter(type=id_)
+        return conditions
+
+    def get_offer_benefits(self):
+        benefits = {}
+        for id_, _ in Benefit.TYPE_CHOICES:
+            benefits[id_] = Benefit.objects.filter(type=id_)
+        return benefits 
+
     def get_stats(self, filters):
         orders = Order.objects.filter(**filters)
+        open_alerts = StockAlert.objects.filter(status=StockAlert.OPEN)
+        closed_alerts = StockAlert.objects.filter(status=StockAlert.CLOSED)
+
+        self.get_offer_conditions()
+
         stats = {
+            'total_products': Product.objects.count(),
+            'total_open_stock_alerts': open_alerts.count(),
+            'total_closed_stock_alerts': closed_alerts.count(),
+            'total_current_offers': self.get_current_offers().count(),
+            'total_current_conditions': self.get_offer_conditions(),
+            'total_current_benefits': self.get_offer_benefits(),
+            #'total_expired_offers': self.get_expired_offers().count(),
+
             'total_orders': orders.count(),
             'total_lines': Line.objects.filter(order__in=orders).count(),
             'total_revenue': orders.aggregate(Sum('total_incl_tax'))['total_incl_tax__sum'] or D('0.00'),
