b42098b99a6ca9c0e6890d5d896b48c5ed77cf47
Sebastian Vetter <sebastian.vetter@tangentone.com.au>
2012-04-27 17:25:11 +1000

    updated stats with graph and daily stats

--- oscar/apps/dashboard/views.py
+++ oscar/apps/dashboard/views.py
@@ -1,15 +1,15 @@
-from datetime import datetime
 from decimal import Decimal as D
+from datetime import datetime, timedelta
 
 from django.views.generic import FormView
 from django.db.models.loading import get_model
-from django.db.models import Sum, Count
+from django.db.models import Avg, Sum, Count
 
 from oscar.apps.dashboard import forms
+from oscar.apps.offer.models import SITE
 
 ConditionalOffer = get_model('offer', 'ConditionalOffer')
-Condition = get_model('offer', 'Condition')
-Benefit = get_model('offer', 'Benefit')
+Voucher = get_model('voucher', 'Voucher')
 
 StockAlert = get_model('partner', 'StockAlert')
 Product = get_model('catalogue', 'Product')
@@ -43,39 +43,63 @@ class IndexView(FormView):
         ctx.update(self.get_stats(filters))
         return ctx
 
-    def get_current_offers(self):
-        return ConditionalOffer.objects.filter(end_date__gt=datetime.now())
+    def get_site_offers(self):
+        return ConditionalOffer.objects.filter(end_date__gt=datetime.now(),
+                                               offer_type=SITE)
 
-    def get_expired_offers(self):
-        return ConditionalOffer.objects.filter(end_date__gt=datetime.now())
+    def get_vouchers(self):
+        return Voucher.objects.filter(end_date__gt=datetime.now())
 
-    def get_offer_conditions(self):
-        conditions = {}
-        for id_, _ in Condition.TYPE_CHOICES:
-            conditions[id_] = Condition.objects.filter(type=id_)
-        return conditions
+    def get_bar_chart_data(self, hours=25):
+        time_now = datetime.now().replace(minute=0, second=0)
+        # subtract 1 to make sure that the full hour is taken into account
+        start_time = time_now - timedelta(hours=hours-1)
 
-    def get_offer_benefits(self):
-        benefits = {}
-        for id_, _ in Benefit.TYPE_CHOICES:
-            benefits[id_] = Benefit.objects.filter(type=id_)
-        return benefits 
+        orders_last_day = Order.objects.filter(date_placed__gt=start_time)
+
+        order_total_per_hour = []
+        for hour in range(0, hours):
+            end_time = start_time + timedelta(hours=1)
+
+            hourly_orders = orders_last_day.filter(date_placed__gt=start_time, date_placed__lt=end_time)
+            total = hourly_orders.aggregate(Sum('total_incl_tax'))['total_incl_tax__sum'] or D('0.0')
+
+            order_total_per_hour.append({'end_time': end_time, 'total_incl_tax': total})
+            start_time = end_time
+
+        max_value = max([x['total_incl_tax'] for x in order_total_per_hour])
+
+        for item in order_total_per_hour:
+            item['percentage'] = int(item['total_incl_tax'] / (max_value / D(100.)))
+
+        return {
+            'order_total_per_hour': order_total_per_hour,
+            'max_revenue': max_value,
+            'y_range': [int(x) for x in reversed(range(0, max_value, (max_value/D(10))))]
+        }
 
     def get_stats(self, filters):
         orders = Order.objects.filter(**filters)
+
+        date_24hrs_ago = datetime.now() - timedelta(hours=24)
+        orders_last_day = Order.objects.filter(date_placed__gt=date_24hrs_ago)
+
         open_alerts = StockAlert.objects.filter(status=StockAlert.OPEN)
         closed_alerts = StockAlert.objects.filter(status=StockAlert.CLOSED)
 
-        self.get_offer_conditions()
-
         stats = {
+            'total_orders_last_day': orders_last_day.count(),
+            'average_order_costs': orders_last_day.aggregate(Avg('total_incl_tax'))['total_incl_tax__avg'] or D('0.00'),
+            'total_revenue_last_day': orders_last_day.aggregate(Sum('total_incl_tax'))['total_incl_tax__sum'] or D('0.00'),
+
+            'bar_chart_data': self.get_bar_chart_data(),
+
             'total_products': Product.objects.count(),
             'total_open_stock_alerts': open_alerts.count(),
             'total_closed_stock_alerts': closed_alerts.count(),
-            'total_current_offers': self.get_current_offers().count(),
-            'total_current_conditions': self.get_offer_conditions(),
-            'total_current_benefits': self.get_offer_benefits(),
-            #'total_expired_offers': self.get_expired_offers().count(),
+
+            'total_site_offers': self.get_site_offers().count(),
+            'total_vouchers': self.get_vouchers().count(),
 
             'total_orders': orders.count(),
             'total_lines': Line.objects.filter(order__in=orders).count(),
