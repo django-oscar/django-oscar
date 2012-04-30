fd0c606489c53ebbb556ebfcac6a1de5d345b538
David Winterbottom <david.winterbottom@gmail.com>
2012-04-30 20:52:47 +0100

    Updated 404 and 500 handling

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 from oscar.app import shop
+from oscar.views import handler500, handler404
 
 admin.autodiscover()
 
@@ -13,13 +14,11 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'', include(shop.urls)),
 )
+
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    if settings.DEBUG:
-        urlpatterns += staticfiles_urlpatterns()
-        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-        urlpatterns += patterns('',
-            url(r'^404$', TemplateView.as_view(template_name='404.html')),
-            url(r'^500$', TemplateView.as_view(template_name='500.html')),
-        )
+    urlpatterns += patterns('',
+        url(r'^404$', TemplateView.as_view(template_name='404.html')),
+        url(r'^500$', TemplateView.as_view(template_name='500.html')),
+    )
