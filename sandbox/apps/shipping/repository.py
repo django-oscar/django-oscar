cefbca1aaae5070bac37e4bd7a5370a33cb24428
David Winterbottom <david.winterbottom@gmail.com>
2012-05-01 18:09:15 +0100

    Updated shipping methods display

--- sandbox/apps/shipping/repository.py
+++ sandbox/apps/shipping/repository.py
@@ -1,9 +1,25 @@
 from oscar.apps.shipping.methods import Free
 from oscar.apps.shipping.repository import Repository as CoreRepository
 
+# Dummy shipping methods
+free1 = Free()
+free1.code = 'free1'
+free1.description = 'Ship by van'
+
+free2 = Free()
+free2.code = 'free2'
+free2.description = 'Ship by boat'
 
 class Repository(CoreRepository):
 
+    methods = {
+        free1.code: free1,
+        free2.code: free2
+    }
+
     def get_shipping_methods(self, user, basket, shipping_addr=None, **kwargs):
-        methods = [Free(), Free()]
+        methods = self.methods.values()
         return self.add_basket_to_methods(basket, methods)
+
+    def find_by_code(self, code):
+        return self.methods.get(code, None)
