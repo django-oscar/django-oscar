47658760184664505cc0f1a98fd7d743c80dcaf0
David Winterbottom <david.winterbottom@gmail.com>
2012-05-03 11:10:24 +0100

    Moved alerts messages to top of screen.
    Also wrote a CSS generator script

---
+++ oscar/static/oscar/less/generate_css.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd $(dirname $BASH_SOURCE)
+
+echo "Generating styles.css"
+lessc styles.less > ../css/styles.css
+
+echo "Generating responsive.css"
+lessc responsive.less > ../css/responsive.css
