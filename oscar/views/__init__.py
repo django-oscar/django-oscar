e226bcbf428361e45b9b917e9bb289c8bb38f142
David Winterbottom <david.winterbottom@gmail.com>
2012-05-08 10:56:07 +0100

    Specialist 404/500 handlers now return correct status codes

--- oscar/views/__init__.py
+++ oscar/views/__init__.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def handler404(request):
-    return render(request, '404.html')
+    return render(request, '404.html', status=404)
 
 
 def handler500(request):
-    return render(request, '500.html')
+    return render(request, '500.html', status=500)
