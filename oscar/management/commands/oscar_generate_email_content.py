79da2dbafc521b67a38ec5e5c19049b935fbf6ea
David Winterbottom <david.winterbottom@gmail.com>
2012-05-16 20:58:41 +0100

    Updated email generation command

--- oscar/management/commands/oscar_generate_email_content.py
+++ oscar/management/commands/oscar_generate_email_content.py
@@ -1,13 +1,10 @@
-import sys
-from optparse import make_option
-
 from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import User
+from django.db.models import get_model
 
-from oscar.core.loading import import_module
-import_module('order.models', ['Order'], locals())
-import_module('customer.models', ['CommunicationEventType'], locals())
-import_module('customer.utils', ['Dispatcher'], locals())
+from oscar.core.loading import get_class
+Order = get_model('order', 'Order')
+CommunicationEventType = get_model('customer', 'CommunicationEventType')
+Dispatcher = get_class('customer.utils', 'Dispatcher')
 
 
 class Command(BaseCommand):
@@ -24,4 +21,5 @@ class Command(BaseCommand):
             raise CommandError("No order found with number %s" % args[1])
         
         messages = CommunicationEventType.objects.get_and_render(args[0], {'order': order})
-        print "Subject: %s\nBody:\n\n%s"% (messages['subject'], messages['body'])
+        print "Subject: %s\nBody:\n\n%sBody HTML:\n\n%s"% (
+            messages['subject'], messages['body'], messages['html'])
