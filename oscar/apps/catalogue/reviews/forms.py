077d9a67a32e4af1d1ad5b26751837e7ea2e4da3
David Winterbottom <david.winterbottom@gmail.com>
2012-05-23 12:07:45 +0100

    Added validation of whitespace-only reviews.

--- oscar/apps/catalogue/reviews/forms.py
+++ oscar/apps/catalogue/reviews/forms.py
@@ -2,15 +2,36 @@ from django import forms
 from django.db.models import get_model
 
 
-class SignedInUserProductReviewForm(forms.ModelForm):
+class ValidateReviewMixin(object):
+
+    def clean_title(self):
+        title = self.cleaned_data['title'].strip()
+        if not title:
+            raise forms.ValidationError("This field is required")
+        return title
+
+    def clean_body(self):
+        body = self.cleaned_data['body'].strip()
+        if not body:
+            raise forms.ValidationError("This field is required")
+        return body
+
+
+class SignedInUserProductReviewForm(forms.ModelForm, ValidateReviewMixin):
     class Meta:
         model = get_model('reviews', 'productreview')
         fields = ('title', 'score', 'body')
 
 
-class AnonymousUserProductReviewForm(forms.ModelForm):
+class AnonymousUserProductReviewForm(forms.ModelForm, ValidateReviewMixin):
     name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
+
+    def clean_name(self):
+        name = self.cleaned_data['name'].strip()
+        if not name:
+            raise forms.ValidationError("This field is required")
+        return name
 
     class Meta:
         model = get_model('reviews', 'productreview')
