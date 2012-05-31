67894d8bc9bee978a46336c355b90f3be3b15a52
Eleni Lixourioti <inelegr88@gmail.com>
2012-05-31 18:28:44 +0100

    tiny fix

--- oscar/core/ajax/http.py
+++ oscar/core/ajax/http.py
@@ -3,8 +3,7 @@ import simplejson as json
 
 
 class JsonResponse(HttpResponse):
-    def __init__(self, content='', mimetype='application/javascript', status=None,
-                 content_type=None):
+    def __init__(self, content='', mimetype='application/javascript', status=None, content_type=None):
         self.dict_content = content
         super(JsonResponse, self).__init__(content, mimetype, status, content_type)
 
