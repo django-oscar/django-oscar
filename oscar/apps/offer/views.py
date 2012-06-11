a71b9e16dd8478228dccc7457484de5aa46d2164
Asia Biega <asia.biega@gmail.com>
2012-06-11 11:41:23 +0200

    Do not let non discountable products be displayed on discount product list

--- oscar/apps/offer/views.py
+++ oscar/apps/offer/views.py
@@ -24,5 +24,5 @@ class OfferDetailView(ListView):
     def get_queryset(self):
         range = self.offer.condition.range
         if range.includes_all_products:
-            return Product.browsable.all()
-        return range.included_products.all()
+            return Product.browsable.filter(not_discountable=False)
+        return range.included_products.filter(not_discountable=False)
