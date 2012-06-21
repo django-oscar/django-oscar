d788fb5bc86033e83dc264340002a143381ae688
Asia Biega <asia.biega@gmail.com>
2012-06-21 17:12:26 +0200

    Test for category creation

--- oscar/apps/dashboard/catalogue/tests.py
+++ oscar/apps/dashboard/catalogue/tests.py
@@ -12,11 +12,22 @@ class ViewTests(ClientTestCase):
 
     def test_pages_exist(self):
         urls = [reverse('dashboard:catalogue-product-list'),
+                reverse('dashboard:catalogue-category-list'),
                 reverse('dashboard:stock-alert-list'),
                ]
         for url in urls:
             self.assertIsOk(self.client.get(url))
 
+
+def create_test_category_tree():
+    trail = 'A > B > C'
+    create_from_breadcrumbs(trail)
+    trail = 'A > B > D'
+    create_from_breadcrumbs(trail)
+    trail = 'A > E > F'
+    create_from_breadcrumbs(trail)
+    trail = 'A > E > G'
+    create_from_breadcrumbs(trail)
 
 
 class CategoryFormTests(TestCase):
@@ -25,14 +36,7 @@ class CategoryFormTests(TestCase):
         Category.objects.all().delete()
 
     def test_conflicting_slugs_recognized(self):
-        trail = 'A > B > C'
-        create_from_breadcrumbs(trail)
-        trail = 'A > B > D'
-        create_from_breadcrumbs(trail)
-        trail = 'A > E > F'
-        create_from_breadcrumbs(trail)
-        trail = 'A > E > G'
-        create_from_breadcrumbs(trail)
+        create_test_category_tree()
 
         f = CategoryForm()
 
@@ -72,3 +76,35 @@ class CategoryFormTests(TestCase):
         ref_node_pk = Category.objects.get(name='A').pk
         conflicting = f.is_slug_conflicting('E', ref_node_pk, 'first-child')
         self.assertEqual(conflicting, False)
+
+
+class CategoryTests(ClientTestCase):
+    is_staff = True
+
+    def setUp(self):
+        super(CategoryTests, self).setUp()
+        create_test_category_tree()
+
+    def test_category_create(self):
+        a = Category.objects.get(name='A')
+        b = Category.objects.get(name='B')
+        c = Category.objects.get(name='C')
+
+        #Redirect to subcategory list view
+        response = self.client.post(reverse('dashboard:catalogue-category-create'),
+                                            {'name': 'Testee',
+                                             '_position': 'left',
+                                             '_ref_node_id': c.id,})
+
+        self.assertIsRedirect(response, reverse('dashboard:catalogue-category-detail-list', 
+                                                args=(b.pk,)))
+
+        #Redirect to main category list view
+        response = self.client.post(reverse('dashboard:catalogue-category-create'),
+                                            {'name': 'Testee',
+                                             '_position': 'right',
+                                             '_ref_node_id': a.id,})
+
+        self.assertIsRedirect(response, reverse('dashboard:catalogue-category-list'))
+
+        self.assertEqual(Category.objects.all().count(), 9)
