f164d61282711ec125db4f0a3ffb2ef64670aaa7
David Winterbottom <david.winterbottom@gmail.com>
2012-07-09 11:59:14 +0100

    Fixed release script

--- release.sh
+++ release.sh
@@ -5,12 +5,9 @@ git tag | grep $RELEASE_NUM > /dev/null && \
 	echo "New version number required ($RELEASE_NUM already used)" && exit 1
 
 # Push to PyPi
-:xa
-
-
-
 ./setup.py sdist upload
 
 # Tag in Git
 git tag $RELEASE_NUM -m "Tagging release $RELEASE_NUM"
 git push --tags
+git push 
