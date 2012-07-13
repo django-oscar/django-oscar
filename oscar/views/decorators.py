e1dc815f28fd096eadd3347022997c33ccace5e6
David Winterbottom <david.winterbottom@gmail.com>
2012-07-13 11:15:19 +0100

    Added warning message for staff_required decorator

--- oscar/views/decorators.py
+++ oscar/views/decorators.py
@@ -1,7 +1,9 @@
 import urlparse
-
 from functools import wraps
+
 from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 
 
 def staff_member_required(view_func, login_url='/accounts/login/'):
@@ -13,7 +15,6 @@ def staff_member_required(view_func, login_url='/accounts/login/'):
     This decorator is based on the admin decorator provided by the the django
     ``auth`` and ``admin`` packages.
     """
-
     @wraps(view_func)
     def _checklogin(request, *args, **kwargs):
         if request.user.is_active and request.user.is_staff:
@@ -27,6 +28,8 @@ def staff_member_required(view_func, login_url='/accounts/login/'):
         if ((not login_scheme or login_scheme == current_scheme) and
             (not login_netloc or login_netloc == current_netloc)):
             path = request.get_full_path()
+
+        messages.warning(request, _("You must log in to access this page"))
 
         from django.contrib.auth.views import redirect_to_login
         return redirect_to_login(path, login_url, REDIRECT_FIELD_NAME)
