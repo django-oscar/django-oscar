7dfbcfab71f3f10b1c53eabf368055bb3cfc8056
Sebastian Vetter <sebastian.vetter@tangentsnowball.com.au>
2012-07-26 12:47:29 +1000

    updated render_promotion template tag to provide copy of global context

--- tests/unit/promotion_tests.py
+++ tests/unit/promotion_tests.py
@@ -1,5 +1,10 @@
-from django.test import TestCase
+import mock
 
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.template import Template, TemplateSyntaxError
+
+from oscar.test import ClientTestCase
 from oscar.apps.promotions import models
 
 
@@ -9,3 +14,29 @@ class PromotionTest(TestCase):
         promotion = models.Image.objects.create(name="dummy banner")
         self.assertEqual('promotions/image.html', promotion.template_name())
 
+
+class PromotionTemplateTagTest(ClientTestCase):
+    anonymous = True
+
+    def test_promotion_templatetag_argument_error(self):
+        self.assertRaises(
+            TemplateSyntaxError, Template,
+            '{% load promotion_tags %}{% render_promotion %}',
+        )
+
+    def test_promotion_templatetag(self):
+        response = self.client.get(reverse('catalogue:index'))
+        promotion = models.Image.objects.create(name="dummy banner")
+
+        template = Template(
+            '{% load promotion_tags %}'
+            '{% render_promotion promotion %}'
+        )
+        ctx = response.context[0]
+        ctx.update({
+            "promotion": promotion,
+            "basket": mock.Mock('FakeBasket')
+        })
+        template.render(ctx)
+        self.assertTrue('promotion' in ctx)
+        self.assertTrue('basket' in ctx)
