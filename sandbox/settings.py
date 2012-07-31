f0a9b2851cfa329089a23bd198a43edc16d97ea1
David Winterbottom <david.winterbottom@gmail.com>
2012-07-31 11:57:31 +0100

    Fixed the 'contribute' makefile target.
    This makes it much easier to get started when contributing.  The docs
    are also updated.

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -20,7 +20,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(os.path.dirname(__file__), 'db.sqlite'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
