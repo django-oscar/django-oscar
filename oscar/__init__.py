48fc82692e17e14096cad63a1006da53f3c11733
David Winterbottom <david.winterbottom@gmail.com>
2012-08-13 16:02:00 +0100

    Bump version

--- oscar/__init__.py
+++ oscar/__init__.py
@@ -3,7 +3,7 @@ import os
 # Use 'final' as the 4th element to indicate
 # a full release
 
-VERSION = (0, 3, 1, 'final')
+VERSION = (0, 3, 2, 'final')
 
 def get_short_version():
     return '%s.%s' % (VERSION[0], VERSION[1])
