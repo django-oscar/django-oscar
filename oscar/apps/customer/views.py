50d1e6f2738cc8602b5ab2eece11484885776fe9
David Winterbottom <david.winterbottom@gmail.com>
2012-08-22 13:05:37 +0100

    Replace hard-coded form classes with class attributes
    This allows the view to be subclassed and alternative form classes used.

--- oscar/apps/customer/views.py
+++ oscar/apps/customer/views.py
@@ -35,36 +35,38 @@ class AccountSummaryView(ListView):
     def get_queryset(self):
         """Return a customer's orders"""
         return self.model._default_manager.filter(user=self.request.user)[0:5]
-    
-    
+
+
 class AccountAuthView(TemplateView):
     template_name = 'customer/login_registration.html'
     redirect_field_name = 'next'
     login_prefix = 'login'
     registration_prefix = 'registration'
     communication_type_code = 'REGISTRATION'
-    
+    login_form_class = EmailAuthenticationForm
+    registration_form_class = EmailUserCreationForm
+
     def get_logged_in_redirect(self):
         return reverse('customer:summary')
-    
+
     def get_context_data(self, *args, **kwargs):
         context = super(AccountAuthView, self).get_context_data(*args, **kwargs)
         redirect_to = self.request.REQUEST.get(self.redirect_field_name, '')
         context[self.redirect_field_name] = redirect_to
-        context['login_form'] = EmailAuthenticationForm(prefix=self.login_prefix)
-        context['registration_form'] = EmailUserCreationForm(prefix=self.registration_prefix)        
+        context['login_form'] = self.login_form_class(prefix=self.login_prefix)
+        context['registration_form'] = self.registration_form_class(prefix=self.registration_prefix)
         return context
-    
+
     def check_redirect(self, context):
         redirect_to = context.get(self.redirect_field_name)
-        
+
         netloc = urlparse.urlparse(redirect_to)[1]
         if not redirect_to:
             redirect_to = settings.LOGIN_REDIRECT_URL
         elif netloc and netloc != self.request.get_host():
             redirect_to = settings.LOGIN_REDIRECT_URL
         return redirect_to
-    
+
     def send_registration_email(self, user):
         code = self.communication_type_code
         ctx = {'user': user,
@@ -78,25 +80,25 @@ class AccountAuthView(TemplateView):
             # Create order event
             messages = event_type.get_messages(ctx)
 
-        if messages and messages['body']:      
+        if messages and messages['body']:
             dispatcher = Dispatcher()
             dispatcher.dispatch_user_messages(user, messages)
-    
+
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(*args, **kwargs)
-        
+
         if request.user.is_authenticated():
             return HttpResponseRedirect(self.get_logged_in_redirect())
 
         self.request.session.set_test_cookie()
         return self.render_to_response(context)
-    
+
     def post(self, request, *args, **kwargs):
         context = self.get_context_data(*args, **kwargs)
         redirect_to = self.check_redirect(context)
-        
+
         if u'login_submit' in self.request.POST:
-            login_form = EmailAuthenticationForm(prefix=self.login_prefix, data=request.POST)            
+            login_form = self.login_form_class(prefix=self.login_prefix, data=request.POST)
             if login_form.is_valid():
                 auth_login(request, login_form.get_user())
                 if request.session.test_cookie_worked():
@@ -105,24 +107,24 @@ class AccountAuthView(TemplateView):
             context['login_form'] = login_form
 
         if u'registration_submit' in self.request.POST:
-            registration_form = EmailUserCreationForm(prefix=self.registration_prefix, data=request.POST)
+            registration_form = self.registration_form_class(prefix=self.registration_prefix, data=request.POST)
             context['registration_form'] = registration_form
             if registration_form.is_valid():
                 user = registration_form.save()
-                
+
                 if getattr(settings, 'OSCAR_SEND_REGISTRATION_EMAIL', True):
                     self.send_registration_email(user)
-                
+
                 user = authenticate(username=user.email, password=registration_form.cleaned_data['password1'])
                 auth_login(self.request, user)
                 if self.request.session.test_cookie_worked():
-                    self.request.session.delete_test_cookie()                
+                    self.request.session.delete_test_cookie()
                 return HttpResponseRedirect(redirect_to)
-        
+
         self.request.session.set_test_cookie()
         return self.render_to_response(context)
 
-    
+
 class EmailHistoryView(ListView):
     """Customer email history"""
     context_object_name = "emails"
@@ -138,7 +140,7 @@ class EmailDetailView(DetailView):
     """Customer order details"""
     template_name = "customer/email.html"
     context_object_name = 'email'
-    
+
     def get_object(self):
         """Return an order object or 404"""
         return get_object_or_404(email_model, user=self.request.user, id=self.kwargs['email_id'])
@@ -159,9 +161,9 @@ class OrderHistoryView(ListView):
 class OrderDetailView(DetailView):
     """Customer order details"""
     model = order_model
-    
+
     def get_template_names(self):
-        return ["customer/order.html"]    
+        return ["customer/order.html"]
 
     def get_object(self):
         return get_object_or_404(self.model, user=self.request.user, number=self.kwargs['order_number'])
@@ -169,29 +171,29 @@ class OrderDetailView(DetailView):
 
 class OrderLineView(DetailView, PostActionMixin):
     """Customer order line"""
-    
+
     def get_object(self):
         """Return an order object or 404"""
         order = get_object_or_404(order_model, user=self.request.user, number=self.kwargs['order_number'])
         return order.lines.get(id=self.kwargs['line_id'])
-    
+
     def do_reorder(self, line):
         if not line.product:
             messages.info(self.request, _("This product is no longer available for re-order"))
             return
-        
+
         # We need to pass response to the get_or_create... method
         # as a new basket might need to be created
         self.response = HttpResponseRedirect(reverse('basket:summary'))
         basket = self.request.basket
-        
+
         # Convert line attributes into basket options
         options = []
         for attribute in line.attributes.all():
             if attribute.option:
                 options.append({'option': attribute.option, 'value': attribute.value})
         basket.add_product(line.product, 1, options)
-        messages.info(self.request, "Line reordered")  
+        messages.info(self.request, "Line reordered")
 
 
 class AddressListView(ListView):
@@ -199,7 +201,7 @@ class AddressListView(ListView):
     context_object_name = "addresses"
     template_name = 'customer/address-book.html'
     paginate_by = 40
-        
+
     def get_queryset(self):
         """Return a customer's addresses"""
         return user_address_model._default_manager.filter(user=self.request.user)
@@ -208,7 +210,7 @@ class AddressListView(ListView):
 class AddressCreateView(CreateView):
     form_class = UserAddressForm
     mode = user_address_model
-    
+
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.user = self.request.user
@@ -225,14 +227,14 @@ class AddressCreateView(CreateView):
 class AddressUpdateView(UpdateView):
     form_class = UserAddressForm
     model = user_address_model
-    
+
     def get_queryset(self):
         """Return a customer's addresses"""
-        return user_address_model._default_manager.filter(user=self.request.user)    
+        return user_address_model._default_manager.filter(user=self.request.user)
 
     def get_template_names(self):
         return ["customer/address-form.html"]
-    
+
     def get_success_url(self):
         return reverse('customer:address-detail', kwargs={'pk': self.get_object().pk })
 
@@ -242,10 +244,10 @@ class AddressDeleteView(DeleteView):
 
     def get_queryset(self):
         """Return a customer's addresses"""
-        return user_address_model._default_manager.filter(user=self.request.user)        
+        return user_address_model._default_manager.filter(user=self.request.user)
 
     def get_success_url(self):
-        return reverse('customer:address-list')    
-    
+        return reverse('customer:address-list')
+
     def get_template_names(self):
         return ["customer/address-delete.html"]
