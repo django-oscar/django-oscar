01a90bbf8107e5c46ce48718cac195c5b48e1887
Sebastian Vetter <sebastian.vetter@tangentsnowball.com.au>
2012-08-30 15:37:27 +1000

    adds a new management command for sending email notifications
    A new management command is added that checks all products for
    active notifications and sends out notification emails for products
    that are available to buy.
    To keep the implementation DRY, a new ``utils`` module is added to the
    notification app that provides the function used to send out emails
    and deactivate notifications that have been processed.
    
    In addition, a new setting is added to Oscar that allows for
    disabling the default setup to send notifications on stock record
    changes. If this setting is disabled, the only way to send out
    notifications is to run the ``oscar_send_notifications`` management
    command as a cronjob or something similar.

--- tests/unit/notification_command_tests.py
+++ tests/unit/notification_command_tests.py
@@ -2,12 +2,12 @@ from datetime import datetime, timedelta
 from django.test import TestCase
 from django.core.management import call_command
 
-from django_dynamic_fixture import get
+from django_dynamic_fixture import get as G
 
 from oscar.apps.catalogue.notification.models import ProductNotification
 
 
-class CleanNotificationsTests(TestCase):
+class TestCleanNotifications(TestCase):
 
     def setUp(self):
         self.date_now = datetime.now()
@@ -29,7 +29,7 @@ class CleanNotificationsTests(TestCase):
         ]
 
         for status, date in status_date_mapping:
-            notification = get(ProductNotification, status=status)
+            notification = G(ProductNotification, status=status)
             notification.date_created = date
             notification.save()
 
@@ -73,3 +73,58 @@ class CleanNotificationsTests(TestCase):
                      *[], **{'days': 1, 'hours': '2'})
 
         self.assertEquals(ProductNotification.objects.count(), 5)
+
+from django.core import mail
+from oscar.apps.catalogue.models import Product
+from oscar.apps.partner.models import StockRecord, Partner
+
+class TestSendNotificationsCommand(TestCase):
+
+    def setUp(self):
+        mail.outbox = []
+        partner = Partner.objects.create(name="Partner")
+
+        self.product1 = G(Product)
+        G(StockRecord, product=self.product1, partner=partner, num_in_stock=50)
+
+        self.product2 = G(Product)
+        G(StockRecord, product=self.product2, partner=partner, num_in_stock=0)
+
+        self.product3 = G(Product)
+        G(StockRecord, product=self.product3, partner=partner, num_in_stock=1)
+
+    def test_sending_notifications_for_products_back_in_stock(self):
+        active_notif1 = G(ProductNotification, status=ProductNotification.ACTIVE,
+                         product=self.product1, date_notified=None)
+        active_notif2 = G(ProductNotification, status=ProductNotification.ACTIVE,
+                         product=self.product1, date_notified=None)
+        active_notif3 = G(ProductNotification, status=ProductNotification.ACTIVE,
+                         product=self.product2, date_notified=None)
+        active_notif4 = G(ProductNotification, status=ProductNotification.ACTIVE,
+                         product=self.product3, date_notified=None)
+
+        inactive_notif1 = G(ProductNotification, status=ProductNotification.INACTIVE,
+                         product=self.product1, date_notified=None)
+        inactive_notif2 = G(ProductNotification, status=ProductNotification.INACTIVE,
+                         product=self.product2, date_notified=None)
+
+        self.assertEquals(mail.outbox, [])
+
+        call_command('oscar_send_notifications')
+
+        for pk in [active_notif1.pk, active_notif2.pk, active_notif4.pk]:
+            notif = ProductNotification.objects.get(pk=pk)
+            self.assertEquals(notif.status, ProductNotification.INACTIVE)
+            self.assertNotEqual(notif.date_notified, None) 
+
+        notif = ProductNotification.objects.get(pk=active_notif3.pk)
+        self.assertEquals(notif.status, ProductNotification.ACTIVE)
+        self.assertEquals(notif.date_notified, None)
+
+        self.assertEquals(len(mail.outbox), 3) 
+
+        for pk in [inactive_notif1.pk, inactive_notif2.pk]:
+            notif = ProductNotification.objects.get(pk=pk)
+            self.assertEquals(notif.status, ProductNotification.INACTIVE)
+            self.assertEquals(notif.date_notified, None)
+
