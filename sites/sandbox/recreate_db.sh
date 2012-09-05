46c691326fe448a687fa98de5ca6bfd63d7fc590
Sebastian Vetter <sebastian.vetter@tangentsnowball.com.au>
2012-09-05 11:45:49 +1000

    the path to fixtures is now correct after moving the sandbox

--- sites/sandbox/recreate_db.sh
+++ sites/sandbox/recreate_db.sh
@@ -10,10 +10,10 @@ echo "Loading fixtures"
 ./manage.py loaddata countries.json
 
 echo "Importing products"
-./manage.py oscar_import_catalogue data/books-catalogue.csv
+./manage.py oscar_import_catalogue ../_fixtures/books-catalogue.csv
 
 echo "Importing Images"
-./manage.py oscar_import_catalogue_images data/books-images.tar.gz
+./manage.py oscar_import_catalogue_images ../_fixtures/books-images.tar.gz
 
 echo "Creating superuser"
 ./manage.py createsuperuser --username=admin --email=admin@test.com
