d8e026010f5903d987403f55a4853b208b8f8cf4
David Winterbottom <david.winterbottom@gmail.com>
2012-09-14 09:22:51 +0100

    Adjust generic bulkedit mixin to work with public-facing views

--- oscar/views/generic.py
+++ oscar/views/generic.py
@@ -7,8 +7,8 @@ from django.utils.translation import ugettext_lazy as _
 class PostActionMixin(object):
     """
     Simple mixin to forward POST request that contain a key 'action'
-    onto a method of form "do_{action}".  
-    
+    onto a method of form "do_{action}".
+
     This only works with DetailView
     """
     def post(self, request, *args, **kwargs):
@@ -23,7 +23,7 @@ class PostActionMixin(object):
             else:
                 messages.error(request, _("Invalid form submission"))
         return super(PostActionMixin, self).post(request, *args, **kwargs)
-    
+
 
 class BulkEditMixin(object):
     """
@@ -32,18 +32,23 @@ class BulkEditMixin(object):
     of rows to be selected and then some 'action' to be performed on them.
     """
     action_param = 'action'
+
+    # Permitted methods that can be used to act on the selected objects
     actions = None
     current_view = None
     checkbox_object_name = None
 
     def get_checkbox_object_name(self):
-        object_list = self.get_queryset()
         if self.checkbox_object_name:
             return self.checkbox_object_name
-        elif hasattr(object_list, 'model'):
+        object_list = self.get_queryset()
+        if hasattr(object_list, 'model'):
             return smart_str(object_list.model._meta.object_name.lower())
         else:
             return None
+
+    def get_queryset(self):
+        pass
 
     def get_error_url(self, request):
         return request.META['HTTP_REFERER']
@@ -60,12 +65,21 @@ class BulkEditMixin(object):
             messages.error(self.request, _("Invalid action"))
             return HttpResponseRedirect(self.get_error_url(request))
 
-        ids = request.POST.getlist('selected_%s' % self.get_checkbox_object_name())
+        ids = request.POST.getlist(
+            'selected_%s' % self.get_checkbox_object_name())
+        ids = map(int, ids)
         if not ids:
-            messages.error(self.request, _("You need to select some %ss") % self.get_checkbox_object_name())
+            messages.error(
+                self.request, _("You need to select some %ss") % self.get_checkbox_object_name())
             return HttpResponseRedirect(self.get_error_url(request))
 
-        raw_objects = self.model.objects.in_bulk(ids)
-        objects = [raw_objects[int(id)] for id in ids]
+        objects = self.get_objects(ids)
         return getattr(self, action)(request, objects)
 
+    def get_objects(self, ids):
+        object_dict = self.get_object_dict(ids)
+        # Rearrange back into the original order
+        return [object_dict[id] for id in ids]
+
+    def get_object_dict(self, ids):
+        return self.model.objects.in_bulk(ids)
