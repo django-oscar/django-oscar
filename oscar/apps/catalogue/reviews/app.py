a6e2612208a2ef4edc11a45ca9ff6e353311d94f
Sebastian Vetter <sebastian.vetter@tangentsnowball.com.au>
2012-09-28 10:49:59 +1000

    Make the review app conform with PEP8

--- oscar/apps/catalogue/reviews/app.py
+++ oscar/apps/catalogue/reviews/app.py
@@ -1,20 +1,22 @@
 from oscar.core.application import Application
 from django.conf.urls import patterns, url
-from oscar.apps.catalogue.reviews.views import ProductReviewDetail, CreateProductReview, CreateProductReviewComplete, ProductReviewList
+from oscar.apps.catalogue.reviews import views
 
 
 class ProductReviewsApplication(Application):
     name = None
-    detail_view = ProductReviewDetail
-    create_view = CreateProductReview
-    create_complete_view = CreateProductReviewComplete
-    list_view = ProductReviewList
+    detail_view = views.ProductReviewDetail
+    create_view = views.CreateProductReview
+    create_complete_view = views.CreateProductReviewComplete
+    list_view = views.ProductReviewList
 
     def get_urls(self):
         urlpatterns = patterns('',
-            url(r'^(?P<pk>\d+)/$', self.detail_view.as_view(), name='reviews-detail'),
+            url(r'^(?P<pk>\d+)/$', self.detail_view.as_view(),
+                name='reviews-detail'),
             url(r'^add/$', self.create_view.as_view(), name='reviews-add'),
-            url(r'^add/complete/$', self.create_complete_view.as_view(), name='reviews-add-complete'),
+            url(r'^add/complete/$', self.create_complete_view.as_view(),
+                name='reviews-add-complete'),
             url(r'^$', self.list_view.as_view(), name='reviews-list'),
         )
         return urlpatterns
