1866718386d6741d08bd66c2173093d6fbdb0ac8
Oliver Randell <oliverrandell@gmail.com>
2012-10-02 16:50:40 +1000

    Addition of files needed to make the image upload work within the product area within the dashboard

---
+++ sites/sandbox/settings_simple.py
@@ -0,0 +1,285 @@
+import os
+
+# Django settings for oscar project.
+PROJECT_DIR = os.path.dirname(__file__)
+location = lambda x: os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), x)
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+SQL_DEBUG = True
+SEND_BROKEN_LINK_EMAILS = True
+
+ADMINS = (
+    ('David Winterbottom', 'david.winterbottom@tangentlabs.co.uk'),
+)
+EMAIL_SUBJECT_PREFIX = '[Oscar sandbox] '
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), 'db.sqlite'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND':
+        'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/London'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = (
+    ('de', 'German'),
+    ('fr', 'French'),
+)
+ROSETTA_STORAGE_CLASS = 'rosetta.storage.SessionRosettaStorage'
+ROSETTA_ENABLE_TRANSLATION_SUGGESTIONS = True
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = location("assets/media")
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+#ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ()
+STATIC_ROOT = location('public/static')
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '$)a7n&o80u!6y5t-+jrd3)3!%vh&shg$wqpjpxc!ar&p#!)n1a'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.request",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    # Oscar specific
+    'oscar.apps.search.context_processors.search_form',
+    'oscar.apps.promotions.context_processors.promotions',
+    'oscar.apps.checkout.context_processors.checkout',
+    'oscar.core.context_processors.metadata',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'oscar.apps.basket.middleware.BasketMiddleware',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+ROOT_URLCONF = 'urls'
+
+from oscar import OSCAR_MAIN_TEMPLATE_DIR
+TEMPLATE_DIRS = (
+    location('templates'),
+    OSCAR_MAIN_TEMPLATE_DIR,
+)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s',
+        },
+        'simple': {
+            'format': '[%(asctime)s] %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'checkout_file': {
+             'level': 'INFO',
+             'class': 'oscar.core.logging.handlers.EnvFileHandler',
+             'filename': 'checkout.log',
+             'formatter': 'verbose'
+        },
+        'gateway_file': {
+             'level': 'INFO',
+             'class': 'oscar.core.logging.handlers.EnvFileHandler',
+             'filename': 'gateway.log',
+             'formatter': 'simple'
+        },
+        'error_file': {
+             'level': 'INFO',
+             'class': 'oscar.core.logging.handlers.EnvFileHandler',
+             'filename': 'errors.log',
+             'formatter': 'verbose'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['null'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['mail_admins', 'error_file'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'oscar.checkout': {
+            'handlers': ['console', 'checkout_file'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'gateway': {
+            'handlers': ['gateway_file'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'django.db.backends': {
+            'handlers': ['null'],
+            'propagate': False,
+            'level': 'DEBUG',
+        },
+    }
+}
+
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.flatpages',
+    'django.contrib.staticfiles',
+    'django_extensions',
+    'debug_toolbar',
+    'south',
+    'rosetta',  # For i18n testing
+    'apps.user',  # For profile testing
+    'apps.gateway',  # For allowing dashboard access
+]
+from oscar import get_core_apps
+INSTALLED_APPS = INSTALLED_APPS + get_core_apps()
+
+AUTHENTICATION_BACKENDS = (
+    'oscar.apps.customer.auth_backends.Emailbackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+LOGIN_REDIRECT_URL = '/accounts/'
+APPEND_SLASH = True
+
+# Haystack settings
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+    },
+}
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
+
+AUTH_PROFILE_MODULE = 'user.Profile'
+
+# Oscar settings
+from oscar.defaults import *
+
+
+OSCAR_RECENTLY_VIEWED_PRODUCTS = 20
+OSCAR_ALLOW_ANON_CHECKOUT = True
+OSCAR_INITIAL_ORDER_STATUS = 'Pending'
+OSCAR_INITIAL_LINE_STATUS = 'Pending'
+OSCAR_ORDER_STATUS_PIPELINE = {
+    'Pending': ('Being processed', 'Cancelled',),
+    'Being processed': ('Processed', 'Cancelled',),
+    'Cancelled': (),
+}
+
+OSCAR_SHOP_NAME = 'Oscar Sandbox'
+OSCAR_SHOP_TAGLINE = 'e-Commerce for Django'
+
+GOOGLE_ANALYTICS_ID = 'UA-XXXXX-Y'
+
+LOG_ROOT = location('logs')
+DISPLAY_VERSION = False
+
+THUMBNAIL_DEBUG = True
+
+# Must be within MEDIA_ROOT for sorl to work
+OSCAR_MISSING_IMAGE_URL = 'image_not_found.jpg'
+
+try:
+    from settings_local import *
+except ImportError:
+    pass
