6f2401b1a3f14804ea64f0566110cb9a6ee903b0
David Winterbottom <david.winterbottom@gmail.com>
2012-10-17 14:10:32 +0100

    Fix bug with history helper templatetag
    Symptom:
    Adding to basket did not work
    
    Cause:
    Was not passing the request variable as part of the context and so the
    basket form could not be generated.  Hence adding to basket led to an
    error.
    
    Solution:
    Add request to the context returned by the helper function.

--- oscar/templatetags/history_tags.py
+++ oscar/templatetags/history_tags.py
@@ -2,6 +2,7 @@ from django import template
 from django.db.models import get_model
 
 from oscar.core.loading import get_class
+
 Product = get_model('catalogue', 'Product')
 get_recently_viewed_product_ids = get_class('customer.history_helpers',
                                             'get_recently_viewed_product_ids')
@@ -12,21 +13,19 @@ register = template.Library()
 @register.inclusion_tag('customer/history/recently_viewed_products.html',
                         takes_context=True)
 def recently_viewed_products(context):
-    u"""
+    """
     Inclusion tag listing the most recently viewed products
     """
     request = context['request']
     product_ids = get_recently_viewed_product_ids(request)
 
-    try:
-        current_product = context.get('product', None)
+    current_product = context.get('product', None)
+    if current_product.id in product_ids:
         product_ids.remove(current_product.id)
-    except (ValueError, AttributeError):
-        pass
-
-    product_dict = Product.browsable.in_bulk(product_ids)
 
     # Reordering as the id order gets messed up in the query
+    product_dict = Product.browsable.in_bulk(product_ids)
     product_ids.reverse()
     products = [product_dict[id] for id in product_ids if id in product_dict]
-    return {'products': products}
+    return {'products': products,
+            'request': request}
