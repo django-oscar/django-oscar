cb58904614543a4c80e7434afeb16e4c75a9a175
David Winterbottom <david.winterbottom@gmail.com>
2012-11-09 12:08:31 +0000

    Offers can now give % discounts off shipping
    This required:
    
    * Adding a new benefit type.
    * Reworking the way shipping methods are looked up by code to ensure
      they are 'primed' correctly.  Ie, they pick up the discount wrapper.
    * Reworking the shipping method repository to use two 'prime_' methods
      to post-process the method instances.

--- oscar/apps/shipping/repository.py
+++ oscar/apps/shipping/repository.py
@@ -2,7 +2,8 @@ from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import ugettext_lazy as _
 
 
-from oscar.apps.shipping.methods import Free, NoShippingRequired
+from oscar.apps.shipping.methods import (
+    Free, NoShippingRequired, PercentageDiscount)
 
 
 class Repository(object):
@@ -21,25 +22,46 @@ class Repository(object):
         and overriding this method.
         """
         methods = [Free()]
-        return self.add_basket_to_methods(basket, methods)
+        return self.prime_methods(basket, methods)
 
-    def get_default_shipping_method(self, user, basket, shipping_addr=None, **kwargs):
-        methods = self.get_shipping_methods(user, basket, shipping_addr, **kwargs)
+    def get_default_shipping_method(self, user, basket, shipping_addr=None,
+                                    **kwargs):
+        """
+        Return a 'default' shipping method to show on the basket page to give
+        the customer an indication of what their order will cost.
+        """
+        methods = self.get_shipping_methods(
+            user, basket, shipping_addr, **kwargs)
         if len(methods) == 0:
-            raise ImproperlyConfigured(_("You need to define some shipping methods"))
+            raise ImproperlyConfigured(
+                _("You need to define some shipping methods"))
         return min(methods, key=lambda method: method.basket_charge_incl_tax())
 
-    def add_basket_to_methods(self, basket, methods):
-        for method in methods:
-            method.set_basket(basket)
-        return methods
+    def prime_methods(self, basket, methods):
+        """
+        Prime a list of shipping method instances
 
-    def find_by_code(self, code):
+        This involves injecting the basket instance into each and adding any
+        discount wrappers.
+        """
+        return [self.prime_method(basket, method) for
+                method in methods]
+
+    def prime_method(self, basket, method):
+        """
+        Prime an individual method instance
+        """
+        method.set_basket(basket)
+        if hasattr(basket, 'shipping_discount'):
+            return PercentageDiscount(method, basket.shipping_discount)
+        return method
+
+    def find_by_code(self, code, basket):
         """
         Return the appropriate Method object for the given code
         """
         known_methods = [Free, NoShippingRequired]
         for klass in known_methods:
             if code == getattr(klass, 'code'):
-                return klass()
+                return self.prime_method(basket, klass())
         return None
