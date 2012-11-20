5ee2541bc7d898ec903036eddfd7d7e028afb7ed
David Winterbottom <david.winterbottom@gmail.com>
2012-11-20 18:01:05 +0000

    Add Postgres support to Vagrant machine

--- sites/puppet/manifests/site.pp
+++ sites/puppet/manifests/site.pp
@@ -3,11 +3,48 @@ Exec {
 	path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 }
 
+class python_dependencies {
+	exec {"update-packages":
+		command => "apt-get update",
+	}
+    $packages = [
+		"build-essential",
+		"python-setuptools",
+		"python-imaging",
+		"python-memcache",
+		"postgresql-server-dev-9.1",
+    ]
+    package {
+        $packages: ensure => installed,
+		require => Exec["update-packages"]
+    }
+}
+
 node precise64 {
+
+	include python_dependencies
 	include userconfig
 
 	# Memcached
 	class {"memcached": max_memory => 64 }
+
+	# Postgres
+	$user = "oscar_user"
+	$password = "oscar_password"
+	$database_name = "oscar_vagrant"
+	class {"postgresql::server": }
+	pg_user {$user:
+		ensure => present,
+		password => $password,
+		createdb => true,
+		require => Class["postgresql::server"],
+	}
+	pg_database {$database_name:
+		ensure => present,
+		owner => $user,
+		require => [Pg_user[$user]],
+		encoding => "UTF8",
+	}
 
 	# Python
 	# - set-up a virtualenv
@@ -20,7 +57,7 @@ node precise64 {
 	    requirements => "/vagrant/requirements.txt"
 	}
     exec {"install-oscar":
-	    command => "cd /vagrant && $virtualenv/bin/python setup.py develop",
+	    command => "$virtualenv/bin/python setup.py develop",
 		require => Python::Venv::Isolate[$virtualenv]
 	}
 }
