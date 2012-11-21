b710ed57c99cfef943b22a2c30230030c1c05fc3
David Winterbottom <david.winterbottom@gmail.com>
2012-11-21 10:29:30 +0000

    Add helper script for testing migrations against MySQL and Postgres
    It recreates the databases and runs all migrations from scratch.

---
+++ sites/sandbox/test_migrations.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# MySQL
+mysql -u root --password=root_password -e "DROP DATABASE IF EXISTS oscar_vagrant; CREATE DATABASE oscar_vagrant"
+./manage.py syncdb --noinput --settings=settings_mysql > /dev/null
+./manage.py migrate --noinput --settings=settings_mysql
+
+# Postgres
+sudo -u postgres psql -c "DROP DATABASE IF EXISTS oscar_vagrant"
+sudo -u postgres psql -c "CREATE DATABASE oscar_vagrant ENCODING UTF8"
+./manage.py syncdb --noinput --settings=settings_postgres > /dev/null
+./manage.py migrate --noinput --settings=settings_postgres
