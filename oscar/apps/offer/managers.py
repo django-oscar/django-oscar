6ece85c530d9856a7db650729e57a9f2a92dfa4b
David Winterbottom <david.winterbottom@gmail.com>
2012-12-03 14:18:20 +0000

    Fix queryset filtering for active offers

--- oscar/apps/offer/managers.py
+++ oscar/apps/offer/managers.py
@@ -5,9 +5,9 @@ from django.db import models
 
 class ActiveOfferManager(models.Manager):
     """
-    For searching/creating ACTIVE offers only.
+    For searching/creating offers within their date range
     """
     def get_query_set(self):
         today = datetime.date.today()
         return super(ActiveOfferManager, self).get_query_set().filter(
-            start_date__lte=today, end_date__gt=today)
+            start_date__lte=today, end_date__gte=today)
