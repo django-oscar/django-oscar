a41f2d31c3a4518ce103296b51a4a2de2232022e
David Winterbottom <david.winterbottom@gmail.com>
2012-12-10 11:12:04 +0000

    Download orders that use an offer in a CSV file
    An extension to the offer detail page.  Reminds me how much the
    reporting classes need reworking.  Some day soon perhaps...

---
+++ oscar/apps/dashboard/offers/reports.py
@@ -0,0 +1,27 @@
+from django.utils.translation import ugettext_lazy as _
+
+from oscar.core.loading import get_class
+ReportCSVFormatter = get_class(
+    'dashboard.reports.reports', 'ReportCSVFormatter')
+
+
+class OrderDiscountCSVFormatter(ReportCSVFormatter):
+    filename_template = 'order-discounts-for-offer-%s.csv'
+
+    def generate_csv(self, response, order_discounts):
+        writer = self.get_csv_writer(response)
+        header_row = [_('Order number'),
+                      _('Order date'),
+                      _('Order total'),
+                      _('Cost')]
+        writer.writerow(header_row)
+        for order_discount in order_discounts:
+            order = order_discount.order
+            row = [order.number,
+                   self.format_datetime(order.date_placed),
+                   order.total_incl_tax,
+                   order_discount.amount]
+            writer.writerow(row)
+
+    def filename(self, offer):
+        return self.filename_template % offer.id
