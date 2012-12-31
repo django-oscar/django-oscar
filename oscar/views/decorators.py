be2824019cda98a1f90cda55c1656fb44f127d80
David Winterbottom <david.winterbottom@gmail.com>
2012-12-31 12:14:20 +0000

    Extend staff_member_required to raise 403 on non-staff users
    Related to #437

--- oscar/views/decorators.py
+++ oscar/views/decorators.py
@@ -2,19 +2,23 @@ import urlparse
 from functools import wraps
 
 from django.contrib import messages
-from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.views import redirect_to_login
+from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse_lazy
 from django.utils.translation import ugettext_lazy as _
 
 
 def staff_member_required(view_func, login_url=None):
     """
-    Decorator for views that checks that the user is logged in and is a staff
-    member. The user is redirected to the login page specified by *login_url*
-    if not authenticated.
+    Ensure that the user is a logged-in staff member.
 
-    This decorator is based on the admin decorator provided by the the django
-    ``auth`` and ``admin`` packages.
+    * If not authenticated, redirect to a specified login URL.
+    * If not staff, show a 403 page
+
+    This decorator is based on the decorator with the same name from
+    django.contrib.admin.view.decorators.  This one is superior as it allows a
+    redirect URL to be specified.
     """
     if login_url is None:
         login_url = reverse_lazy('customer:login')
@@ -24,19 +28,21 @@ def staff_member_required(view_func, login_url=None):
         if request.user.is_active and request.user.is_staff:
             return view_func(request, *args, **kwargs)
 
-        # If the login url is the same scheme and net location then just
-        # use the path as the "next" url.
-        path = request.build_absolute_uri()
+        # If user is not logged in, redirect to login page
+        if not request.user.is_authenticated():
+            # If the login url is the same scheme and net location then just
+            # use the path as the "next" url.
+            path = request.build_absolute_uri()
+            login_scheme, login_netloc = urlparse.urlparse(login_url)[:2]
+            current_scheme, current_netloc = urlparse.urlparse(path)[:2]
+            if ((not login_scheme or login_scheme == current_scheme) and
+                (not login_netloc or login_netloc == current_netloc)):
+                path = request.get_full_path()
 
-        login_scheme, login_netloc = urlparse.urlparse(login_url)[:2]
-        current_scheme, current_netloc = urlparse.urlparse(path)[:2]
-        if ((not login_scheme or login_scheme == current_scheme) and
-            (not login_netloc or login_netloc == current_netloc)):
-            path = request.get_full_path()
-
-        messages.warning(request, _("You must log in to access this page"))
-
-        from django.contrib.auth.views import redirect_to_login
-        return redirect_to_login(path, login_url, REDIRECT_FIELD_NAME)
+            messages.warning(request, _("You must log in to access this page"))
+            return redirect_to_login(path, login_url, REDIRECT_FIELD_NAME)
+        else:
+            # User does not have permission to view this page
+            raise PermissionDenied
 
     return _checklogin
