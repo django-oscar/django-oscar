dd1d1713be871f01da981d26a7eb6b5a9f417d72
Maik Hoepfel <m@maikhoepfel.de>
2013-01-11 15:21:04 +0000

    Fix for Django's dropped verify_exists functionality
    Please note that ExtendedURLFields's verify_exist does not
    default to True anymore.

--- oscar/forms/fields.py
+++ oscar/forms/fields.py
@@ -9,18 +9,25 @@ class ExtendedURLField(fields.URLField):
     validating local relative URLs, ie. '/product/'
     """
 
-    def __init__(self, max_length=None, min_length=None, verify_exists=False,
-            *args, **kwargs):
-        # intentionally skip one step when calling super()
-        super(fields.URLField, self).__init__(max_length, min_length, *args,
-                                              **kwargs)
-        validator = validators.ExtendedURLValidator(
-            verify_exists=verify_exists)
+    def __init__(self, max_length=None, min_length=None, verify_exists=None,
+                 *args, **kwargs):
+        # We don't pass on verify_exists as it has been deprecated in Django
+        # 1.4
+        super(fields.URLField, self).__init__(
+            max_length, min_length, *args, **kwargs)
+        # Even though it is deprecated, we still pass on 'verify_exists' as
+        # Oscar's ExtendedURLValidator uses it to determine whether to test
+        # local URLs.
+        if verify_exists is not None:
+            validator = validators.ExtendedURLValidator(
+                verify_exists=verify_exists)
+        else:
+            validator = validators.ExtendedURLValidator()
         self.validators.append(validator)
 
     def to_python(self, value):
         # We need to avoid having 'http' inserted at the start of
-        # every value
+        # every value so that local URLs are valid.
         if value and value.startswith('/'):
             return value
         return super(ExtendedURLField, self).to_python(value)
