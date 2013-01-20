386c803e6f8c13238c399a3095dec9dadf08b29c
Sebastian Vetter <sebastian@roadside-developer.com>
2013-01-20 23:36:50 +1100

    Switch to less 1.3.3

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(name='django-oscar',
           'Babel>=0.9,<0.10',
           'django-compressor>=1.2,<1.3',
           'virtual-node>=0.0.1',
-          'virtual-less>=0.0.1a'],
+          'virtual-less>=0.0.1-1.3.3'],
       dependency_links=['https://github.com/toastdriven/django-haystack/tarball/0e95d8696f8ba770f9c60152136aba32f5591fd6#egg=django-haystack-2.0.0-beta'],
       # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
