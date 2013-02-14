8e02c1e48717f9836d7aaec55ed7cc4ed6139c31
David Winterbottom <david.winterbottom@gmail.com>
2013-02-14 10:48:25 +0000

    Adjust email auth backend to allow emails to contain capitals
    Before it was lower-casing everything first which prevented users being
    able to login if their email address contained capitals.  We now only
    lower case the host part as this is case-insensitive.
    
    Fixes #505

--- oscar/apps/customer/auth_backends.py
+++ oscar/apps/customer/auth_backends.py
@@ -3,14 +3,19 @@ from django.contrib.auth.backends import ModelBackend
 
 
 class Emailbackend(ModelBackend):
+
     def authenticate(self, email=None, password=None, *args, **kwargs):
         if email is None:
             if not 'username' in kwargs or kwargs['username'] is None:
                 return None
             email = kwargs['username']
-        email = email.lower()
+
+        # We lowercase the host part as this is what Django does when saving a
+        # user
+        local, host = email.split('@')
+        clean_email = local + '@' + host.lower()
         try:
-            user = User.objects.get(email=email)
+            user = User.objects.get(email=clean_email)
         except User.DoesNotExist:
             return None
         if user.check_password(password):
