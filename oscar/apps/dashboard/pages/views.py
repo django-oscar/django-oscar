cab334bb2dce365f065e9e88f547b6c020b84a9c
David Winterbottom <david.winterbottom@gmail.com>
2013-02-18 12:20:14 +0000

    Tidy pages dashboard templates

--- oscar/apps/dashboard/pages/views.py
+++ oscar/apps/dashboard/pages/views.py
@@ -1,12 +1,13 @@
-from django.db.models.loading import get_model
-from django.template.defaultfilters import slugify
-from django.http import HttpResponseRedirect
-from django.utils.translation import ugettext_lazy as _
+from django.contrib import messages
 from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
+from django.db.models.loading import get_model
+from django.http import HttpResponseRedirect
+from django.template.defaultfilters import slugify
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
 from django.views import generic
 from django.views.generic import ListView
-from django.contrib import messages
 
 from oscar.core.validators import URLDoesNotExistValidator
 from oscar.apps.dashboard.pages import forms
@@ -62,18 +63,12 @@ class PageListView(ListView):
 
 
 class PageCreateView(generic.CreateView):
-    """
-    View for creating a flatpage from dashboard.
-    """
     template_name = 'dashboard/pages/update.html'
     model = FlatPage
     form_class = forms.PageUpdateForm
     context_object_name = 'page'
 
     def get_context_data(self, **kwargs):
-        """
-        Get context data with additional *title* object.
-        """
         ctx = super(PageCreateView, self).get_context_data(**kwargs)
         ctx['title'] = _('Create New Page')
         return ctx
@@ -94,22 +89,23 @@ class PageCreateView(generic.CreateView):
 
         try:
             URLDoesNotExistValidator()(page.url)
-
+        except ValidationError:
+            pass
+        else:
             # use current site as default for new page
             page.save()
             page.sites.add(Site.objects.get_current())
 
             return HttpResponseRedirect(self.get_success_url(page))
 
-        except ValidationError:
-            pass
-
         ctx = self.get_context_data()
         ctx['form'] = form
         return self.render_to_response(ctx)
 
     def get_success_url(self, page):
-        messages.success(self.request, _("Created new page '%s'") % page.title)
+        msg = render_to_string('oscar/dashboard/pages/messages/saved.html',
+                               {'page': page})
+        messages.success(self.request, msg, extra_tags='safe noicon')
         return reverse('dashboard:page-list')
 
 
@@ -123,45 +119,30 @@ class PageUpdateView(generic.UpdateView):
     context_object_name = 'page'
 
     def get_context_data(self, **kwargs):
-        """
-        Get context data with additional *title* and *page* objects attached.
-        """
         ctx = super(PageUpdateView, self).get_context_data(**kwargs)
-        ctx['title'] = _('Update Page')
+        ctx['title'] = self.object.title
         return ctx
 
     def form_valid(self, form):
-        """
-        Store updated flatpage from form data. Checks wether a site
-        is specified for the flatpage or sets the current site by
-        default.
-        """
+        # Ensure saved page is added to the current site
         page = form.save(commit=False)
-
         if not page.sites.count():
             page.sites.add(Site.objects.get_current())
         page.save()
         return HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self):
-        """
-        Get URL to redirect to when updating page was successful.
-        """
-        messages.success(self.request, _("Updated page '%s'") % self.object.title)
+        msg = render_to_string('oscar/dashboard/pages/messages/saved.html',
+                               {'page': self.object})
+        messages.success(self.request, msg, extra_tags='safe noicon')
         return reverse('dashboard:page-list')
 
 
 class PageDeleteView(generic.DeleteView):
-    """
-    View for deleting flatpages from the dashboard. It performs an
-    'are you sure?' check before actually deleting the page.
-    """
     template_name = 'dashboard/pages/delete.html'
     model = FlatPage
 
     def get_success_url(self):
-        """
-        Get URL to redirect to when deleting page is succesful.
-        """
-        messages.success(self.request, _("Deleted page '%s'") % self.object.title)
+        messages.success(
+            self.request, _("Deleted page '%s'") % self.object.title)
         return reverse('dashboard:page-list')
