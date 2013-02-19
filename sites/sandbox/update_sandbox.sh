cbc04b4701463304979377012da0df4b1aa0ee7c
David Winterbottom <david.winterbottom@gmail.com>
2013-02-19 16:11:36 +0000

    Remove CSS compilation from sandbox update script

--- sites/sandbox/update_sandbox.sh
+++ sites/sandbox/update_sandbox.sh
@@ -9,9 +9,6 @@ source ../../virtualenvs/sandbox/bin/activate
 python setup.py develop
 pip install -r requirements.txt
 
-# Compile CSS from LESS files
-oscar/static/oscar/less/generate_css.sh
-
 cd sites/sandbox
 ./manage.py syncdb --noinput
 ./manage.py migrate
