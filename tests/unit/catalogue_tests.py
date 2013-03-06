8e4d5edd95c13fc5f6524660bd2560d681605380
David Winterbottom <david.winterbottom@gmail.com>
2013-03-06 13:02:56 +0000

    Move category tests to integration folder
    They use the DB so they are not really unit tests.

--- tests/unit/catalogue_tests.py
+++ tests/unit/catalogue_tests.py
@@ -1,11 +1,9 @@
 from django.test import TestCase
-from django.core.exceptions import ValidationError
 
-from oscar.apps.catalogue.models import (Product, ProductClass, Category,
+from oscar.apps.catalogue.models import (Product, ProductClass,
                                          ProductAttribute,
                                          AttributeOptionGroup,
                                          AttributeOption)
-from oscar.apps.catalogue.categories import create_from_breadcrumbs
 
 
 class TestProductClassModel(TestCase):
@@ -22,115 +20,6 @@ class TestProductClassModel(TestCase):
             name="Download",
             requires_shipping=False
         )
-
-
-class TestCategoryFactory(TestCase):
-
-    def setUp(self):
-        Category.objects.all().delete()
-
-    def test_can_create_single_level_category(self):
-        trail = 'Books'
-        category = create_from_breadcrumbs(trail)
-        self.assertIsNotNone(category)
-        self.assertEquals(category.name, 'Books')
-        self.assertEquals(category.slug, 'books')
-
-    def test_can_create_parent_and_child_categories(self):
-        trail = 'Books > Science-Fiction'
-        category = create_from_breadcrumbs(trail)
-
-        self.assertIsNotNone(category)
-        self.assertEquals(category.name, 'Science-Fiction')
-        self.assertEquals(category.get_depth(), 2)
-        self.assertEquals(category.get_parent().name, 'Books')
-        self.assertEquals(2, Category.objects.count())
-        self.assertEquals(category.slug, 'books/science-fiction')
-
-    def test_can_create_multiple_categories(self):
-        trail = 'Books > Science-Fiction > Star Trek'
-        create_from_breadcrumbs(trail)
-        trail = 'Books > Factual > Popular Science'
-        category = create_from_breadcrumbs(trail)
-
-        self.assertIsNotNone(category)
-        self.assertEquals(category.name, 'Popular Science')
-        self.assertEquals(category.get_depth(), 3)
-        self.assertEquals(category.get_parent().name, 'Factual')
-        self.assertEquals(5, Category.objects.count())
-        self.assertEquals(category.slug, 'books/factual/popular-science', )
-
-    def test_can_use_alternative_separator(self):
-        trail = 'Food|Cheese|Blue'
-        create_from_breadcrumbs(trail, separator='|')
-        self.assertEquals(3, len(Category.objects.all()))
-
-    def test_updating_subtree_slugs_when_moving_category_to_new_parent(self):
-        trail = 'A > B > C'
-        create_from_breadcrumbs(trail)
-        trail = 'A > B > D'
-        create_from_breadcrumbs(trail)
-        trail = 'A > E > F'
-        create_from_breadcrumbs(trail)
-        trail = 'A > E > G'
-        create_from_breadcrumbs(trail)
-
-        trail = 'T'
-        target = create_from_breadcrumbs(trail)
-        category = Category.objects.get(name='A')
-
-        category.move(target, pos='first-child')
-
-        c1 = Category.objects.get(name='A')
-        self.assertEqual(c1.slug, 't/a')
-        self.assertEqual(c1.full_name, 'T > A')
-
-        child = Category.objects.get(name='F')
-        self.assertEqual(child.slug, 't/a/e/f')
-        self.assertEqual(child.full_name, 'T > A > E > F')
-
-        child = Category.objects.get(name='D')
-        self.assertEqual(child.slug, 't/a/b/d')
-        self.assertEqual(child.full_name, 'T > A > B > D')
-
-    def test_updating_subtree_when_moving_category_to_new_sibling(self):
-        trail = 'A > B > C'
-        create_from_breadcrumbs(trail)
-        trail = 'A > B > D'
-        create_from_breadcrumbs(trail)
-        trail = 'A > E > F'
-        create_from_breadcrumbs(trail)
-        trail = 'A > E > G'
-        create_from_breadcrumbs(trail)
-
-        category = Category.objects.get(name='E')
-        target = Category.objects.get(name='A')
-
-        category.move(target, pos='right')
-
-        child = Category.objects.get(name='E')
-        self.assertEqual(child.slug, 'e')
-        self.assertEqual(child.full_name, 'E')
-
-        child = Category.objects.get(name='F')
-        self.assertEqual(child.slug, 'e/f')
-        self.assertEqual(child.full_name, 'E > F')
-
-
-class TestCategory(TestCase):
-
-    def test_supports_has_children_method(self):
-        """Supports has_children method"""
-        root = Category.add_root(name="Products")
-        self.assertFalse(root.has_children())
-        root.add_child(name="Books")
-        self.assertTrue(root.has_children())
-
-    def test_enforces_slug_uniqueness(self):
-        root = Category.add_root(name="Products")
-        root.add_child(name="Books")
-        with self.assertRaises(ValidationError):
-            root.add_child(name="Books")
 
 
 class ProductTests(TestCase):
