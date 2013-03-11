42922307a70b7a6f3e256ed7b4370aa3296c245f
Davi Vidal <davividal@gmail.com>
2013-03-11 09:37:41 -0300

    Starting partners_dashboard

---
+++ oscar/apps/dashboard/partners/app.py
@@ -0,0 +1,34 @@
+from django.conf.urls import patterns, url
+
+from oscar.views.decorators import staff_member_required
+from oscar.core.application import Application
+from oscar.apps.dashboard.partners import views
+
+
+class PartnersDashboardApplication(Application):
+    name = None
+    list_view = views.PartnerListView
+    detail_view = views.PartnerDetailView
+    create_view = views.PartnerCreateView
+    update_view = views.PartnerUpdateView
+    delete_view = views.PartnerDeleteView
+
+    def get_urls(self):
+        urlpatterns = patterns('',
+            url(r'^$', self.list_view.as_view(), name='partner-list'),
+            url(r'^(?P<pk>\d+)/$', self.detail_view.as_view(),
+                name='partner-detail'),
+            url(r'^create/$', self.create_view.as_view(),
+                name='partner-create'),
+            url(r'^(?P<pk>\d+)/update/$', self.update_view.as_view(),
+                name='partner-update'),
+            url(r'^(?P<pk>\d+)/delete/$', self.delete_view.as_view(),
+                name='partner-delete'),
+        )
+        return self.post_process_urls(urlpatterns)
+
+    def get_url_decorator(self, url_name):
+        return staff_member_required
+
+
+application = PartnersDashboardApplication()
