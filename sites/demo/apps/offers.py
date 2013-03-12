bd5b9c15477cdb090d287d40a5e58d357beb8565
David Winterbottom <david.winterbottom@gmail.com>
2013-03-12 17:14:27 +0000

    Rename demo offers module to be the same as sandbox
    (So the fixture loads in both envs).

--- sites/demo/apps/offers.py
+++ sites/demo/apps/offers.py
@@ -27,3 +27,22 @@ class BasketOwnerCalledBarry(models.Condition):
 
     def consume_items(self, basket, affected_lines):
         return
+
+
+class ChangesOwnerName(models.Benefit):
+
+    class Meta:
+        proxy = True
+
+    def apply(self, basket, condition, offer=None):
+        condition.consume_items(basket, ())
+        return models.PostOrderAction(
+            "You will have your name changed to Barry!")
+
+    def apply_deferred(self, basket):
+        basket.owner.first_name = "Barry"
+        basket.owner.save()
+
+    @property
+    def description(self):
+        return "Changes owners name"
