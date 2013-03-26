f5093fa02fdd32644afe9e758bf6eb35e915b7fd
Maik Hoepfel <m@maikhoepfel.de>
2013-03-26 13:42:15 +0100

    render_product: Docs & default paths

--- oscar/templatetags/product_tags.py
+++ oscar/templatetags/product_tags.py
@@ -14,8 +14,8 @@ def render_product(context, product):
     product class of the passed product.  This allows alternative templates to
     be used for different product classes.
     """
-    names = ['catalogue/partials/product-for-upc-%s.html' % product.upc,
-             'catalogue/partials/product-for-class-%s.html' % product.product_class.name.lower(),
+    names = ['catalogue/partials/product/upc-%s.html' % product.upc,
+             'catalogue/partials/product/class-%s.html' % product.product_class.name.lower(),
              'catalogue/partials/product.html']
     template_ = select_template(names)
     # Ensure the passed product is in the context as 'product'
