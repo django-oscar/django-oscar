6d0915103ae126694327184c72966d236e86e087
David Winterbottom <david.winterbottom@gmail.com>
2013-03-27 12:52:05 +0000

    Default to having debug toolbar disabled
    Makes sandbox MUCH faster.

--- sites/sandbox/settings.py
+++ sites/sandbox/settings.py
@@ -140,7 +140,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Allow languages to be selected
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
