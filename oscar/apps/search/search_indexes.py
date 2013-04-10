6d258aaca5156fa4313f2300f1bb73102a88eda6
David Winterbottom <david.winterbottom@gmail.com>
2013-04-10 14:54:28 +0100

    Solr search now works!
    Tested with Solr 4.2.1 on OSX.

--- oscar/apps/search/search_indexes.py
+++ oscar/apps/search/search_indexes.py
@@ -8,8 +8,10 @@ class ProductIndex(indexes.SearchIndex, indexes.Indexable):
     Base class for products solr index definition.  Overide by creating your
     own copy of oscar.search_indexes.py
     """
-    text = indexes.EdgeNgramField(document=True, use_template=True,
-                                  template_name='oscar/search/indexes/product/item_text.txt')
+    text = indexes.EdgeNgramField(
+        document=True, use_template=True,
+        template_name='oscar/search/indexes/product/item_text.txt')
+
     title = indexes.EdgeNgramField(model_attr='title', null=True)
     upc = indexes.CharField(model_attr="upc", null=True)
     date_created = indexes.DateTimeField(model_attr='date_created')
@@ -22,13 +24,15 @@ class ProductIndex(indexes.SearchIndex, indexes.Indexable):
         """
         Used when the entire index for model is updated.
 
-        Orders by the most recently updated so that new objects are indexed first
+        Orders by the most recently updated so that new objects are indexed
+        first
         """
-        return self.get_model().objects.order_by('-date_updated')
+        return self.get_model().browsable.order_by('-date_updated')
 
     def get_updated_field(self):
         """
-        Used to specify the field used to determine if an object has been updated
+        Used to specify the field used to determine if an object has been
+        updated
 
         Can be used to filter the query set when updating the index
         """
--- sites/demo/settings.py
+++ sites/demo/settings.py
@@ -240,7 +240,8 @@ APPEND_SLASH = True
 # Haystack settings
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://127.0.0.1:8983/solr',
     },
 }
 
