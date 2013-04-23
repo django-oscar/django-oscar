c06899c3185e3c4687c392173507ed9d9a3ee193
Skirmantas Jurgaitis <skirmantas.jurgaitis@tangentlabs.co.uk>
2013-04-23 13:25:45 +0100

    Include 'back to search results' link on product detail page
    This change introduces a templatetag which assigns context for adding a
    'back to search results' link to a product detail page.  This can be
    useful for certain types of user (who don't use the back button) and
    want to get back to their, possibly complicated, set of search filters.
    
    Fixes #456

--- oscar/templatetags/history_tags.py
+++ oscar/templatetags/history_tags.py
@@ -1,9 +1,13 @@
+import urlparse
 from django import template
 from django.db.models import get_model
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import resolve, Resolver404
 
 from oscar.core.loading import get_class
 
 Product = get_model('catalogue', 'Product')
+Site = get_model('sites', 'Site')
 get_recently_viewed_product_ids = get_class('customer.history_helpers',
                                             'get_recently_viewed_product_ids')
 
@@ -29,3 +33,47 @@ def recently_viewed_products(context):
     products = [product_dict[id] for id in product_ids if id in product_dict]
     return {'products': products,
             'request': request}
+
+
+@register.assignment_tag(takes_context=True)
+def get_back_button(context):
+    """
+    Show back button, custom title available for different urls, for
+    example 'Back to search results', no back button if user came from other
+    site
+    """
+    request = context.get('request', None)
+    if not request:
+        raise Exception('Cannot get request from context')
+
+    referrer = request.META.get('HTTP_REFERER', None)
+    if not referrer:
+        return None
+
+    try:
+        url = urlparse.urlparse(referrer)
+    except:
+        return None
+
+    if request.get_host() != url.netloc:
+        try:
+            Site.objects.get(domain=url.netloc)
+        except Site.DoesNotExist:
+            # Came from somewhere else, don't show back button:
+            return None
+
+    try:
+        match = resolve(url.path)
+    except Resolver404:
+        return None
+
+    # This dict can be extended to link back to other browsing pages
+    titles = {
+        'search:search': _('Back to search results'),
+    }
+    title = titles.get(match.view_name, None)
+
+    if title is None:
+        return None
+
+    return {'url': referrer, 'title': unicode(title), 'match': match}
