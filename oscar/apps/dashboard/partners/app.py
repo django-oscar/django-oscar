71f8ec92411a27f5029e48d281340bcd399ff9bc
David Winterbottom <david.winterbottom@gmail.com>
2013-05-07 17:06:56 +0100

    Change URLs for partner dashboard
    Plus change URL names to try and be as conventional as possible.

--- oscar/apps/dashboard/partners/app.py
+++ oscar/apps/dashboard/partners/app.py
@@ -7,14 +7,17 @@ from oscar.apps.dashboard.partners import views
 
 class PartnersDashboardApplication(Application):
     name = None
+
     list_view = views.PartnerListView
     create_view = views.PartnerCreateView
     update_view = views.PartnerUpdateView
     delete_view = views.PartnerDeleteView
+
     manage_users_view = views.PartnerManageUsers
-    update_user_view = views.PartnerUpdateUserView
     unlink_user_view = views.PartnerUnlinkUserView
     create_user_view = views.PartnerCreateUserView
+
+    update_user_view = views.PartnerUpdateUserView
 
     def get_urls(self):
         urlpatterns = patterns('',
@@ -25,19 +28,24 @@ class PartnersDashboardApplication(Application):
                 name='partner-update'),
             url(r'^(?P<pk>\d+)/delete/$', self.delete_view.as_view(),
                 name='partner-delete'),
-            url(r'^(?P<pk>\d+)/manage_users/$', self.manage_users_view.as_view(),
-                name='partner-manage-users'),
-            url(r'^user/add_for/(?P<partner_pk>\d+)/$', self.create_user_view.as_view(),
-                name='partner-create-user'),
-            url(r'^user/(?P<pk>\d+)/update/$', self.update_user_view.as_view(),
-                name='partner-update-user'),
-            url(r'^user/(?P<user_pk>\d+)/unlink_from/(?P<partner_pk>\d+)/$',
-                self.unlink_user_view.as_view(), name='partner-unlink-user'),
 
+            url(r'^(?P<pk>\d+)/users/$',
+                self.manage_users_view.as_view(),
+                name='partner-user-list'),
+            url(r'^(?P<partner_pk>\d+)/users/add/$',
+                self.create_user_view.as_view(),
+                name='partner-user-create'),
+            url(r'^(?P<partner_pk>\d+)/users/(?P<user_pk>\d+)/unlink/$',
+                self.unlink_user_view.as_view(), name='partner-user-unlink'),
+
+            url(r'^users/(?P<pk>\d+)/update/$',
+                self.update_user_view.as_view(),
+                name='partner-user-update'),
         )
         return self.post_process_urls(urlpatterns)
 
     def get_url_decorator(self, url_name):
         return staff_member_required
+
 
 application = PartnersDashboardApplication()
