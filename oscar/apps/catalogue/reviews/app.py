dbc1e45aaefd20960b7026272f6f568950e47d9f
Maik Hoepfel <m@maikhoepfel.de>
2013-05-17 16:21:47 +0100

    Make reviews app hidable
    Using the newly introduced feature hiding functionality, the reviews app
    can now be hidden.

--- oscar/apps/catalogue/reviews/app.py
+++ oscar/apps/catalogue/reviews/app.py
@@ -6,6 +6,7 @@ from oscar.apps.catalogue.reviews import views
 
 class ProductReviewsApplication(Application):
     name = None
+    hidable_feature_name = "reviews"
     detail_view = views.ProductReviewDetail
     create_view = views.CreateProductReview
     list_view = views.ProductReviewList
@@ -17,7 +18,7 @@ class ProductReviewsApplication(Application):
             url(r'^add/$', self.create_view.as_view(), name='reviews-add'),
             url(r'^$', self.list_view.as_view(), name='reviews-list'),
         )
-        return urlpatterns
+        return self.post_process_urls(urlpatterns)
 
 
 application = ProductReviewsApplication()
