7f898d71cfa792492026d5655715d584b7df3d3e
Georgiy Kutsurua <g.kutsurua@ostrovok.ru>
2013-05-24 14:14:22 +0400

    Fix has stock record (when fetch related): None vs Instance Thanks for @maikhoepfel

--- oscar/apps/catalogue/abstract_models.py
+++ oscar/apps/catalogue/abstract_models.py
@@ -393,7 +393,7 @@ class AbstractProduct(models.Model):
         except ObjectDoesNotExist:
             return False
         else:
-            return isinstance(self.stockrecord, get_class('partner.models', 'StockRecord'))
+            return self.stockrecord is not None
 
     def is_purchase_permitted(self, user, quantity):
         """
