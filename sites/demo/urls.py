3a558aa1646f42f796c92fd2836f8c8c2c85d0ca
Jannis Leidel <jannis@leidel.info>
2013-06-08 19:57:25 +0200

    Stop manually serving static files, this is done by staticfiles' runserver command automatically.

--- sites/demo/urls.py
+++ sites/demo/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include
 from django.conf import settings
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from stores.app import application as stores_app
 from stores.dashboard.app import application as dashboard_app
@@ -31,6 +30,5 @@ urlpatterns = patterns('',
 )
 
 if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
