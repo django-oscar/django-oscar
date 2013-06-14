e5310f1147694992d1eb48a037261f805955294c
David Winterbottom <david.winterbottom@gmail.com>
2013-06-14 11:18:56 +0100

    Adjust test generator to make tests pass

--- tests/unit/forms/widget_tests.py
+++ tests/unit/forms/widget_tests.py
@@ -3,18 +3,23 @@ import nose
 from oscar.forms import widgets
 
 
+def compare_date_format(format, expected):
+    nose.tools.eq_(
+        widgets.datetime_format_to_js_date_format(format), expected)
+
+
 def test_datetime_to_date_format_conversion():
     format_testcases = (
         ('%Y-%m-%d', 'yy-mm-dd'),
         ('%Y-%m-%d %H:%M', 'yy-mm-dd'),
     )
-
-    def compare(format, expected):
-        nose.tools.eq_(
-            widgets.datetime_format_to_js_date_format(format), expected)
-
     for format, expected in format_testcases:
-        yield compare, format, expected
+        yield compare_date_format, format, expected
+
+
+def compare_time_format(format, expected):
+    nose.tools.eq_(
+        widgets.datetime_format_to_js_time_format(format), expected)
 
 
 def test_datetime_to_time_format_conversion():
@@ -23,10 +28,5 @@ def test_datetime_to_time_format_conversion():
         ('%Y-%m-%d %H:%M', 'HH:mm'),
         ('%d/%m/%Y', ''),
     )
-
-    def compare(format, expected):
-        nose.tools.eq_(
-            widgets.datetime_format_to_js_time_format(format), expected)
-
     for format, expected in format_testcases:
-        yield compare, format, expected
+        yield compare_time_format, format, expected
