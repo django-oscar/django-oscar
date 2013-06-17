9d65dbb84126a26e8e6dfe31179087f75134f564
David Winterbottom <david.winterbottom@gmail.com>
2013-06-17 12:10:16 +0100

    Load User model from compatibility layer

--- tests/integration/auth_tests.py
+++ tests/integration/auth_tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import authenticate
 from django.core import mail
+
+from oscar.core.compat import get_user_model
 
 User = get_user_model()
 
