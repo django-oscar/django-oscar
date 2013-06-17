7d0ca83f231f6b04479831ccb32f49367a143e4c
David Winterbottom <david.winterbottom@gmail.com>
2013-06-17 10:55:03 +0100

    Fix a few broken url tags

--- oscar/apps/customer/abstract_models.py
+++ oscar/apps/customer/abstract_models.py
@@ -13,69 +13,74 @@ from django.utils.translation import ugettext_lazy as _
 from oscar.apps.customer.managers import CommunicationTypeManager
 from oscar.core.compat import AUTH_USER_MODEL
 
+if hasattr(auth_models, 'BaseUserManager'):
+    # Only define custom UserModel when Django >= 1.5
+    class UserManager(auth_models.BaseUserManager):
 
-class UserManager(auth_models.BaseUserManager):
+        def create_user(self, email, password=None, **extra_fields):
+            """
+            Creates and saves a User with the given username, email and
+            password.
+            """
+            now = timezone.now()
+            if not email:
+                raise ValueError('The given email must be set')
+            email = UserManager.normalize_email(email)
+            user = self.model(
+                email=email, is_staff=False, is_active=True,
+                is_superuser=False,
+                last_login=now, date_joined=now, **extra_fields)
 
-    def create_user(self, email, password=None, **extra_fields):
+            user.set_password(password)
+            user.save(using=self._db)
+            return user
+
+        def create_superuser(self, email, password, **extra_fields):
+            u = self.create_user(email, password, **extra_fields)
+            u.is_staff = True
+            u.is_active = True
+            u.is_superuser = True
+            u.save(using=self._db)
+            return u
+
+    class AbstractUser(auth_models.AbstractBaseUser,
+                       auth_models.PermissionsMixin):
         """
-        Creates and saves a User with the given username, email and password.
+        An abstract base user suitable for use in Oscar projects.
+
+        This is basically a copy of the core AbstractUser model but without a
+        username field
         """
-        now = timezone.now()
-        if not email:
-            raise ValueError('The given email must be set')
-        email = UserManager.normalize_email(email)
-        user = self.model(
-            email=email, is_staff=False, is_active=True, is_superuser=False,
-            last_login=now, date_joined=now, **extra_fields)
+        email = models.EmailField(_('email address'), unique=True)
+        first_name = models.CharField(
+            _('First name'), max_length=255, blank=True)
+        last_name = models.CharField(
+            _('Last name'), max_length=255, blank=True)
+        is_staff = models.BooleanField(
+            _('Staff status'), default=False,
+            help_text=_('Designates whether the user can log into this admin '
+                        'site.'))
+        is_active = models.BooleanField(
+            _('Active'), default=True,
+            help_text=_('Designates whether this user should be treated as '
+                        'active. Unselect this instead of deleting accounts.'))
+        date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+        objects = UserManager()
 
-    def create_superuser(self, email, password, **extra_fields):
-        u = self.create_user(email, password, **extra_fields)
-        u.is_staff = True
-        u.is_active = True
-        u.is_superuser = True
-        u.save(using=self._db)
-        return u
+        USERNAME_FIELD = 'email'
 
+        class Meta:
+            verbose_name = _('User')
+            verbose_name_plural = _('Users')
+            abstract = True
 
-class AbstractUser(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
-    """
-    An abstract base user suitable for use in Oscar projects.
+        def get_full_name(self):
+            full_name = '%s %s' % (self.first_name, self.last_name)
+            return full_name.strip()
 
-    This is basically a copy of the core AbstractUser model but without a
-    username field
-    """
-    email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('First name'), max_length=255, blank=True)
-    last_name = models.CharField(_('Last name'), max_length=255, blank=True)
-    is_staff = models.BooleanField(
-        _('Staff status'), default=False,
-        help_text=_('Designates whether the user can log into this admin '
-                    'site.'))
-    is_active = models.BooleanField(
-        _('Active'), default=True,
-        help_text=_('Designates whether this user should be treated as '
-                    'active. Unselect this instead of deleting accounts.'))
-    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-
-    class Meta:
-        verbose_name = _('User')
-        verbose_name_plural = _('Users')
-        abstract = True
-
-    def get_full_name(self):
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
-
-    def get_short_name(self):
-        return self.first_name
+        def get_short_name(self):
+            return self.first_name
 
 
 class AbstractEmail(models.Model):
