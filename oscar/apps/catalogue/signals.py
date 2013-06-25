22d4715741177b4e7890cea0b8c253a3187f1f9e
Sebastian Vetter <sebastian@roadside-developer.com>
2013-06-25 11:58:26 +1000

    Fix typo in catalogue signals module

--- oscar/apps/catalogue/signals.py
+++ oscar/apps/catalogue/signals.py
@@ -4,4 +4,4 @@ product_viewed = django.dispatch.Signal(
     providing_args=["product", "user", "request", "response"])
 
 # This needs to be moved into the search app when it is refactored
-product_search = django.dispatch.Signal(providing_args=["query", '"user'])
+product_search = django.dispatch.Signal(providing_args=["query", "user"])
