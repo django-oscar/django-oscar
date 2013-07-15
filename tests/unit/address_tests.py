1a87ae47fa29c9d68c4f554d78015b62606d6ff6
Izidor Matušov <izidor.matusov@tangentsnowball.com>
2013-07-15 17:27:53 +0100

    Use clean() only for tests
    When using full_clean(), every field is tested including fields ommitted
    for test simplicity. Build shouldn't fail now.

--- tests/unit/address_tests.py
+++ tests/unit/address_tests.py
@@ -52,14 +52,14 @@ INVALID_POSTCODES = [
 def assert_valid_postcode(country_value, postcode_value):
     country = Country(iso_3166_1_a2=country_value)
     address = UserAddress(country=country, postcode=postcode_value)
-    address.full_clean()
+    address.clean()
 
 
 @raises(exceptions.ValidationError)
 def assert_invalid_postcode(country_value, postcode_value):
     country = Country(iso_3166_1_a2=country_value)
     address = UserAddress(country=country, postcode=postcode_value)
-    address.full_clean()
+    address.clean()
 
 
 def test_postcode_is_validated_for_country():
