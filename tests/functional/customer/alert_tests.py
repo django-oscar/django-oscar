ea7237fbbbc90647ec0e54135ff9bab36b69352b
Maik Hoepfel <m@maikhoepfel.de>
2013-09-05 15:52:32 +0100

    Fix odd bug in tests
    This was a code change introduced in the refactoring, but does not make
    any sense. Luckily, it also caused the tests to fail, so I'm happy to
    revert it.

--- tests/functional/customer/alert_tests.py
+++ tests/functional/customer/alert_tests.py
@@ -48,7 +48,7 @@ class TestAUserWithAnActiveStockAlert(WebTest):
         alerts = ProductAlert.objects.filter(user=self.user)
         self.assertEqual(1, len(alerts))
         alert = alerts[0]
-        self.assertFalse(alert.is_cancelled)
+        self.assertTrue(alert.is_cancelled)
 
 
     def test_gets_notified_when_it_is_back_in_stock(self):
