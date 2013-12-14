30de4a936db8e75c0510c7a89eee25e39874c1c7
Markus Bertheau <mbertheau@gmail.com>
2013-12-14 20:11:04 +0900

    Remove threshold and ignored warnings from lint.sh
    Also output the errors directly now that it's not over 100 anymore.

--- lint.sh
+++ lint.sh
@@ -4,26 +4,16 @@
 #
 # This is run on Travis to ensure that pull requests conform to the project coding standards.
 
-# Ideally, this figure should be < 100.  I'll keep reducing it as the 
-# codebase gets tidied up incrementally.
-THRESHOLD=0
-
-# Some warnings aren't worth worrying about...
-IGNORE="W292,E202,E128,E124"
-
 # Run flake8 and convert the output into a format that the "violations" plugin 
 # for Jenkins/Hudson can understand.  Ignore warnings from migrations we we don't
 # really care about those.
 ERRORFILE="violations.txt"
-flake8 --ignore=$IGNORE oscar | perl -ple "s/: /: [E] /" | grep -v migrations > $ERRORFILE
+flake8 --exclude=migrations oscar | perl -ple "s/: /: [E] /" > $ERRORFILE
 
 # Check that the number of violations is acceptable
 NUMERRORS=`cat $ERRORFILE | wc -l`
-if [ $NUMERRORS -gt $THRESHOLD ]
+if [ $NUMERRORS -gt 0 ]
 then
-	echo 
-	echo "Too many flake8 errors - maximum allowed is $THRESHOLD, found $NUMERRORS"
-	echo 
-	echo "To fix, run 'make lint' and examine $ERRORFILE"
-	exit 1
+    cat violations.txt
+    exit 1
 fi
