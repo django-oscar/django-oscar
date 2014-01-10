3caf138af85c0a0a841612b0217bc78c55a34fb3
Xavier Ordoquy <xordoquy@linovia.com>
2014-01-10 01:14:32 +0100

    Use six iterates.

--- oscar/apps/dashboard/catalogue/views.py
+++ oscar/apps/dashboard/catalogue/views.py
@@ -1,3 +1,5 @@
+import six
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.views import generic
 from django.db.models import get_model, Q
@@ -211,7 +213,7 @@ class ProductCreateUpdateView(generic.UpdateView):
         ctx = super(ProductCreateUpdateView, self).get_context_data(**kwargs)
         ctx['product_class'] = self.product_class
 
-        for ctx_name, formset_class in self.formsets.iteritems():
+        for ctx_name, formset_class in six.iteritems(self.formsets):
             if ctx_name not in ctx:
                 ctx[ctx_name] = formset_class(self.product_class,
                                               self.request.user,
@@ -239,7 +241,7 @@ class ProductCreateUpdateView(generic.UpdateView):
             self.object = form.save()
 
         formsets = {}
-        for ctx_name, formset_class in self.formsets.iteritems():
+        for ctx_name, formset_class in six.iteritems(self.formsets):
             formsets[ctx_name] = formset_class(self.product_class,
                                                self.request.user,
                                                self.request.POST,
