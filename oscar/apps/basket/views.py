798a21045d861a132128988ff2d415c8b69f59c8
David Winterbottom <david.winterbottom@gmail.com>
2014-01-21 17:18:45 +0000

    Update voucher addition counter when voucher is removed

--- oscar/apps/basket/views.py
+++ oscar/apps/basket/views.py
@@ -13,7 +13,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from extra_views import ModelFormSetView
 from oscar.core import ajax
-from oscar.apps.basket.signals import basket_addition, voucher_addition
+from oscar.apps.basket import signals
 from oscar.core.loading import get_class, get_classes
 Applicator = get_class('offer.utils', 'Applicator')
 (BasketLineFormSet, BasketLineForm, AddToBasketForm, BasketVoucherForm,
@@ -280,7 +280,7 @@ class BasketAddView(FormView):
     form_class = AddToBasketForm
     product_select_form_class = ProductSelectionForm
     product_model = get_model('catalogue', 'product')
-    add_signal = basket_addition
+    add_signal = signals.basket_addition
 
     def get(self, request, *args, **kwargs):
         return HttpResponseRedirect(reverse('basket:summary'))
@@ -349,7 +349,7 @@ class BasketAddView(FormView):
 class VoucherAddView(FormView):
     form_class = BasketVoucherForm
     voucher_model = get_model('voucher', 'voucher')
-    add_signal = voucher_addition
+    add_signal = signals.voucher_addition
 
     def get(self, request, *args, **kwargs):
         return HttpResponseRedirect(reverse('basket:summary'))
@@ -425,16 +425,17 @@ class VoucherAddView(FormView):
 
 class VoucherRemoveView(View):
     voucher_model = get_model('voucher', 'voucher')
-
-    def get(self, request, *args, **kwargs):
-        return HttpResponseRedirect(reverse('basket:summary'))
+    remove_signal = signals.voucher_removal
+    http_method_names = ['post']
 
     def post(self, request, *args, **kwargs):
-        voucher_id = int(kwargs.pop('pk'))
+        response = HttpResponseRedirect(reverse('basket:summary'))
+
+        voucher_id = kwargs['pk']
         if not request.basket.id:
             # Hacking attempt - the basket must be saved for it to have
             # a voucher in it.
-            return HttpResponseRedirect(reverse('basket:summary'))
+            return response
         try:
             voucher = request.basket.vouchers.get(id=voucher_id)
         except ObjectDoesNotExist:
@@ -442,10 +443,12 @@ class VoucherRemoveView(View):
                 request, _("No voucher found with id '%d'") % voucher_id)
         else:
             request.basket.vouchers.remove(voucher)
-            request.basket.save()
+            self.remove_signal.send(
+                sender=self, basket=request.basket, voucher=voucher)
             messages.info(
                 request, _("Voucher '%s' removed from basket") % voucher.code)
-        return HttpResponseRedirect(reverse('basket:summary'))
+
+        return response
 
 
 class SavedView(ModelFormSetView):
--- oscar/apps/voucher/receivers.py
+++ oscar/apps/voucher/receivers.py
@@ -6,4 +6,10 @@ def track_voucher_addition(basket, voucher, **kwargs):
     voucher.save()
 
 
+def track_voucher_removal(basket, voucher, **kwargs):
+    voucher.num_basket_additions -= 1
+    voucher.save()
+
+
 signals.voucher_addition.connect(track_voucher_addition)
+signals.voucher_removal.connect(track_voucher_removal)
