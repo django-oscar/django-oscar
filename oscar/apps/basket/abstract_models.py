5098ab0ea22cf99728fc567828914b90b6f7c061
Maik Hoepfel <m@maikhoepfel.de>
2014-01-23 16:06:56 +0000

    Remove several code paths exclusive to Django 1.4

--- oscar/apps/basket/abstract_models.py
+++ oscar/apps/basket/abstract_models.py
@@ -118,15 +118,6 @@ class AbstractBasket(models.Model):
             self._lines = self.lines.select_related(
                 'product', 'product__stockrecord'
             ).all().prefetch_related('attributes', 'product__images')
-
-            # Assign strategy to each line so it can use it to determine
-            # prices.  This is only needed for Django 1.4.5, where accessing
-            # self.basket from within the line will create a new basket
-            # instance (with no strategy assigned).  In later version, the
-            # original basket instance is cached and keeps its strategy
-            # property.
-            for line in self._lines:
-                line.strategy = self.strategy
         return self._lines
 
     def is_quantity_allowed(self, qty):
