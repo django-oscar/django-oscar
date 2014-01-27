066fc4008fdb87a6ae3764e706d886b647663890
David Winterbottom <david.winterbottom@gmail.com>
2014-01-27 17:21:44 +0000

    Bump version to 0.6.2
    Plus add some release notes.

--- docs/source/releases/index.rst
+++ docs/source/releases/index.rst
@@ -13,3 +13,4 @@ Release notes for each version of Oscar published to PyPI.
     v0.5.3
     v0.6
     v0.6.1
+    v0.6.2
--- oscar/__init__.py
+++ oscar/__init__.py
@@ -3,7 +3,7 @@ import os
 # Use 'final' as the 4th element to indicate
 # a full release
 
-VERSION = (0, 6, 1, 'final', 0)
+VERSION = (0, 6, 2, 'final', 0)
 
 
 def get_short_version():
