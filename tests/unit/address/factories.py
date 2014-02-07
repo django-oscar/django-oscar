0d673fb40e79b1b8c6858d794cf59d10039b89e5
Andrea Grandi <a.grandi@gmail.com>
2014-02-07 17:16:34 +0000

    Add User and Country factories

--- tests/unit/address/factories.py
+++ tests/unit/address/factories.py
@@ -1,5 +1,22 @@
 import factory
 from oscar.apps.address import models
+from oscar.core.compat import get_user_model
+
+class UserFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = get_user_model()
+
+    first_name = 'Oscar'
+    last_name = 'Ecommerce'
+    username = 'oscar'
+    password = 'oscar'
+    is_active = True
+    is_superuser = False
+
+class CountryFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = models.Country
+
+    iso_3166_1_a2='GB' 
+    name="UNITED KINGDOM"
 
 class UserAddressFactory(factory.DjangoModelFactory):
     FACTORY_FOR = models.UserAddress
@@ -10,3 +27,5 @@ class UserAddressFactory(factory.DjangoModelFactory):
     line1="1 King Road"
     line4="London"
     postcode="SW1 9RE"
+    country = factory.SubFactory(CountryFactory)
+    user = factory.SubFactory(UserFactory)
