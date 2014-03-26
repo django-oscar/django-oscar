21d73a1a9643c3596c7b4aed2bc5e96e702995aa
David Winterbottom <david.winterbottom@gmail.com>
2014-03-26 10:30:10 +0000

    Rewrite checkout functional tests
    We no longer use Django's test client.

--- tests/functional/checkout/guest_checkout_tests.py
+++ tests/functional/checkout/guest_checkout_tests.py
@@ -1,4 +1,3 @@
-from decimal import Decimal as D
 import sys
 
 from django.test.utils import override_settings
@@ -7,9 +6,9 @@ from django.conf import settings
 from django.utils.importlib import import_module
 import mock
 
-from oscar.apps.address.models import Country
 from oscar.test.testcases import WebTestCase
 from oscar.test import factories
+from . import CheckoutMixin
 
 # Python 3 compat
 try:
@@ -24,43 +23,6 @@ def reload_url_conf():
         reload(sys.modules[settings.ROOT_URLCONF])
     import_module(settings.ROOT_URLCONF)
 
-
-class CheckoutMixin(object):
-
-    def create_digital_product(self):
-        product = factories.create_product(price=D('12.00'), num_in_stock=None)
-        product.product_class.requires_shipping = False
-        product.product_class.track_stock = False
-        product.product_class.save()
-        return product
-
-    def add_product_to_basket(self, product=None):
-        if product is None:
-            product = factories.create_product(price=D('12.00'),
-                                               num_in_stock=10)
-        detail_page = self.get(product.get_absolute_url())
-        form = detail_page.forms['add_to_basket_form']
-        form.submit()
-
-    def enter_guest_details(self, email='guest@example.com'):
-        index_page = self.get(reverse('checkout:index'))
-        index_page.form['username'] = email
-        index_page.form.submit()
-
-    def enter_shipping_address(self):
-        Country.objects.get_or_create(
-            iso_3166_1_a2='GB', is_shipping_country=True)
-        address_page = self.get(reverse('checkout:shipping-address'))
-        form = address_page.forms['new_shipping_address']
-        form['first_name'] = 'John'
-        form['last_name'] = 'Doe'
-        form['line1'] = '1 Egg Road'
-        form['line4'] = 'Shell City'
-        form['postcode'] = 'N12 9RT'
-        form.submit()
-
-    def enter_shipping_method(self):
-        self.get(reverse('checkout:shipping-method'))
 
 @override_settings(OSCAR_ALLOW_ANON_CHECKOUT=True)
 class TestIndexView(CheckoutMixin, WebTestCase):
