5517a113f8c8c3bd2763eeb9b73ebc7d61a6863f
David Winterbottom <david.winterbottom@gmail.com>
2014-05-06 10:19:18 +0100

    Slight tidy of sandbox URLs

--- sites/sandbox/urls.py
+++ sites/sandbox/urls.py
@@ -5,9 +5,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 
 from oscar.app import shop
-# These simply need to be imported into this namespace.  Ignore the PEP8
-# warning that they aren't used.
-from oscar.views import handler500, handler404, handler403
+from oscar.views import handler500, handler404, handler403  # noqa
 
 from apps.sitemaps import base_sitemaps
 
@@ -23,8 +21,10 @@ urlpatterns = [
     # i18n URLS need to live outside of i18n_patterns scope of the shop
     url(r'^i18n/', include('django.conf.urls.i18n')),
     # include a basic sitemap
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.index', {'sitemaps': base_sitemaps}),
-    url(r'^sitemap-(?P<section>.+)\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': base_sitemaps}),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.index', {
+        'sitemaps': base_sitemaps}),
+    url(r'^sitemap-(?P<section>.+)\.xml$',
+        'django.contrib.sitemaps.views.sitemap', {'sitemaps': base_sitemaps}),
 ]
 
 # Prefix Oscar URLs with language codes
