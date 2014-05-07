9ffc450e690e5309cc615b1c327d03e2d20da285
David Winterbottom <david.winterbottom@gmail.com>
2014-05-07 13:16:28 +0100

    Improve sandbox gateway page
    - Use i18n URLs
    - Add breadcrumbs

--- sites/sandbox/urls.py
+++ sites/sandbox/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
@@ -16,8 +16,6 @@ urlpatterns = [
     # Include admin as convenience. It's unsupported and you should
     # use the dashboard
     url(r'^admin/', include(admin.site.urls)),
-    # Custom functionality to allow dashboard users to be created
-    url(r'^gateway/', include('apps.gateway.urls')),
     # i18n URLS need to live outside of i18n_patterns scope of the shop
     url(r'^i18n/', include('django.conf.urls.i18n')),
     # include a basic sitemap
@@ -29,6 +27,9 @@ urlpatterns = [
 
 # Prefix Oscar URLs with language codes
 urlpatterns += i18n_patterns('',
+    # Custom functionality to allow dashboard users to be created
+    url(r'gateway/', include('apps.gateway.urls')),
+    # Oscar's normal URLs
     url(r'', include(shop.urls)),
 )
 
