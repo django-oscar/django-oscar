61597e185969198d73f95a9267829a1c6204b436
David Winterbottom <david.winterbottom@gmail.com>
2014-05-21 15:59:42 +0100

    Document the migration testing process better

--- Makefile
+++ Makefile
@@ -70,13 +70,15 @@ coverage:
 lint:
 	./lint.sh
 
+testmigrations:
+	pip install -r requirements_vagrant.txt
+	cd sites/sandbox && ./test_migrations.sh
+
 # It is important that this target only depends on install
 # (instead of upgrade) because we install Django in the .travis.yml
 # and upgrade would overwrite it.  We also build the sandbox as part of this target
 # to catch any errors that might come from that build process.
-travis: install lint coverage sandbox demo
-	pip install -r requirements_vagrant.txt
-	cd sites/sandbox && ./test_migrations.sh
+travis: install lint coverage sandbox demo testmigrations
 
 messages:
 	# Create the .po files used for i18n
--- sites/sandbox/test_migrations.sh
+++ sites/sandbox/test_migrations.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
+# 
+# Test migrations run correctly with MySQL and Postgres
 
-# fail if any command fails
+# Fail if any command fails
 # http://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
 set -e
 set -o pipefail
 
 if [ ! "$TRAVIS" == "true" ]
 then
-  mysql -u root --password="root_password" -e "DROP DATABASE IF EXISTS oscar_vagrant; CREATE DATABASE oscar_vagrant"
-  mysql -u root --password="root_password" -e "GRANT ALL PRIVILEGES ON oscar_vagrant.* TO 'travis'@'%' IDENTIFIED BY '';"
+  # If not on Travis, then create databases
+  echo "Creating MySQL database and user"
+  mysql -u root -e "DROP DATABASE IF EXISTS oscar_vagrant; CREATE DATABASE oscar_vagrant"
+  mysql -u root -e "GRANT ALL PRIVILEGES ON oscar_vagrant.* TO 'travis'@'localhost' IDENTIFIED BY '';"
 
-  sudo -u postgres psql -c "DROP ROLE IF EXISTS travis"
-  sudo -u postgres psql -c "CREATE ROLE travis LOGIN PASSWORD ''"
-  sudo -u postgres psql -c "DROP DATABASE IF EXISTS oscar_vagrant"
-  sudo -u postgres psql -c "CREATE DATABASE oscar_vagrant"
+  echo "Creating Postgres database and user"
+  psql -c "DROP ROLE IF EXISTS travis"
+  psql -c "CREATE ROLE travis LOGIN PASSWORD ''"
+  psql -c "DROP DATABASE IF EXISTS oscar_vagrant"
+  psql -c "CREATE DATABASE oscar_vagrant"
 fi
 
 # MySQL
+echo "Running migrations against MySQL"
 ./manage.py syncdb --noinput --settings=settings_mysql > /dev/null
 ./manage.py migrate --noinput --settings=settings_mysql
 
 # Postgres
+echo "Running migrations against Postgres"
 ./manage.py syncdb --noinput --settings=settings_postgres > /dev/null
 ./manage.py migrate --noinput --settings=settings_postgres
