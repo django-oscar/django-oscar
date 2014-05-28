5c78392d8fc8422c50b5da4f4b60193d2518e81e
Maik Hoepfel <m@maikhoepfel.de>
2014-05-28 09:51:39 +0100

    Management command to find duplicate email addresses
    Oscar currently treats the local part of an email address in a
    case-sensitive way. That's standards compliant, but the majority of
    users is not aware of it and most SMTP server setups ignore casing.
    
    In preparation for switching to a case-insensitive handling of email
    addresses, this commit adds a management command to find duplicate email
    addressses. Casing is ignored.
    
    Related to #1179.

---
+++ oscar/management/commands/oscar_find_duplicate_emails.py
@@ -0,0 +1,28 @@
+from collections import Counter
+
+from django.core.management.base import BaseCommand
+from oscar.core.compat import get_user_model
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = ('Finds email addresses that are used by more than one user. '
+            'Casing is ignored.')
+
+    def handle(self, *args, **options):
+        emails = User.objects.values_list('email', flat=True)
+        emails = map(lambda x: x.lower(), emails)
+        duplicates = sorted([
+            (email, count) for email, count in Counter(emails).most_common()
+            if count > 1])
+        if duplicates:
+            for email, __ in duplicates:
+                users = User.objects.filter(email__iexact=email)
+                user_strings = [
+                    "{} (#{})".format(user.username, user.pk)
+                    for user in users]
+                print("{email} is assigned to: {pk_list}".format(
+                    email=email, pk_list=u", ".join(user_strings)))
+        else:
+            print("No duplicate email addresses found!")
