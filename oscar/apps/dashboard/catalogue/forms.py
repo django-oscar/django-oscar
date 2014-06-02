35cf61deb0687fcc5624d91fa372ab62b22a03df
David Winterbottom <david.winterbottom@gmail.com>
2014-06-02 10:58:39 +0100

    Rework dashboard catalogue form to pass product_class explicitly
    It's nasty to hang things on self so that multiple methods have access
    to them. Better to pass them explicitly.

--- oscar/apps/dashboard/catalogue/forms.py
+++ oscar/apps/dashboard/catalogue/forms.py
@@ -282,10 +282,9 @@ class ProductForm(forms.ModelForm):
         }
 
     def __init__(self, product_class, data=None, *args, **kwargs):
-        self.product_class = product_class
-        self.set_initial_attribute_values(kwargs)
+        self.set_initial_attribute_values(product_class, kwargs)
         super(ProductForm, self).__init__(data, *args, **kwargs)
-        self.instance.product_class = self.product_class
+        self.instance.product_class = product_class
 
         # Set the initial value of the is_group field.  This isn't watertight:
         # if the product is intended to be a parent product but doesn't have
@@ -313,12 +312,16 @@ class ProductForm(forms.ModelForm):
             self.fields['title'].widget = forms.TextInput(
                 attrs={'autocomplete': 'off'})
 
-    def set_initial_attribute_values(self, kwargs):
+    def set_initial_attribute_values(self, product_class, kwargs):
+        """
+        Update the kwargs['initial'] value to have the initial values based on
+        the product instance's attributes
+        """
         if kwargs.get('instance', None) is None:
             return
         if 'initial' not in kwargs:
             kwargs['initial'] = {}
-        for attribute in self.product_class.attributes.all():
+        for attribute in product_class.attributes.all():
             try:
                 value = kwargs['instance'].attribute_values.get(
                     attribute=attribute).value
@@ -328,7 +331,7 @@ class ProductForm(forms.ModelForm):
                 kwargs['initial']['attr_%s' % attribute.code] = value
 
     def add_attribute_fields(self, is_parent=False):
-        for attribute in self.product_class.attributes.all():
+        for attribute in self.instance.product_class.attributes.all():
             self.fields['attr_%s' % attribute.code] \
                 = self.get_attribute_field(attribute)
             # Attributes are not required for a parent product
@@ -358,7 +361,7 @@ class ProductForm(forms.ModelForm):
         Set product class and attributes before saving
         """
         product = super(ProductForm, self).save(commit=False)
-        for attribute in self.product_class.attributes.all():
+        for attribute in self.instance.product_class.attributes.all():
             value = self.cleaned_data['attr_%s' % attribute.code]
             setattr(product.attr, attribute.code, value)
         product.save()
