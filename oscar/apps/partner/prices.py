68b25681743e671c10f95ce0ccd1b8beae8700fe
Maik Hoepfel <m@maikhoepfel.de>
2014-06-03 12:44:25 +0100

    Update Base pricing policy interface
    This change achieves two things:
    - Pricing policies now are mostly concerned with returning a
      oscar.core.prices.Price instance instead of exposing attributes
      of a price directly
    - Prices are now returned dependent on the quantity, which allows for
      implementation of non-linear prices.
    
    Down the rabbit hole we go!

--- oscar/apps/partner/prices.py
+++ oscar/apps/partner/prices.py
@@ -12,26 +12,42 @@ class Base(object):
     #: Whether tax is known
     is_tax_known = False
 
-    #: Price excluding tax
-    excl_tax = None
-
-    #: Price including tax
-    incl_tax = None
-
-    #: Price to use for offer calculations
-    @property
-    def effective_price(self):
-        # Default to using the price excluding tax for calculations
-        return self.excl_tax
-
-    #: Price tax
-    tax = None
-
     #: Price currency (3 char code)
     currency = None
 
+    #: Price for single unit to use for offer calculations
+    #: Note that offers app currently won't work with non-linear prices!
+    @property
+    def effective_price(self):
+        # Default to using the price excluding tax for calculations
+        return self.get_price().excl_tax
+
+    def get_price(self, quantity=1):
+        """
+        Returns a oscar.core.prices.Price instance for a given quantity.
+        """
+        return prices.Price(currency=None, excl_tax=None)
+
     def __repr__(self):
         return "%s(%r)" % (self.__class__.__name__, self.__dict__)
+
+    ## Deprecated attributes ##
+    # TODO Raise DeprecationWarning
+
+    #: Price for single unit excluding tax
+    @property
+    def excl_tax(self):
+        return self.get_price().excl_tax
+
+    #: Price for single unit including tax
+    @property
+    def incl_tax(self):
+        return self.get_price().incl_tax
+
+    #: Price tax for single unit
+    @property
+    def tax(self):
+        return self.get_price().tax
 
 
 class Unavailable(Base):
