a5d31b704d74861aa35ef04826877399bdb7cb10
David Winterbottom <david.winterbottom@gmail.com>
2014-06-10 12:50:49 +0100

    Adjust demo site shipping for 0.8 shipping changes

--- sites/demo/apps/shipping/repository.py
+++ sites/demo/apps/shipping/repository.py
@@ -3,5 +3,7 @@ from oscar.apps.shipping import repository
 from . import methods
 
 
+# Override shipping repository in order to provide our own two
+# custom methods
 class Repository(repository.Repository):
-    methods = (methods.Standard, methods.Express)
+    methods = (methods.Standard(), methods.Express())
