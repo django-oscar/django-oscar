47cb430d2ad16487a21ee77e8ec74118336c2faa
Maik Hoepfel <m@maikhoepfel.de>
2014-06-12 13:54:13 +0100

    Revert to costly method of recording a user's order for analytics
    Django's F() expressions don't work with decimals; I'm assuming
    DecimalFields are represented as strings internally and it would be
    unsupported by the database backends.
    
    Reverting to pre-84f520e9 fixes the problem. As orders are comparitively
    rare, this is not a major issue.

--- oscar/apps/analytics/receivers.py
+++ oscar/apps/analytics/receivers.py
@@ -51,26 +51,28 @@ def _record_products_in_order(order):
 
 
 def _record_user_order(user, order):
-    try:
-        record = UserRecord.objects.filter(user=user)
-        affected = record.update(
-            num_orders=F('num_orders') + 1,
-            num_order_lines=F('num_order_lines') + order.num_lines,
-            num_order_items=F('num_order_items') + order.num_items,
-            total_spent=F('total_spent') + order.total_incl_tax,
-            date_last_order=order.date_placed)
-        if not affected:
-            UserRecord.objects.create(
-                user=user, num_orders=1, num_order_lines=order.num_lines,
-                num_order_items=order.num_items,
-                total_spent=order.total_incl_tax,
-                date_last_order=order.date_placed)
-    except IntegrityError:
-        logger.error(
-            "IntegrityError in analytics when recording a user order.")
+    """
+    Records a user's order
 
+    Note: Unfortunately, Django's F() expressions aren't working with decimals.
+          So we have to go down the costly route of fetching the record and
+          updating it.
+    """
+    if user.is_authenticated():
+        try:
+            record, __ = UserRecord.objects.get_or_create(user=user)
+            record.num_orders += 1
+            record.num_order_lines += order.num_lines
+            record.num_order_items += order.num_items
+            record.total_spent += order.total_incl_tax
+            record.date_last_order = order.date_placed
+            record.save()
+        except IntegrityError:
+            logger.error(
+                "IntegrityError in analytics when recording a user order.")
 
 # Receivers
+
 
 @receiver(product_viewed)
 def receive_product_view(sender, product, user, **kwargs):
