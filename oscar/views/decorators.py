7166914c7d228fc7127a5e9b37807dcef8eb124c
Maik Hoepfel <m@maikhoepfel.de>
2014-06-19 14:13:49 +0100

    [Backport] Support for methods on User model when checking permissions
    The ``permissions_required`` decorator, and hence
    ``Application.permissions_map`` and
    ``Application.default_permissions``, now
    handle both methods and properties on the User model.
    Previously, it
    wasn't supported, but a docstring showed
    ``is_anonymous`` as an example,
    which is a method.
    
    Cherry-picked from 8b610f66cd79b38f2a12509a44f37490117f6267

--- oscar/views/decorators.py
+++ oscar/views/decorators.py
@@ -55,7 +55,7 @@ def check_permissions(user, permissions):
     Permissions can be a list or a two-tuple of lists. If it is a two-tuple,
     both permission lists will be evaluated and the outcome will be or'ed.
     Each item of the list(s) must be either a valid Django permission name
-    (model.codename) or an attribute on the User model
+    (model.codename) or a property or method on the User model
     (e.g. 'is_active', 'is_superuser').
 
     Example usage:
@@ -69,13 +69,18 @@ def check_permissions(user, permissions):
     def _check_one_permission_list(perms):
         regular_permissions = [perm for perm in perms if '.' in perm]
         conditions = [perm for perm in perms if '.' not in perm]
-        if conditions and ['is_active', 'is_anonymous'] not in conditions:
-            # always check for is_active where appropriate
+        # always check for is_active if not checking for is_anonymous
+        if (conditions and
+                'is_anonymous' not in conditions and
+                'is_active' not in conditions):
             conditions.append('is_active')
-        passes_conditions = all([getattr(user, perm) for perm in conditions])
+        attributes = [getattr(user, perm) for perm in conditions]
+        # evaluates methods, explicitly casts properties to booleans
+        passes_conditions = all([
+            attr() if callable(attr) else bool(attr) for attr in attributes])
         return passes_conditions and user.has_perms(regular_permissions)
 
-    if permissions is None:
+    if not permissions:
         return True
     elif isinstance(permissions, list):
         return _check_one_permission_list(permissions)
