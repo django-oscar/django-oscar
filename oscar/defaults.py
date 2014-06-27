0cc0518cc2a94ccb794f8d2c81c553c71c69fa88
Maik Hoepfel <m@maikhoepfel.de>
2014-06-27 17:04:53 +0100

    Allow disabling deletion of image files and thumbnails
    Oscar by default deleted image files and thumbnails. That is usually
    fine, but has been an issue when using remote storages like S3. This
    change also makes the only remaining import from sorl optional, hence
    paving the way to making it completely optional.
    
    Fixes #1373.

--- oscar/defaults.py
+++ oscar/defaults.py
@@ -22,6 +22,7 @@ OSCAR_DEFAULT_CURRENCY = 'GBP'
 # Paths
 OSCAR_IMAGE_FOLDER = 'images/products/%Y/%m/'
 OSCAR_PROMOTION_FOLDER = 'images/promotions/'
+OSCAR_DELETE_IMAGE_FILES = True
 
 # Copy this image from oscar/static/img to your MEDIA_ROOT folder.
 # It needs to be there so Sorl can resize it.
