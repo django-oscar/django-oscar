40bd5b4639d2c170354cb756e1a0881110d48ee1
Michael van Tellingen <m.vantellingen@lukkien.com>
2014-06-29 22:03:36 +0200

    Use altered StockRecord model in tests
    The test suite brings it's own stock record model for testing. Django
    1.7 requires that we only import models from apps in INSTALLED_APPS, so
    we need to use it to avoid clashes.

--- oscar/test/newfactories.py
+++ oscar/test/newfactories.py
@@ -22,6 +22,8 @@ from oscar.apps.partner import models as partner_models, strategy
 from oscar.apps.voucher import models as voucher_models
 from oscar.core.compat import get_user_model
 
+from tests._site.apps.partner.models import StockRecord
+
 __all__ = ["UserFactory", "CountryFactory", "UserAddressFactory",
            "BasketFactory", "VoucherFactory", "ProductFactory",
            "StockRecordFactory", "ProductAttributeFactory",
@@ -88,7 +90,7 @@ class PartnerFactory(factory.DjangoModelFactory):
 
 
 class StockRecordFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = partner_models.StockRecord
+    FACTORY_FOR = StockRecord
 
     partner = factory.SubFactory(PartnerFactory)
     partner_sku = factory.Sequence(lambda n: 'unit%d' % n)
