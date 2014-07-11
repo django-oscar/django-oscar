d4243c50deb4e75d73ed3e47d4b174cc01ed3ca5
Maik Hoepfel <m@maikhoepfel.de>
2014-07-11 16:11:49 +0100

    Tweaks to Country model
    * Added some docstrings because I kept having to look up details
      of the implementation
    * Removed index on the alpha3 and numeric code because we never use
      those fields for lookups
    * Turned the numeric field into a CharField as suggested

--- oscar/apps/address/abstract_models.py
+++ oscar/apps/address/abstract_models.py
@@ -376,24 +376,30 @@ class AbstractAddress(models.Model):
 class AbstractCountry(models.Model):
     """
     International Organization for Standardization (ISO) 3166-1 Country list.
+
+    The field names are a bit awkward, but kept for backwards compatibility.
+    pycountry's syntax of alpha2, alpha3, name and official_name seems sane.
     """
-    iso_3166_1_a2 = models.CharField(_('ISO 3166-1 alpha-2'), max_length=2,
-                                     primary_key=True)
-    iso_3166_1_a3 = models.CharField(_('ISO 3166-1 alpha-3'), max_length=3,
-                                     blank=True, db_index=True)
-    # This should have been a CharField as it needs to be padded with zeros to
-    # be 3 digits.  Access via the numeric_code instead.
-    iso_3166_1_numeric = models.PositiveSmallIntegerField(
-        _('ISO 3166-1 numeric'), null=True, db_index=True)
-    name = models.CharField(_('Official name (CAPS)'), max_length=128)
+    iso_3166_1_a2 = models.CharField(
+        _('ISO 3166-1 alpha-2'), max_length=2, primary_key=True)
+    iso_3166_1_a3 = models.CharField(
+        _('ISO 3166-1 alpha-3'), max_length=3, blank=True)
+    iso_3166_1_numeric = models.CharField(
+        _('ISO 3166-1 numeric'), blank=True, max_length=3)
+
+    #: The commonly used name
+    #: e.g. 'United Kingdom'
     printable_name = models.CharField(_('Country name'), max_length=128)
+    #: The full official name of a country
+    #: e.g. 'United Kingdom of Great Britain and Northern Ireland'
+    name = models.CharField(_('Official name'), max_length=128)
 
     display_order = models.PositiveSmallIntegerField(
         _("Display order"), default=0, db_index=True,
         help_text=_('Higher the number, higher the country in the list.'))
 
-    is_shipping_country = models.BooleanField(_("Is Shipping Country"),
-                                              default=False, db_index=True)
+    is_shipping_country = models.BooleanField(
+        _("Is shipping country"), default=False, db_index=True)
 
     class Meta:
         abstract = True
@@ -407,13 +413,21 @@ class AbstractCountry(models.Model):
     @property
     def code(self):
         """
-        Shorthand for the ISO 3166 code
+        Shorthand for the ISO 3166 Alpha-2 code
         """
         return self.iso_3166_1_a2
 
     @property
     def numeric_code(self):
-        return u"%.03d" % self.iso_3166_1_numeric
+        """
+        Shorthand for the ISO 3166 numeric code.
+
+        iso_3166_1_numeric used to wrongly be a integer field, but has to be
+        padded with leading zeroes. It's since been converted to a char field,
+        but the database might still contain non-padded strings. That's why
+        the padding is kept.
+        """
+        return u"%.03d" % int(self.iso_3166_1_numeric)
 
 
 class AbstractShippingAddress(AbstractAddress):
