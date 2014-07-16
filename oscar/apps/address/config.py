8f1f56ed22b6767444f6981411ea3291e4ed1f10
Maik Hoepfel <m@maikhoepfel.de>
2014-07-16 15:03:38 +0100

    Add translatable verbose names to all app configs
    That way, the app names can be localised, which e.g. makes the Django
    admin interface nicer.

--- oscar/apps/address/config.py
+++ oscar/apps/address/config.py
@@ -1,7 +1,9 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class AddressConfig(AppConfig):
     label = 'address'
     name = 'oscar.apps.address'
+    verbose_name = _('Address')
 
--- oscar/apps/customer/config.py
+++ oscar/apps/customer/config.py
@@ -1,10 +1,12 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class CustomerConfig(AppConfig):
     label = 'customer'
     name = 'oscar.apps.customer'
+    verbose_name = _('Customer')
 
     def ready(self):
-        from oscar.apps.customer.history import *  # noqa
+        from oscar.apps.customer import history  # noqa
         from .alerts import receivers  # noqa
