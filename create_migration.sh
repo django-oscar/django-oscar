642846087f3f7a3b9ad50ca1e4bcdb3d0c9ff82f
David Winterbottom <david.winterbottom@gmail.com>
2014-07-23 15:07:22 +0100

    Improve create_migration script
    It now:
    
    - Restores the previous versions of Django and South
    - Uses a bash array to reduce duplication
    
    (Took the bait from 722c058e5ae1b5a3ac641ec84f42d2d972a5d483)

--- create_migration.sh
+++ create_migration.sh
@@ -1,39 +1,37 @@
 #!/usr/bin/env bash
 #
-# Rather verbose and destructive script to create both South migrations and
-# the new native migrations. This will install and uninstall Django versions
-# in your virtualenv, only work with the default SQLite database, destroy that
-# database repeatedly
+# Rather verbose and destructive script to create both South migrations and the
+# new native migrations (which we need as we support both Django 1.6 and 1.7).
+# This will install and uninstall Django versions in your virtualenv, only work
+# with the default SQLite database, destroy that database repeatedly
 
+# Grab current version of Django from virtualenv
+DJANGO_VERSION=$(pip freeze | awk 'BEGIN {FS="=="} /Django/ {print $2}')
+SOUTH_VERSION=$(pip freeze | awk 'BEGIN {FS="=="} /South/ {print $2}')
+
+APPS=( analytics checkout address shipping catalogue reviews partner basket payment \
+       offer order customer promotions search voucher wishlists )
+
+echo "Uninstalling Django(==$DJANGO_VERSION) and South(==$SOUTH_VERSION)"
 pip uninstall Django South -y
 
+echo "Generating Django-native (>=1.7) migrations"
 # Commit of the stable/1.7.x branch that contains fix for https://code.djangoproject.com/ticket/23041,
 # but doesn't suffer from https://code.djangoproject.com/ticket/23014
 pip install https://github.com/django/django/archive/88135a8cf7d587b88e47f1223cf01c7698b52b74.zip
 rm -f sites/sandbox/db.sqlite
 sites/sandbox/manage.py migrate
-sites/sandbox/manage.py makemigrations analytics checkout address shipping catalogue reviews partner basket payment offer order customer promotions search voucher wishlists
+sites/sandbox/manage.py makemigrations ${APPS[@]}
 
-pip uninstall Django -y
-pip install Django==1.6.5 South==1.0
+echo "Generating Django 1.6 migrations"
+pip install "Django==1.6.5" "South==1.0"
 rm -f sites/sandbox/db.sqlite
 sites/sandbox/manage.py syncdb --noinput
 sites/sandbox/manage.py migrate
-sites/sandbox/manage.py schemamigration analytics --auto
-sites/sandbox/manage.py schemamigration checkout --auto
-sites/sandbox/manage.py schemamigration address --auto
-sites/sandbox/manage.py schemamigration shipping --auto
-sites/sandbox/manage.py schemamigration catalogue --auto
-sites/sandbox/manage.py schemamigration reviews --auto
-sites/sandbox/manage.py schemamigration partner --auto
-sites/sandbox/manage.py schemamigration basket --auto
-sites/sandbox/manage.py schemamigration payment --auto
-sites/sandbox/manage.py schemamigration offer --auto
-sites/sandbox/manage.py schemamigration order --auto
-sites/sandbox/manage.py schemamigration customer --auto
-sites/sandbox/manage.py schemamigration promotions --auto
-sites/sandbox/manage.py schemamigration search --auto
-sites/sandbox/manage.py schemamigration voucher --auto
-sites/sandbox/manage.py schemamigration wishlists --auto
+for APP in "${APPS[@]}"
+do
+    sites/sandbox/manage.py schemamigration $APP --auto
+done
 
-pip uninstall Django -y
+echo "Restoring Django(==$DJANGO_VERSION) and South(==$SOUTH_VERSION)"
+pip install "Django==$DJANGO_VERSION" "South==$SOUTH_VERSION"
