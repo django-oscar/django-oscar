97b6efb055e2ef35e326caf6f95400571e290899
Maik Hoepfel <m@maikhoepfel.de>
2014-08-07 18:03:36 +0100

    Correct amount calculation when creating payment events in dashboard
    The final line price was calculated as qty * qty * unit price, as the
    line price was multiplied with the quantity for a second time. That's no
    good.
    
    Reported in #1442. Fixes #1442.

--- tests/functional/dashboard/order_tests.py
+++ tests/functional/dashboard/order_tests.py
@@ -7,7 +7,8 @@ from django_dynamic_fixture import get, G
 
 from oscar.test.testcases import WebTestCase
 from oscar.test.factories import create_order, create_basket
-from oscar.apps.order.models import Order, OrderNote
+from oscar.apps.order.models import Order, OrderNote, PaymentEvent, \
+    PaymentEventType
 from oscar.core.compat import get_user_model
 
 
@@ -128,6 +129,7 @@ class TestOrderDetailPage(WebTestCase):
         super(TestOrderDetailPage, self).setUp()
         # ensures that initial statuses are as expected
         self.order = create_order()
+        self.event_type = PaymentEventType.objects.create(name='foo')
         url = reverse('dashboard:order-detail',
                       kwargs={'number': self.order.number})
         self.page = self.get(url)
@@ -167,6 +169,16 @@ class TestOrderDetailPage(WebTestCase):
         # fetch order again
         self.assertEqual(Order.objects.get(pk=self.order.pk).status, new_status)
 
+    def test_allows_creating_payment_event(self):
+        line = self.order.lines.all()[0]
+        form = self.page.forms['order_lines_form']
+        form['line_action'] = 'create_payment_event'
+        form['selected_line'] = [line.pk]
+        form['payment_event_type'] = self.event_type.code
+        form.submit()
+
+        self.assertTrue(PaymentEvent.objects.exists())
+
 
 class TestChangingOrderStatus(WebTestCase):
     is_staff = True
@@ -196,6 +208,7 @@ class TestChangingOrderStatus(WebTestCase):
         self.assertEqual(1, len(notes))
         self.assertEqual(OrderNote.SYSTEM, notes[0].note_type)
 
+
 class TestChangingOrderStatusFromFormOnOrderDetailVew(WebTestCase):
     is_staff = True
 
@@ -224,6 +237,7 @@ class TestChangingOrderStatusFromFormOnOrderDetailVew(WebTestCase):
         notes = self.order.notes.all()
         self.assertEqual(1, len(notes))
         self.assertEqual(OrderNote.SYSTEM, notes[0].note_type)
+
 
 class LineDetailTests(WebTestCase):
     is_staff = True
