6f3d530a15851f0726004ef19cdb373089cb7aff
David Winterbottom <david.winterbottom@gmail.com>
2014-09-02 16:56:44 +0100

    Fix tests for order status updating
    The hidden widget means the tests have less set-up to do.

--- tests/functional/dashboard/order_tests.py
+++ tests/functional/dashboard/order_tests.py
@@ -209,20 +209,19 @@ class TestChangingOrderStatus(WebTestCase):
         self.assertEqual(OrderNote.SYSTEM, notes[0].note_type)
 
 
-class TestChangingOrderStatusFromFormOnOrderDetailVew(WebTestCase):
+class TestChangingOrderStatusFromFormOnOrderListView(WebTestCase):
     is_staff = True
 
     def setUp(self):
-        super(TestChangingOrderStatusFromFormOnOrderDetailVew, self).setUp()
+        super(TestChangingOrderStatusFromFormOnOrderListView, self).setUp()
 
-        Order.pipeline = {'A': ('B', 'C'), 'B': ('A', 'C'),'C': ('A', 'B')}
+        Order.pipeline = {'A': ('B', 'C'), 'B': ('A', 'C'), 'C': ('A', 'B')}
         self.order = create_order(status='A')
         url = reverse('dashboard:order-list')
 
         page = self.get(url)
         form = page.forms['orders_form']
         form['new_status'] = 'B'
-        form.set('action', True, index=1)
         form['selected_order'] = self.order.pk
         self.response = form.submit()
 
