8ac9b7a8378abb94be3f644499d0b019c72ee15b
David Winterbottom <david.winterbottom@gmail.com>
2014-09-10 15:19:01 +0100

    Correct typo in demo form meta

--- sites/demo/apps/checkout/forms.py
+++ sites/demo/apps/checkout/forms.py
@@ -19,7 +19,7 @@ class BillingAddressForm(payment_forms.BillingAddressForm):
 
     class Meta(payment_forms.BillingAddressForm):
         model = BillingAddress
-        exclude = ('serach_text', 'first_name', 'last_name')
+        exclude = ('search_text', 'first_name', 'last_name')
 
     def __init__(self, shipping_address, data=None, *args, **kwargs):
         # Store a reference to the shipping address
