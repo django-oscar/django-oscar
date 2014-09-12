f7d88dca46080aefef123348b9539514543d27cb
Sergey Maranchuk <slav0nic0@gmail.com>
2014-09-12 12:23:28 +0300

    correct last commit

--- oscar/apps/payment/abstract_models.py
+++ oscar/apps/payment/abstract_models.py
@@ -273,8 +273,8 @@ class AbstractBankcard(models.Model):
         self.issue_number = kwargs.pop('issue_number', None)
         self.ccv = kwargs.pop('ccv', None)
         super(AbstractBankcard, self).__init__(*args, **kwargs)
-        self.card_type = bankcards.bankcard_type(self.number)
         if self.id is None:
+            self.card_type = bankcards.bankcard_type(self.number)
             if self.card_type is None:
                 self.card_type = 'Unknown card type'
 
