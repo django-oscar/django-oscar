f17060c0f3a83acda5e977c01fce01cd9962081b
Maik Hoepfel <m@maikhoepfel.de>
2014-10-08 11:08:59 +0100

    Bump Haystack version to avoid Django version conflict
    The problem is detailed in #1480. We need to require Haystack > 2.3.1 to
    avoid it clashing with our support for Django 1.7.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(name='django-oscar',
           # https://github.com/AndrewIngram/django-extra-views/pull/85
           'django-extra-views>=0.2,<0.6.5',
           # Search support
-          'django-haystack>=2.2.0,<2.3.0',
+          'django-haystack>=2.3.1,<2.4.0',
           # Treebeard is used for categories
           'django-treebeard==2.0',
           # Sorl is used as the default thumbnailer
