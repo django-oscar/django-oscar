27dd7fe119c373abd1c62be628949c34175ad350
Edvinas Jurevicius <ikonitas@gmail.com>
2014-10-12 18:57:29 +0100

    Update choices wording for review form

--- oscar/apps/catalogue/reviews/choice_lists.py
+++ oscar/apps/catalogue/reviews/choice_lists.py
@@ -4,8 +4,8 @@
 from django.utils.translation import ugettext_lazy as _
 
 
-SORT_BY_SCORE = _('Score')
-SORT_BY_RECENCY = _('Recency')
+SORT_BY_SCORE = 'Score'
+SORT_BY_RECENCY = 'Recency'
 SORT_REVIEWS_BY_CHOICES = (
     (SORT_BY_SCORE, _('Score')),
     (SORT_BY_RECENCY, _('Recency')),
