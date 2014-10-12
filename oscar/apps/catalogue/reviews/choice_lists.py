a3bd327f0837a8fda185dad477e130e5d344bd29
Edvinas Jurevicius <ikonitas@gmail.com>
2014-10-12 15:14:00 +0100

    Change choices for review form more friendly

--- oscar/apps/catalogue/reviews/choice_lists.py
+++ oscar/apps/catalogue/reviews/choice_lists.py
@@ -4,8 +4,8 @@
 from django.utils.translation import ugettext_lazy as _
 
 
-SORT_BY_SCORE = 1
-SORT_BY_RECENCY = 2
+SORT_BY_SCORE = _('Score')
+SORT_BY_RECENCY = _('Recency')
 SORT_REVIEWS_BY_CHOICES = (
     (SORT_BY_SCORE, _('Score')),
     (SORT_BY_RECENCY, _('Recency')),
