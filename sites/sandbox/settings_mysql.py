57ac90fbea95baa9f87fd8ddb3924ae20f80367d
David Winterbottom <david.winterbottom@gmail.com>
2014-10-14 10:47:29 +0100

    Change database name to oscar_travis for migration testing
    oscar_vagrant was confusing.

--- sites/sandbox/settings_mysql.py
+++ sites/sandbox/settings_mysql.py
@@ -1,12 +1,12 @@
 import pymysql
 pymysql.install_as_MySQLdb()
 
-from settings import *
+from settings import *  # noqa
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'oscar_vagrant',
+        'NAME': 'oscar_travis',
         'USER': 'travis',
         'PASSWORD': '',
         'HOST': 'localhost',
