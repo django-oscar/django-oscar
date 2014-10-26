4d871d98e2c0a51f1ae428beb546c052d82d87c0
Michael van Tellingen <m.vantellingen@lukkien.com>
2014-10-26 13:32:03 +0100

    Add __all__ to models module of overridable apps
    Since the recommended way to override models within an app is to add
    ‘from oscar.apps.order.models import *’ we need to limit what is 
    exported otherwise subtle bugs may occur in the users code.
    
    Simply defining __all__ [‘Model1’, ‘Model2’] doesn’t work since Django
    will raise an AppRegistryNotReady exception for all models listed in
    the __all__ list but are not available. Dynamically building the list
    solves this.

--- oscar/apps/address/models.py
+++ oscar/apps/address/models.py
@@ -2,12 +2,18 @@ from oscar.core.loading import is_model_registered
 from oscar.apps.address.abstract_models import (
     AbstractUserAddress, AbstractCountry)
 
+__all__ = []
+
 
 if not is_model_registered('address', 'UserAddress'):
     class UserAddress(AbstractUserAddress):
         pass
 
+    __all__.append('UserAddress')
+
 
 if not is_model_registered('address', 'Country'):
     class Country(AbstractCountry):
         pass
+
+    __all__.append('Country')
--- oscar/apps/basket/models.py
+++ oscar/apps/basket/models.py
@@ -2,6 +2,10 @@ from oscar.core.loading import is_model_registered
 from oscar.apps.basket.abstract_models import (
     AbstractBasket, AbstractLine, AbstractLineAttribute)
 
+__all__ = [
+    'InvalidBasketLineError',
+]
+
 
 class InvalidBasketLineError(Exception):
     pass
@@ -11,12 +15,18 @@ if not is_model_registered('basket', 'Basket'):
     class Basket(AbstractBasket):
         pass
 
+    __all__.append('Basket')
+
 
 if not is_model_registered('basket', 'Line'):
     class Line(AbstractLine):
         pass
 
+    __all__.append('Line')
+
 
 if not is_model_registered('basket', 'LineAttribute'):
     class LineAttribute(AbstractLineAttribute):
         pass
+
+    __all__.append('LineAttribute')
