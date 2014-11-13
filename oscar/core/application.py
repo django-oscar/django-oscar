7a7090d8cc3afacf56715eb437c89b5cff811832
Maik Hoepfel <m@maikhoepfel.de>
2014-11-13 11:48:13 +0100

    Improve docs for Application.permissions_map
    This just led to confusion, so I decided to improve the docs a bit.
    
    We really should be using Pytohn sets instead of lists for this.

--- oscar/core/application.py
+++ oscar/core/application.py
@@ -15,7 +15,12 @@ class Application(object):
     #: A name that allows the functionality within this app to be disabled
     hidable_feature_name = None
 
-    #: Maps view names to a tuple or list of permissions
+    #: Maps view names to lists of permissions. We expect tuples of 
+    #: lists as dictionary values. A list is a set of permissions that all 
+    #: needto be fulfilled (AND). Only one set of permissions has to be
+    #: fulfilled (OR).
+    #: If there's only one set of permissions, as a shortcut, you can also
+    #: just define one list.
     permissions_map = {}
 
     #: Default permission for any view not in permissions_map
