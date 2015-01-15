64e2878c96feb04773089b784557b8ceaf15b0be
Mike Dingjan <m.dingjan@lukkien.com>
2015-01-15 16:57:17 +0100

    allow cookies to be set as secure cookie.

--- oscar/apps/basket/middleware.py
+++ oscar/apps/basket/middleware.py
@@ -84,7 +84,8 @@ class BasketMiddleware(object):
             cookie = self.get_basket_hash(request.basket.id)
             response.set_cookie(
                 cookie_key, cookie,
-                max_age=settings.OSCAR_BASKET_COOKIE_LIFETIME, httponly=True)
+                max_age=settings.OSCAR_BASKET_COOKIE_LIFETIME,
+                secure=settings.OSCAR_BASKET_COOKIE_SECURE, httponly=True)
         return response
 
     def get_cookie_key(self, request):
