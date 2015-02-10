4491e5bdd59a7cea6551569070c3402cd92b1cd8
Jonas Borges <jonasborgesalves@gmail.com>
2015-02-10 20:41:14 -0200

    Improve the code to fix #1647.

--- src/oscar/apps/voucher/abstract_models.py
+++ src/oscar/apps/voucher/abstract_models.py
@@ -79,7 +79,7 @@ class AbstractVoucher(models.Model):
         test_datetime = test_datetime or timezone.now()
         return self.start_datetime <= test_datetime <= self.end_datetime
 
-    def is_available_to_user(self, user=None, no_message_on_result=None):
+    def is_available_to_user(self, user=None):
         """
         Test whether this voucher is available to the passed user.
 
@@ -104,8 +104,6 @@ class AbstractVoucher(models.Model):
                 if not is_available:
                     message = _("You have already used this voucher in "
                                 "a previous order")
-        if no_message_on_result:
-            return is_available
                 
         return is_available, message
 
