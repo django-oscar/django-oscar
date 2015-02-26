18e2186b17f4d355b487d7472fd79497d8267b28
John Parton <john.parton@plushrugs.com>
2015-02-26 11:29:12 -0600

    Fix false-negative for UPCs when UPC matches a variant.

--- oscar/apps/dashboard/catalogue/views.py
+++ oscar/apps/dashboard/catalogue/views.py
@@ -115,13 +115,9 @@ class ProductListView(SingleTableMixin, generic.TemplateView):
         data = self.form.cleaned_data
 
         if data.get('upc'):
-            # If there's an exact UPC match, it returns just the matched
-            # product. Otherwise does a broader icontains search.
-            qs_match = queryset.filter(upc=data['upc'])
-            if qs_match.exists():
-                queryset = qs_match
-            else:
-                queryset = queryset.filter(upc__icontains=data['upc'])
+            # Filter the queryset by upc
+            matches_upc = Product.objects.filter(upc__icontains=data['upc'])
+            queryset = queryset.filter( Q(id=matches_upc.values('id')) | Q(id=matches_upc.values('parent_id')) )
 
         if data.get('title'):
             queryset = queryset.filter(title__icontains=data['title'])
