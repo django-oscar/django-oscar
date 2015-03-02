a73df0c895780a1c1c551b1f1bb0c7f30979266f
John Parton <john.parton@plushrugs.com>
2015-03-02 08:11:20 -0600

    Use subquery logic with exists check.

--- oscar/apps/dashboard/catalogue/views.py
+++ oscar/apps/dashboard/catalogue/views.py
@@ -115,14 +115,17 @@ class ProductListView(SingleTableMixin, generic.TemplateView):
         data = self.form.cleaned_data
 
         if data.get('upc'):
-            # If there's an exact UPC match, it returns just the matched
-            # product. Otherwise does a broader icontains search.
-            qs_match = queryset.filter(Q(upc=data['upc']) | Q(parent__upc=data['upc']))
+            # Filter the queryset by upc
+            # If there's an exact match, return it, otherwise return results
+            # that contain the UPC
+            matches_upc = Product.objects.filter(upc=data['upc'])
+            qs_match = queryset.filter(Q(id=matches_upc.values('id')) | Q(id=matches_upc.values('parent_id')))
 
             if qs_match.exists():
                 queryset = qs_match
             else:
-                queryset = queryset.filter(Q(upc__icontains=data['upc']) | Q(parent__upc__icontains=data['upc']))
+                matches_upc = Product.objects.filter(upc__icontains=data['upc'])
+                queryset = queryset.filter(Q(id=matches_upc.values('id')) | Q(id=matches_upc.values('parent_id')))
 
         if data.get('title'):
             queryset = queryset.filter(title__icontains=data['title'])
