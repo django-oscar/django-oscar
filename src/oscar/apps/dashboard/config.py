9298bfe5d37d5e38eecdee7dfa3808943a12eccb
Markus Bertheau <mbertheau@gmail.com>
2015-04-25 11:08:26 +0200

    Wording changes

--- src/oscar/apps/dashboard/config.py
+++ src/oscar/apps/dashboard/config.py
@@ -5,4 +5,4 @@ from django.utils.translation import ugettext_lazy as _
 class DashboardConfig(AppConfig):
     label = 'dashboard'
     name = 'oscar.apps.dashboard'
-    verbose_name = _('Dashboard')
+    verbose_name = _('Cockpit')
