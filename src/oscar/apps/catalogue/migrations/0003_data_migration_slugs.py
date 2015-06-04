4fbadf8060bbcac27e22607089d6bb1f3e59f1a7
Alexander Gaevsky <sasha@sasha0.ru>
2015-06-04 12:57:52 +0100

    fixed category model import, #1689

--- src/oscar/apps/catalogue/migrations/0003_data_migration_slugs.py
+++ src/oscar/apps/catalogue/migrations/0003_data_migration_slugs.py
@@ -3,17 +3,30 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+from oscar.core.loading import get_model
+
+# Django database migrations require us to fetch the category model
+# via apps.get_model to get an instance of the model at this point
+# in time of migrations. That snapshot does not expose custom
+# properties, only whatever is represented in the migration files.
+# But because for our slug munging below, we need to know the slug
+# separator, which is a custom property, we also load the actual
+# ORM model. We MUST NOT use that to save data, we just fetch
+# the property.
+
+ORMCategory = get_model('catalogue', 'Category')
+
 
 def remove_ancestor_slugs(apps, schema_editor):
-    Category = apps.get_model('catalogue', 'Category')
-    for category in Category.objects.all():
-        category.slug = category.slug.split(Category._slug_separator)[-1]
+    MigrationCategory = apps.get_model('catalogue', 'Category')
+    for category in MigrationCategory.objects.all():
+        category.slug = category.slug.split(ORMCategory._slug_separator)[-1]
         category.save()
 
 
 def add_ancestor_slugs(apps, schema_editor):
-    Category = apps.get_model('catalogue', 'Category')
-    for category in Category.objects.all():
+    MigrationCategory = apps.get_model('catalogue', 'Category')
+    for category in MigrationCategory.objects.all():
         category.slug = category.full_slug
         category.save()
 
