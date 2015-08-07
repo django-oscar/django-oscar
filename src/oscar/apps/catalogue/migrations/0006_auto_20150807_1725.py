f21c6c6a98ca4bfe293441a3d6007dd5862f0397
Dan Helyar <dan.helyar@kitesystems.com>
2015-08-07 10:46:13 +0100

    Add missing migrations for catalogue and customer apps.

---
+++ src/oscar/apps/catalogue/migrations/0006_auto_20150807_1725.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.core.validators
+import oscar.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('catalogue', '0005_auto_20150604_1450'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='productattribute',
+            name='code',
+            field=models.SlugField(max_length=128, verbose_name='Code', validators=[django.core.validators.RegexValidator(regex=b'^[a-zA-Z_][0-9a-zA-Z_]*$', message="Code can only contain the letters a-z, A-Z, digits, and underscores, and can't start with a digit"), oscar.core.validators.non_python_keyword]),
+            preserve_default=True,
+        ),
+    ]
