1bb099634e2ce2f7f8ed1da273574ae5c1c34709
John Parton <john.parton@plushrugs.com>
2015-09-28 12:31:43 -0500

    Use new product factories in strategy_tests.

--- tests/integration/partner/strategy_tests.py
+++ tests/integration/partner/strategy_tests.py
@@ -2,9 +2,8 @@ from django.test import TestCase
 from decimal import Decimal as D
 
 from oscar.apps.partner import strategy
-from oscar.apps.catalogue import models
-from oscar.test import factories
 from oscar.apps.basket.models import Line
+from oscar.test import factories
 
 
 class TestDefaultStrategy(TestCase):
@@ -13,24 +12,24 @@ class TestDefaultStrategy(TestCase):
         self.strategy = strategy.Default()
 
     def test_no_stockrecords(self):
-        product = factories.create_product()
+        product = factories.StandaloneProductFactory(stockrecords=[])
         info = self.strategy.fetch_for_product(product)
         self.assertFalse(info.availability.is_available_to_buy)
         self.assertIsNone(info.price.incl_tax)
 
     def test_one_stockrecord(self):
-        product = factories.create_product(price=D('1.99'), num_in_stock=4)
+        product = factories.StandaloneProductFactory(stockrecords__price_excl_tax=D('1.99'), 
+                                                     stockrecords__num_in_stock=4)
         info = self.strategy.fetch_for_product(product)
         self.assertTrue(info.availability.is_available_to_buy)
         self.assertEqual(D('1.99'), info.price.excl_tax)
         self.assertEqual(D('1.99'), info.price.incl_tax)
 
     def test_product_which_doesnt_track_stock(self):
-        product_class = models.ProductClass.objects.create(
-            name="Digital", track_stock=False)
-        product = factories.create_product(
-            product_class=product_class,
-            price=D('1.99'), num_in_stock=None)
+        product = factories.StandaloneProductFactory(product_class__name="Digital",
+                                                     product_class__track_stock=False,
+                                                     stockrecords__price_excl_tax=D('1.99'), 
+                                                     stockrecords__num_in_stock=None)
         info = self.strategy.fetch_for_product(product)
         self.assertTrue(info.availability.is_available_to_buy)
 
