5c827d0742bfef331a1fb82359a74a373053391c
John Parton <john.parton@plushrugs.com>
2015-10-20 09:44:06 -0500

    Update AbstractEmail.__str__ for case where user is null.

--- src/oscar/apps/customer/abstract_models.py
+++ src/oscar/apps/customer/abstract_models.py
@@ -130,8 +130,12 @@ class AbstractEmail(models.Model):
         verbose_name_plural = _('Emails')
 
     def __str__(self):
-        return _(u"Email to %(user)s with subject '%(subject)s'") % {
-            'user': self.user.get_username(), 'subject': self.subject}
+        if self.user:
+            return _(u"Email to %(user)s with subject '%(subject)s'") % {
+                'user': self.user.get_username(), 'subject': self.subject}
+        else:
+            return _(u"Anonymous email to %(email)s with subject '%(subject)s'") % {
+                'email': self.email, 'subject': self.subject}
 
 
 @python_2_unicode_compatible
