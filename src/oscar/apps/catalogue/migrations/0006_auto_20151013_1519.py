cedf2682142b4816cad8a6287010b6e58ea08765
John Parton <john.parton@plushrugs.com>
2015-12-01 15:25:16 -0600

    Remove code validator from child images migration.

--- src/oscar/apps/catalogue/migrations/0006_auto_20151013_1519.py
+++ src/oscar/apps/catalogue/migrations/0006_auto_20151013_1519.py
@@ -18,9 +18,4 @@ class Migration(migrations.Migration):
             name='child_images',
             field=models.ManyToManyField(related_name='children', verbose_name=b'Child images', to='catalogue.ProductImage', blank=b'True'),
         ),
-        migrations.AlterField(
-            model_name='productattribute',
-            name='code',
-            field=models.SlugField(max_length=128, verbose_name='Code', validators=[django.core.validators.RegexValidator(regex=b'^[a-zA-Z_][0-9a-zA-Z_]*$', message="Code can only contain the letters a-z, A-Z, digits, and underscores, and can't start with a digit."), oscar.core.validators.non_python_keyword]),
-        ),
     ]
