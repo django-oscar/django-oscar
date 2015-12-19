061990409c0f1f15bc4ea78f3b33a21feab0a706
Anentropic <ego@anentropic.com>
2015-12-20 08:34:43 +1300

    add javascript to enable product image sorting in dashboard

--- src/oscar/static/oscar/js/oscar/dashboard.js
+++ src/oscar/static/oscar/js/oscar/dashboard.js
@@ -82,6 +82,8 @@ var oscar = (function(o, $) {
             o.dashboard.initMasks(el);
             o.dashboard.initWYSIWYG(el);
             o.dashboard.initSelects(el);
+            o.dashboard.initSortables(el);
+            o.dashboard.initProductImages(el);
         },
         initMasks: function(el) {
             $(el).find(':input').inputmask();
@@ -223,6 +225,62 @@ var oscar = (function(o, $) {
                 if (!form || $(":invalid", form).length == 0)
                     $(this).button('loading');
             });
+        },
+        initSortables: function(el) {
+            var $sortables = $(el).find('.sortable');
+            $sortables.sortable({
+                cursor: "move",
+                update: function(evt, ui) {
+                    o.dashboard.updateSortFields(evt.target);
+                }
+            });
+            $sortables.disableSelection();
+        },
+        updateSortFields: function(sortableParent) {
+            var $sortableParent = $(sortableParent);
+            var fieldName = $sortableParent.data('fieldName') || "display_order";
+            var $sortFields = $sortableParent.find("input[name$="+fieldName+"]");
+            $sortFields.each(function(i){
+                $(this).val(i);
+            });
+        },
+        initProductImages: function(el) {
+            // convert last 'extra' form into a multi-upload
+            // (assumes `extra=1` in django formset)
+            var $productImages = $('#product_images');
+            var $extraImg = $productImages.find('.upload-image li').last();
+            o.dashboard._extraProductImg = $extraImg.clone();
+
+            $extraImg.find('input[type=file]').attr('multiple', 'multiple');
+
+            $extraImg.children('input,label').remove();
+            $extraImg.find('img[alt="thumbnail"]').remove();
+
+            var $totalForms = $productImages.find("input[name$=TOTAL_FORMS]");
+            $totalForms.val(parseInt($totalForms.val(), 10) - 1);
+
+            // update attrs on last el (don't conflict with future clones)
+            $extraImg.find("div[id^='id_images-'],button[id^=upload_button_id_images-]").each(function(){
+                var $el = $(this);
+                var elAttr = $el.attr('id');
+                if (elAttr) {
+                    var parts = elAttr.split('-');
+                    parts[1] = 'EXTRA';
+                    $el.attr('id', parts.join('-'));
+                }
+            });
+            $extraImg.find("input[id^='id_images-']").each(function(){
+                var $el = $(this);
+                ["id", "name"].forEach(function(attr){
+                    var elAttr = $el.attr(attr);
+                    if (elAttr) {
+                        var parts = elAttr.split('-');
+                        parts[1] = 'EXTRA0';
+                        $el.attr(attr, parts.join('-'));
+                    }
+                });
+            });
+            $extraImg.children('div').first().append('<div class="spent-inputs"></div>');
         },
         offers: {
             init: function() {
@@ -366,17 +424,77 @@ var oscar = (function(o, $) {
                 // image without uploading it. Upload only occures when
                 // submitting the form.
                 if (window.FileReader) {
-                    $('input[type="file"]').change(function(evt) {
-                        var reader = new FileReader();
-                        var imgId = evt.target.id + "-image";
-                        reader.onload = (function() {
-                            return function(e) {
-                                var imgDiv = $("#"+imgId);
-                                imgDiv.children('img').attr('src', e.target.result);
-                            };
-                        })();
-                        reader.readAsDataURL(evt.target.files[0]);
-                    });
+                    function onFileChange(evt) {
+                        var $input = $(evt.target);
+                        if ($input.attr('multiple')) {
+
+                            var $parentTab = $input.parents('.tab-pane').first();
+                            var $extraImg = $input.parents('.upload-image').children('li').last();
+                            var $totalForms = $parentTab.find("input[name$=TOTAL_FORMS]");
+                            var numExisting = parseInt($totalForms.val(), 10);
+
+                            for (i=0; i<evt.target.files.length; i++) {
+                                var $newImg = o.dashboard._extraProductImg.clone();
+                                var index = numExisting + i;
+
+                                // update attrs on cloned el
+                                $newImg.find("[id^='id_images-'],"+
+                                             "[for^='id_images-'],"+
+                                             "[id^='upload_button_id_images-'],"+
+                                             "img[alt='thumbnail']").each(function(){
+                                    var $el = $(this);
+                                    ["id", "name", "for", "onload", "onerror"].forEach(function(attr){
+                                        var val = $el.attr(attr);
+                                        if (val) {
+                                            var parts = val.split('-');
+                                            parts[1] = index;
+                                            $el.attr(attr, parts.join('-'));
+                                        }
+                                    });
+                                });
+                                $newImg.find('#id_images-'+index+'-display_order').val(index);
+                                $newImg.insertBefore($extraImg);
+
+                                var $newFile = $newImg.find('input[type="file"]');
+                                $newFile.change(onFileChange);
+                                $newFile.get(0).files[0] = evt.target.files[i];
+                                // the script-attached files are not submitted by browser
+                                // so we rely on backend receiving files from EXTRAn fields
+                                $newFile.trigger('change');
+                            }
+
+                            if (evt.target.files.length) {
+                                $totalForms.val(parseInt($totalForms.val(), 10) + evt.target.files.length);
+
+                                var $extraFile = $extraImg.find('div.input-field > input[type="file"]').last();
+                                var $newExtraFile = $extraFile.clone();
+                                ["id", "name"].forEach(function(attr){
+                                    var parts = $extraFile.attr(attr).split('-');
+                                    var index = parseInt(parts[1][5]); // EXTRAn
+                                    parts[1] = 'EXTRA' + (index + 1);
+                                    $newExtraFile.attr(attr, parts.join('-'));
+                                });
+                                $newExtraFile.change(onFileChange);
+                                $newExtraFile.insertAfter($extraFile);
+
+                                var $spentDiv = $extraImg.find('.spent-inputs').first();
+                                $extraFile.appendTo($spentDiv);
+                            }
+
+                        } else {
+                            var reader = new FileReader();
+                            var imgId = evt.target.id + "-image";
+                            reader.onload = (function() {
+                                return function(e) {
+                                    var imgDiv = $("#" + imgId);
+                                    console.log(imgDiv, e.target.result);
+                                    imgDiv.children('img').attr('src', e.target.result);
+                                };
+                            })();
+                            reader.readAsDataURL(evt.target.files[0]);
+                        }
+                    }
+                    $('input[type="file"]').change(onFileChange);
                 }
             }
         },
