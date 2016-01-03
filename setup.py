a9870a3477c1c30dee41b426df77a1fed7ccd455
Michael van Tellingen <michael@mvantellingen.nl>
2016-01-03 14:08:06 +0100

    Fix version string for solr-thumbnail in setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(name='django-oscar',
           # Treebeard is used for categories
           'django-treebeard>=4.0',
           # Sorl is used as the default thumbnailer
-          'sorl-thumbnail>=12.4a1,<13.0',
+          'sorl-thumbnail>=12.4a1',
           # Babel is used for currency formatting
           'Babel>=1.0,<1.4',
           # For converting non-ASCII to ASCII when creating slugs
