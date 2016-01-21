afe08db40965aee8df797d55c64a8de7fc0f3e50
Maik Hoepfel <m@maikhoepfel.de>
2016-01-21 10:18:34 +0100

    Remove Django 1.7 support
    We only support two versions of Django. That policy might change given
    that Django has started doing LTS releases. But for now, it only makes
    sense to remove Django 1.7 support.

--- sites/sandbox/apps/user/models.py
+++ sites/sandbox/apps/user/models.py
@@ -3,6 +3,8 @@ Sample user/profile models for testing.  These aren't enabled by default in the
 sandbox
 """
 
+from django.contrib.auth.models import (
+    AbstractUser, BaseUserManager, AbstractBaseUser)
 from django.db import models
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
@@ -25,49 +27,43 @@ class Profile(models.Model):
     age = models.PositiveIntegerField(verbose_name='Age')
 
 
-# A simple extension of the core User model for Django 1.5
-try:
-    from django.contrib.auth.models import (
-        AbstractUser, BaseUserManager, AbstractBaseUser)
-except ImportError:
-    pass
-else:
+# A simple extension of the core User model for Django 1.5+
+class ExtendedUserModel(AbstractUser):
+    twitter_username = models.CharField(max_length=255, unique=True)
 
-    class ExtendedUserModel(AbstractUser):
-        twitter_username = models.CharField(max_length=255, unique=True)
 
-    class CustomUserManager(BaseUserManager):
+class CustomUserManager(BaseUserManager):
 
-        def create_user(self, email, password=None):
-            now = timezone.now()
-            email = BaseUserManager.normalize_email(email)
-            user = self.model(email=email, last_login=now)
-            user.set_password(password)
-            user.save(using=self._db)
-            return user
+    def create_user(self, email, password=None):
+        now = timezone.now()
+        email = BaseUserManager.normalize_email(email)
+        user = self.model(email=email, last_login=now)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-        def create_superuser(self, email, password):
-            return self.create_user(email, password)
+    def create_superuser(self, email, password):
+        return self.create_user(email, password)
 
-    # A user model which doesn't extend AbstractUser
-    @python_2_unicode_compatible
-    class CustomUserModel(AbstractBaseUser):
-        name = models.CharField(max_length=255, blank=True)
-        email = models.EmailField(unique=True)
-        twitter_username = models.CharField(max_length=255, unique=True)
+# A user model which doesn't extend AbstractUser
+@python_2_unicode_compatible
+class CustomUserModel(AbstractBaseUser):
+    name = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(unique=True)
+    twitter_username = models.CharField(max_length=255, unique=True)
 
-        USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
 
-        objects = CustomUserManager()
+    objects = CustomUserManager()
 
-        def __str__(self):
-            return self.email
+    def __str__(self):
+        return self.email
 
-        def get_full_name(self):
-            return self.name
+    def get_full_name(self):
+        return self.name
 
-        get_short_name = get_full_name
+    get_short_name = get_full_name
 
-    # A simple extension of the core Oscar User model
-    class ExtendedOscarUserModel(abstract_models.AbstractUser):
-        twitter_username = models.CharField(max_length=255, unique=True)
+# A simple extension of the core Oscar User model
+class ExtendedOscarUserModel(abstract_models.AbstractUser):
+    twitter_username = models.CharField(max_length=255, unique=True)
