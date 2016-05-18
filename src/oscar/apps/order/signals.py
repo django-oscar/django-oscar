2b9f02cd5d97c1d5fd32c8322c850d1b2d0d9b53
Michael Kutý <6du1ro.n@gmail.com>
2016-05-18 12:54:28 +0200

    Send signals on order and order line status change.

--- src/oscar/apps/order/signals.py
+++ src/oscar/apps/order/signals.py
@@ -1,3 +1,9 @@
 import django.dispatch
 
 order_placed = django.dispatch.Signal(providing_args=["order", "user"])
+
+order_status_changed = django.dispatch.Signal(
+    providing_args=["order", "old_status", "new_status"])
+
+order_line_status_changed = django.dispatch.Signal(
+    providing_args=["line", "old_status", "new_status"])
