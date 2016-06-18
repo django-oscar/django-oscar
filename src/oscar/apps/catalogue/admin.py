bb23036dddb4266c52ab4256e9cf95df30b0d911
Michael van Tellingen <michael@mvantellingen.nl>
2016-06-18 11:27:40 +0200

    Don't add treebeard to INSTALLED_APPS for now.
    The module isn't 100% compatible yet with Django 1.10

--- src/oscar/apps/catalogue/admin.py
+++ src/oscar/apps/catalogue/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from treebeard.admin import TreeAdmin
+# from treebeard.admin import TreeAdmin
 from treebeard.forms import movenodeform_factory
 
 from oscar.core.loading import get_model
@@ -84,7 +84,7 @@ class AttributeOptionGroupAdmin(admin.ModelAdmin):
     inlines = [AttributeOptionInline, ]
 
 
-class CategoryAdmin(TreeAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     form = movenodeform_factory(Category)
     list_display = ('name', 'slug')
 
