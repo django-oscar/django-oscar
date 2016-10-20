75366647b82d2ef6f409c1d8e1b3a02b0d4dceb3
Yuriy Vaskin <vaskin90@gmail.com>
2016-10-20 14:33:46 +0300

    - fixed sorting by title

--- src/oscar/apps/search/search_indexes.py
+++ src/oscar/apps/search/search_indexes.py
@@ -16,6 +16,7 @@ class ProductIndex(indexes.SearchIndex, indexes.Indexable):
 
     upc = indexes.CharField(model_attr="upc", null=True)
     title = indexes.EdgeNgramField(model_attr='title', null=True)
+    title_exact = indexes.CharField(model_attr='title', null=True, indexed=False)
 
     # Fields for faceting
     product_class = indexes.CharField(null=True, faceted=True)
