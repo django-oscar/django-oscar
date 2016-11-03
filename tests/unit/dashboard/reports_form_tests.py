5ef5570873d6d092db4757d2ec984a8220bc2358
Alexander Gaevsky <sasha@sasha0.ru>
2016-11-03 16:02:25 +0200

    Fix dashboard report form when date range is incomplete, add tests.

---
+++ tests/unit/dashboard/reports_form_tests.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+
+from oscar.apps.dashboard.reports import forms
+
+
+class TestReportsForm(TestCase):
+
+    def test_date_range_empty(self):
+        form = forms.ReportForm(data={})
+        self.assertFalse(form.is_valid())
+
+    def test_date_range_complete(self):
+        form = forms.ReportForm(data={
+            'date_from': '2016-11-02',
+            'date_to': '2016-11-03',
+            'report_type': 'order_report'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_date_range_incomplete(self):
+        form = forms.ReportForm(data={
+            'report_type': 'order_report',
+            'date_to': '',
+            'date_from': '2016-11-02'
+        })
+        self.assertTrue(form.is_valid(), form.errors)
+        form = forms.ReportForm(data={
+            'date_from': '',
+            'date_to': '2016-11-03',
+            'report_type': 'order_report'
+        })
+        self.assertTrue(form.is_valid())
+        form = forms.ReportForm(data={
+            'date_from': '2016-11-02',
+            'report_type': 'order_report'
+        })
+        self.assertTrue(form.is_valid())
+        form = forms.ReportForm(data={
+            'date_to': '2016-11-03',
+            'report_type': 'order_report'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_date_range_incorrect(self):
+        form = forms.ReportForm(data={
+            'date_from': '2016-11-03',
+            'date_to': '2016-11-02',
+            'report_type': 'order_report'
+        })
+        self.assertFalse(form.is_valid())
