929ff456120911ae3e98d6b84cece6d715ac6ff5
Pete Graham <git@petegraham.co.uk>
2016-12-29 15:35:09 +0000

    Fix building sandbox

--- src/oscar/management/commands/oscar_import_catalogue.py
+++ src/oscar/management/commands/oscar_import_catalogue.py
@@ -1,5 +1,4 @@
 import logging
-from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
 
@@ -15,21 +14,27 @@ class Command(BaseCommand):
     args = '/path/to/file1.csv /path/to/file2.csv ...'
     help = 'For creating product catalogues based on a CSV file'
 
-    option_list = BaseCommand.option_list + (
-        make_option('--flush', action='store_true', dest='flush',
-                    default=False, help='Flush tables before importing'),
-        make_option('--delimiter', dest='delimiter', default=",",
-                    help='Delimiter used within CSV file(s)'))
+    def add_arguments(self, parser):
+        parser.add_argument('/path/to/file.csv', nargs='+')
+
+        parser.add_argument(
+            '--flush',
+            action='store_true',
+            dest='flush',
+            default=False,
+            help='Flush tables before importing')
+        parser.add_argument(
+            '--delimiter',
+            dest='delimiter',
+            default=",",
+            help='Delimiter used within CSV file(s)')
 
     def handle(self, *args, **options):
-        if not args:
-            raise CommandError("Please select a CSV file to import")
-
         logger.info("Starting catalogue import")
         importer = CatalogueImporter(
             logger, delimiter=options.get('delimiter'),
             flush=options.get('flush'))
-        for file_path in args:
+        for file_path in options['/path/to/file.csv']:
             logger.info(" - Importing records from '%s'" % file_path)
             try:
                 importer.handle(file_path)
