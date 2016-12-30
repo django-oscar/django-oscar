482473aaa7037db8d84b809c056e859925220743
Michael van Tellingen <michael@mvantellingen.nl>
2016-12-30 11:54:02 +0100

    Improve naming of arguments for the management commands

--- src/oscar/management/commands/oscar_fork_app.py
+++ src/oscar/management/commands/oscar_fork_app.py
@@ -7,14 +7,13 @@ from oscar.core import customisation
 
 
 class Command(BaseCommand):
-    args = '<app label> <destination folder>'
     help = (
         "Create a local version of one of Oscar's app so it can "
         "be customised")
 
     def add_arguments(self, parser):
-        parser.add_argument('<app label>')
-        parser.add_argument('<destination folder>')
+        parser.add_argument('app_label', help='The application to fork')
+        parser.add_argument('target_path', help='The path to copy the files to')
 
     def handle(self, *args, **options):
         # Use a stdout logger
@@ -23,7 +22,7 @@ class Command(BaseCommand):
         logger.addHandler(stream)
         logger.setLevel(logging.DEBUG)
 
-        app_label, folder_path = options['<app label>'], options['<destination folder>']
+        app_label, folder_path = options['app_label'], options['target_path']
         try:
             customisation.fork_app(app_label, folder_path, logger)
         except Exception as e:
