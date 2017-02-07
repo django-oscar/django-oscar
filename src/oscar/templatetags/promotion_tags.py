b6b071e7546f59dd905c9b88f52a6e547b64e07d
Michael van Tellingen <michael@mvantellingen.nl>
2017-02-07 23:04:25 +0100

    Fix the render_promotion template-tag

--- src/oscar/templatetags/promotion_tags.py
+++ src/oscar/templatetags/promotion_tags.py
@@ -1,26 +1,19 @@
-from django.template import Library, Node, RequestContext, Variable
+from django.template import Library, Node, RequestContext
 from django.template.loader import select_template
 
 register = Library()
 
 
-class PromotionNode(Node):
-    def __init__(self, promotion):
-        self.promotion_var = Variable(promotion)
+@register.simple_tag(takes_context=True)
+def render_promotion(context, promotion):
+    template = select_template([
+        promotion.template_name(), 'promotions/default.html'])
 
-    def render(self, context):
-        promotion = self.promotion_var.resolve(context)
-        template = select_template([promotion.template_name(),
-                                    'promotions/default.html'])
-        args = {'promotion': promotion}
-        args.update(**promotion.template_context(request=context['request']))
-        ctx = RequestContext(context['request'], args)
-        return template.render(ctx)
+    args = {
+        'request': context['request'],
+        'promotion': promotion
+    }
+    args.update(**promotion.template_context(request=context['request']))
 
-
-def get_promotion_html(parser, token):
-    _, promotion = token.split_contents()
-    return PromotionNode(promotion)
-
-
-register.tag('render_promotion', get_promotion_html)
+    ctx = RequestContext(context['request'], args)
+    return template.render(ctx)
