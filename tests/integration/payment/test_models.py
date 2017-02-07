be8a2795c79fe784965ebf2e7b5aa0bbe64f963f
Michael van Tellingen <michael@mvantellingen.nl>
2017-02-07 21:53:23 +0100

    Rename test file to match module tested.
    As in `test_model.py` -> `test_models.py`

--- tests/integration/payment/test_models.py
+++ tests/integration/payment/test_models.py
@@ -3,8 +3,9 @@ from decimal import Decimal as D
 
 from django.test import TestCase
 
-from oscar.core.compat import get_user_model
 from oscar.apps.payment.models import Bankcard, Source
+from oscar.core.compat import get_user_model
+from oscar.test import factories
 
 
 class TestBankcard(TestCase):
@@ -38,6 +39,19 @@ class TestBankcard(TestCase):
         saved_bankcard = Bankcard.objects.get(id=bankcard.id)
         self.assertEqual('Mastercard', saved_bankcard.card_type)
 
+    def test_cardtype_persists_after_save(self):
+        user = factories.UserFactory()
+        end = datetime.date(day=1, month=1, year=2010)
+        bankcard = Bankcard(
+            user=user, number="5500000000000004", expiry_date=end)
+        self.assertEqual('Mastercard', bankcard.card_type)
+
+        bankcard.save()
+        self.assertEqual('Mastercard', bankcard.card_type)
+
+        reloaded_bankcard = Bankcard.objects.get(id=bankcard.id)
+        self.assertEqual('Mastercard', reloaded_bankcard.card_type)
+
 
 class TestSource(TestCase):
 
@@ -60,3 +74,27 @@ class TestSource(TestCase):
             amount_refunded=D('20'))
         self.assertEqual(
             D('80') - D('20'), source.amount_available_for_refund)
+
+
+class TestAPaymentSource(TestCase):
+
+    def setUp(self):
+        order = factories.create_order()
+        self.source = factories.SourceFactory(order=order)
+
+    def test_allocation_doesnt_error(self):
+        self.source.allocate(D('100.00'))
+
+    def test_debit_doesnt_error(self):
+        self.source.allocate(D('100.00'))
+        self.source.debit(D('80.00'))
+
+    def test_full_debit_doesnt_error(self):
+        self.source.allocate(D('100.00'))
+        self.source.debit()
+        self.assertEqual(D('0.00'), self.source.balance)
+
+    def test_refund_doesnt_error(self):
+        self.source.allocate(D('100.00'))
+        self.source.debit(D('80.00'))
+        self.source.refund(D('50.00'))
