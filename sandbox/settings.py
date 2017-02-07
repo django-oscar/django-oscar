7ef2fc1a627ca3237a3d5eb9199574a8112f6551
Samir Shah <solaris.smoke@gmail.com>
2017-02-07 09:22:48 +0300

    Remove obsolete debug toolbar settings from sandbox. Fixes #2229.
    `DEBUG_TOOLBAR_PATCH_SETTINGS` is obsolete in django-debug-toolbar 1.6. We also don't need to specify `DEBUG_TOOLBAR_PANELS` as it is identical to the default.

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -321,23 +321,6 @@ HAYSTACK_CONNECTIONS = {
 # Debug Toolbar
 # =============
 
-# Implicit setup can often lead to problems with circular imports, so we
-# explicitly wire up the toolbar
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-]
 INTERNAL_IPS = ['127.0.0.1', '::1']
 
 # ==============
