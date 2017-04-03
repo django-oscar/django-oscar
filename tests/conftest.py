9809fd494a5992906eeed8179b413268b31a81f8
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-04-03 11:13:09 +0200

    Remove tests

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-from .config import configure
-
-
-def pytest_configure():
-    configure()
--- tests/unit/offer/__init__.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from oscar.apps.offer import models
-
-
-class OfferTest(TestCase):
-    def setUp(self):
-        self.range = models.Range(
-            name="All products range", includes_all_products=True)
