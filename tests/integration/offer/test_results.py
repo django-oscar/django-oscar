fe68f1172f9fc3f6f84a21e87cf1bcbfd914abf8
Paul J Stevens <paul@nfg.nl>
2017-05-20 16:37:08 +0200

    fix bug in grouped_voucher_discounts (#2366)
    * fix bug in grouped_voucher_discounts
    
    * add test for grouped_voucher_discounts
    
    * remove 3.6.1 syntax
    
    * improve test

--- tests/integration/offer/test_results.py
+++ tests/integration/offer/test_results.py
@@ -3,6 +3,7 @@ from decimal import Decimal as D
 from django.test import TestCase
 
 from oscar.apps.offer import models, results
+from oscar.test.factories import VoucherFactory, ConditionalOfferFactory
 
 
 class TestOfferApplicationsObject(TestCase):
@@ -28,3 +29,24 @@ class TestOfferApplicationsObject(TestCase):
         result = models.PostOrderAction("Something will happen")
         self.applications.add(self.offer, result)
         self.assertEqual(1, len(self.applications.post_order_actions))
+
+    def test_grouped_voucher_discounts(self):
+        voucher = VoucherFactory()
+        offer1 = ConditionalOfferFactory(name='offer1')
+        offer1.set_voucher(voucher)
+        result1 = models.BasketDiscount(D('2.00'))
+
+        offer2 = ConditionalOfferFactory(name='offer2')
+        offer2.set_voucher(voucher)
+        result2 = models.BasketDiscount(D('1.00'))
+
+        self.applications.add(offer1, result1)
+        self.applications.add(offer2, result2)
+
+        assert len(self.applications) == 2
+
+        discounts = self.applications.grouped_voucher_discounts
+        discounts = [x for x in discounts]
+        assert len(discounts) == 1
+        assert discounts[0]['voucher'] == voucher
+        assert discounts[0]['discount'] == D('3.00')
