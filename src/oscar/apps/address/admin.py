0bb94ed300b69106fd28ed0174a4becccc24a4a0
John-Scott Atlakson <john.scott.atlakson@gmail.com>
2017-06-13 22:37:05 -0700

    Enhanced CountryAdmin functionalty
    Now shows display order so this can easily be seen at a glance
    Allows filtering by shipping country status to narrow results
    Allows searching on additional fields so admin users can search by ISO codes rather than only name

--- src/oscar/apps/address/admin.py
+++ src/oscar/apps/address/admin.py
@@ -8,7 +8,19 @@ class UserAddressAdmin(admin.ModelAdmin):
 
 
 class CountryAdmin(admin.ModelAdmin):
-    search_fields = ('name',)
+    list_display = [
+        '__str__',
+        'display_order'
+    ]
+    list_filter = [
+        'is_shipping_country'
+    ]
+    search_fields = [
+        'name', 
+        'printable_name', 
+        'iso_3166_1_a2', 
+        'iso_3166_1_a3'
+    ]
 
 
 admin.site.register(get_model('address', 'useraddress'), UserAddressAdmin)
