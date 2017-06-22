79182290d17ec3966a5c1dd9fa1750ef272577b9
Markus Bertheau <mbertheau@gmail.com>
2017-06-22 10:05:16 +0200

    Fix voucher is_active filter
    https://machtfit.atlassian.net/browse/MAC-1754

--- src/oscar/apps/dashboard/vouchers/views.py
+++ src/oscar/apps/dashboard/vouchers/views.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from oscar.core.loading import get_model
 from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
 from django.views import generic
 
 from oscar.core.loading import get_class
@@ -56,8 +57,8 @@ class VoucherListView(generic.ListView):
             self.description_ctx['code_filter'] \
                 = _("with code '%s'") % data['code']
         if data['is_active']:
-            today = datetime.date.today()
-            qs = qs.filter(start_date__lte=today, end_date__gte=today)
+            now = timezone.now()
+            qs = qs.filter(start_datetime__lte=now, end_datetime__gte=now)
             self.description_ctx['main_filter'] = _('Active vouchers')
 
         return qs
