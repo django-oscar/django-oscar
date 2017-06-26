91655b049f1577b4650b7b28f7e8877a3742c25b
Michael van Tellingen <michael@mvantellingen.nl>
2017-06-26 10:03:05 +0200

    Allow latest django-haystack release (for Django 1.11)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ install_requires = [
     # https://github.com/AndrewIngram/django-extra-views/issues/114
     'django-extra-views>=0.2,<0.6.5',
     # Search support
-    'django-haystack>=2.5.0,<2.6.0',
+    'django-haystack>=2.5.0,<2.7.0',
     # Treebeard is used for categories
     'django-treebeard>=4.1.0',
     # Sorl is used as the default thumbnailer
