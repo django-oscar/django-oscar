5b1e81945dc701d4363f1d261b940312ad80fb16
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-07-10 16:35:37 +0200

    Remove admin.py's

--- src/oscar/apps/wishlists/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from oscar.core.loading import get_model
-
-WishList = get_model('wishlists', 'WishList')
-Line = get_model('wishlists', 'Line')
-
-
-admin.site.register(WishList)
-admin.site.register(Line)
