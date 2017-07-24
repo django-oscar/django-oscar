a9f632ee93d16c3bc15555e977370f23ad16c4d9
Markus Bertheau <mbertheau@gmail.com>
2017-07-24 17:23:22 +0200

    Bump version to 1.0.1-machtfit-36

--- src/oscar/__init__.py
+++ src/oscar/__init__.py
@@ -2,7 +2,7 @@ import os
 
 # Use 'dev', 'beta', or 'final' as the 4th element to indicate release type.
 
-VERSION = (1, 0, 1, 'machtfit', 35)
+VERSION = (1, 0, 1, 'machtfit', 36)
 
 
 def get_short_version():
