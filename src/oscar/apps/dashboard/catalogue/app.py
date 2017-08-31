534a2af221d48c478502f97ad554c7e9b39c6760
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-08-31 17:57:18 +0200

    Delete dashboard categories table and views

--- src/oscar/apps/dashboard/catalogue/app.py
+++ src/oscar/apps/dashboard/catalogue/app.py
@@ -1,49 +1,13 @@
-from django.conf.urls import url
 
 from oscar.core.application import Application
-from oscar.core.loading import get_class
 
 
 class CatalogueApplication(Application):
     name = None
 
-    default_permissions = ['is_staff', ]
-    permissions_map = _map = {
-        'catalogue-product-delete': (['is_staff'],
-                                     ['partner.dashboard_access']),
-        'catalogue-product-lookup': (['is_staff'],
-                                     ['partner.dashboard_access']),
-    }
-
-    category_list_view = get_class('dashboard.catalogue.views',
-                                   'CategoryListView')
-    category_detail_list_view = get_class('dashboard.catalogue.views',
-                                          'CategoryDetailListView')
-    category_create_view = get_class('dashboard.catalogue.views',
-                                     'CategoryCreateView')
-    category_update_view = get_class('dashboard.catalogue.views',
-                                     'CategoryUpdateView')
-    category_delete_view = get_class('dashboard.catalogue.views',
-                                     'CategoryDeleteView')
-
     def get_urls(self):
         urls = [
-            url(r'^categories/$', self.category_list_view.as_view(),
-                name='catalogue-category-list'),
-            url(r'^categories/(?P<pk>\d+)/$',
-                self.category_detail_list_view.as_view(),
-                name='catalogue-category-detail-list'),
-            url(r'^categories/create/$', self.category_create_view.as_view(),
-                name='catalogue-category-create'),
-            url(r'^categories/create/(?P<parent>\d+)$',
-                self.category_create_view.as_view(),
-                name='catalogue-category-create-child'),
-            url(r'^categories/(?P<pk>\d+)/update/$',
-                self.category_update_view.as_view(),
-                name='catalogue-category-update'),
-            url(r'^categories/(?P<pk>\d+)/delete/$',
-                self.category_delete_view.as_view(),
-                name='catalogue-category-delete'),
+
         ]
         return self.post_process_urls(urls)
 
--- src/oscar/apps/dashboard/catalogue/forms.py
+++
@@ -1,9 +0,0 @@
-from treebeard.forms import movenodeform_factory
-
-from oscar.core.loading import get_model
-
-Category = get_model('catalogue', 'Category')
-
-CategoryForm = movenodeform_factory(
-    Category,
-    fields=['name', 'description', 'image'])
