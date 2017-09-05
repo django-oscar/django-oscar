80d42bb6997fefbf6da04f8233a1c426be422c27
Samir Shah <solaris.smoke@gmail.com>
2017-09-05 06:09:00 +0300

    Log invalid URL names in dashboard nav access function.
    If a url_name is invalid, then log the exception instead of silently discarding that menu item.

--- tests/integration/dashboard/test_nav.py
+++ tests/integration/dashboard/test_nav.py
@@ -1,27 +1,35 @@
 from django.test import TestCase
+
 from oscar.apps.dashboard.menu import get_nodes
-from oscar.core.compat import get_user_model
+from oscar.apps.dashboard.nav import default_access_fn
+from oscar.test.factories import UserFactory
 
 
-User = get_user_model()
-
-
-class TestCategory(TestCase):
+class DashboardAccessFunctionTestCase(TestCase):
 
     def setUp(self):
-        self.staff_user = User.objects.create_user('staff', 'staff@example.com',
-                                                   'pw1')
-        self.staff_user.is_staff = True
-        self.staff_user.save()
-        self.non_staff_user = User.objects.create_user('nostaff',
-                                                       'nostaff@example.com',
-                                                       'pw2')
-        self.non_staff_user.save()
+        self.staff_user = UserFactory(is_staff=True)
+        self.non_staff_user = UserFactory()
+
+    def test_default_access_fn_no_url_name(self):
+        self.assertTrue(default_access_fn(self.staff_user, None))
+
+    def test_default_access_fn_staff(self):
+        self.assertTrue(default_access_fn(self.staff_user, 'dashboard:index'))
+
+    def test_default_access_fn_non_staff_user(self):
+        self.assertFalse(default_access_fn(self.non_staff_user, 'dashboard:index'))
+
+    def test_default_access_fn_invalid_url_name(self):
+        self.assertFalse(default_access_fn(self.staff_user, 'invalid_module:index'))
+
+
+class DashboardNavTestCase(TestCase):
 
     def test_staff_user_has_menu(self):
-        menu = get_nodes(self.staff_user)
+        menu = get_nodes(UserFactory(is_staff=True))
         self.assertTrue(menu)
 
     def test_non_staff_user_has_empty_menu(self):
-        menu = get_nodes(self.non_staff_user)
+        menu = get_nodes(UserFactory())
         self.assertEqual(menu, [])
