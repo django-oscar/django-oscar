f286de56d652c638afcbfcf4326fb444d5341e64
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-09-07 14:27:25 +0200

    Remove complety dashboard user app

--- src/oscar/apps/dashboard/users/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.utils.translation import ugettext_lazy as _, pgettext_lazy
-
-
-class UserSearchForm(forms.Form):
-    email = forms.CharField(required=False, label=_("Email"))
-    name = forms.CharField(
-        required=False, label=pgettext_lazy(u"User's name", u"Name"))
