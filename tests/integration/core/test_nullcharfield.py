df539128cb6921685379953a0f4ed1c2e0932f33
Samir Shah <solaris.smoke@gmail.com>
2017-12-13 05:12:57 +0300

    Add tests specifically for NullCharField.

---
+++ tests/integration/core/test_nullcharfield.py
@@ -0,0 +1,25 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.test import TestCase
+
+from oscar.models.fields import NullCharField
+
+
+class NullCharFieldTest(TestCase):
+
+    def test_from_db_value_converts_null_to_string(self):
+        field = NullCharField()
+        self.assertEqual('', field.from_db_value(None, expression=None, connection=None, context=None))
+
+    def test_get_prep_value_converts_empty_string_to_null(self):
+        field = NullCharField()
+        self.assertEqual(None, field.get_prep_value(''))
+
+    def test_raises_exception_for_invalid_null_blank_combo(self):
+        with self.assertRaises(ImproperlyConfigured):
+            NullCharField(null=True, blank=False)
+
+        with self.assertRaises(ImproperlyConfigured):
+            NullCharField(null=False, blank=True)
+
+        with self.assertRaises(ImproperlyConfigured):
+            NullCharField(null=False, blank=False)
