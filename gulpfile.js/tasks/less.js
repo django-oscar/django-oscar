00220454cb32949ac7051e69cbc11c280dbd8284
gogobook <O968041428@gmail.com>
2017-12-25 17:21:54 +0800

    Upgrade gulp, gulp-sourcemaps in package.json. Rewrite less.js. Add .vscode to .gitignore.

--- gulpfile.js/tasks/less.js
+++ gulpfile.js/tasks/less.js
@@ -1,16 +1,21 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
     sourcemaps = require('gulp-sourcemaps'),
-    less_task = function() {
-        gulp.src('src/oscar/static/oscar/less/*.less')
-            .pipe(less({
-                includePaths: [
+    pump = require('pump'),
+    less_task = function(cb) {
+        pump([
+            gulp.src('src/oscar/static/oscar/less/*.less'),
+            sourcemaps.init(),
+            less({includePaths: [
                     'src/oscar/static/less/',
-                ],
-                outputStyle: null,
-            }))
-            .pipe(sourcemaps.write('src/oscar/static/oscar/css/'))
-            .pipe(gulp.dest('src/oscar/static/oscar/css/'));
+                    ],
+                    outputStyle: null,
+                }),
+            sourcemaps.write('/'),
+            gulp.dest('src/oscar/static/oscar/css/')
+            ],
+            cb
+        );
     };
 
 gulp.task('less', less_task);
