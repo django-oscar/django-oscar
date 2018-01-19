ce9f3d775453d4c0c6cfea26d55a4649fba802de
yn_coder <yn.coder@gmail.com>
2018-01-19 22:23:24 +0300

    #2548 test 'number of notifications is not changed if we view the notofication list'

--- tests/functional/customer/test_notification.py
+++ tests/functional/customer/test_notification.py
@@ -1,7 +1,8 @@
 from oscar.test.testcases import WebTestCase
 from oscar.apps.customer.notifications import services
 from oscar.test.factories import UserFactory
-
+from django.utils.six.moves import http_client
+from django.core.urlresolvers import reverse
 
 class TestAUserWithUnreadNotifications(WebTestCase):
 
@@ -12,4 +13,10 @@ class TestAUserWithUnreadNotifications(WebTestCase):
     def test_can_see_them_in_page_header(self):
         homepage = self.app.get('/', user=self.user)
         self.assertTrue('num_unread_notifications' in homepage.context)
+        self.assertEqual(1, homepage.context['num_unread_notifications'])
+
+        path = reverse('notifications-inbox:anon-order' )
+        response = self.app.get(path)
+        self.assertEqual(http_client.OK, response.status_code)
+        # number of notifications should not change if we view the notofication list
         self.assertEqual(1, homepage.context['num_unread_notifications'])
