fbe9a3e508259e5351358341d3227e54b9c53f57
yn_coder <yn.coder@gmail.com>
2018-01-25 20:06:28 +0300

    #2548 tests for notifications (fixed fail for new notification id)

--- tests/functional/customer/test_notification.py
+++ tests/functional/customer/test_notification.py
@@ -3,6 +3,7 @@ from oscar.apps.customer.notifications import services
 from oscar.test.factories import UserFactory
 from django.utils.six.moves import http_client
 from django.core.urlresolvers import reverse
+from oscar.apps.customer.models import Notification
 
 TEST_MSG_TEXT = "Test message with code 123x456c789"
 
@@ -28,7 +29,8 @@ class TestAUserWithUnreadNotifications(WebTestCase):
     def test_notification_mark_it_as_read(self):
         homepage = self.app.get('/', user=self.user)
         self.assertEqual(1, homepage.context['num_unread_notifications'])
-        path = reverse('customer:notifications-detail', kwargs={'pk': 1} )
+        n = Notification.objects.get( subject = TEST_MSG_TEXT )
+        path = reverse('customer:notifications-detail', kwargs={'pk': n.id} )
         response = self.app.get(path)
         self.assertEqual(http_client.OK, response.status_code)
         # number of notifications should not change if we visit the notification list
