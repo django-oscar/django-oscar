0fbe83751163ac4011c0687af1b2dc318d06bfb7
Alexander Gaevsky <sasha@sasha0.ru>
2018-02-22 14:01:03 +0200

    Switch default homepage to catalogue index.

--- src/oscar/defaults.py
+++ src/oscar/defaults.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 OSCAR_SHOP_NAME = 'Oscar'
 OSCAR_SHOP_TAGLINE = ''
-OSCAR_HOMEPAGE = reverse_lazy('promotions:home')
+OSCAR_HOMEPAGE = reverse_lazy('catalogue:index')
 
 # Dynamic class loading
 OSCAR_DYNAMIC_CLASS_LOADER = 'oscar.core.loading.default_class_loader'
