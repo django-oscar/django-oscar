c1d95ccb0a258e7ca9ec8cda40925f513f6049e0
Samir Shah <solaris.smoke@gmail.com>
2018-04-25 06:01:49 +0300

    Add phonenumbers dependency to setup.py.
    We cannot rely on this being installed by django-phonenumber-field.
    See https://github.com/django-oscar/django-oscar/issues/2681.
    
    Fixes #2681.

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ install_requires = [
     # For manipulating search URLs
     'purl>=0.7',
     # For phone number field
+    'phonenumbers',
     'django-phonenumber-field>=2.0,<2.1',
     # Used for oscar.test.contextmanagers.mock_signal_receiver
     'mock>=1.0.1,<3.0',
