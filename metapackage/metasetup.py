01eccdc26459e61bc17ca5276b3669d95280f28d
Lars van de Kerkhof <lars@permanentmarkers.nl>
2018-08-20 13:19:17 +0200

    Only defensive pins are needed in the metapackage, the rest comes from the code package,.

--- metapackage/metasetup.py
+++ metapackage/metasetup.py
@@ -21,30 +21,13 @@ OSCAR_VERSION = get_version().replace(' ', '-')
 install_requires = [
     'oscar==%s' % OSCAR_VERSION,
     'django>=1.11,<2.2',
-    # PIL is required for image fields, Pillow is the "friendly" PIL fork
-    'pillow>=4.0',
-    # We use the ModelFormSetView from django-extra-views for the basket page
     'django-extra-views>=0.11,<0.12',
-    # Search support
     'django-haystack>=2.5.0,<3.0.0',
-    # Treebeard is used for categories
-    'django-treebeard>=4.3.0',
-    # Sorl is used as the default thumbnailer
     'sorl-thumbnail>=12.4.1,<12.5',
-    # Babel is used for currency formatting
     'Babel>=1.0,<3.0',
-    # For manipulating search URLs
-    'purl>=0.7',
-    # For phone number field
-    'phonenumbers',
     'django-phonenumber-field>=2.0,<2.1',
-    # Used for oscar.test.newfactories
     'factory-boy>=2.4.1,<3.0',
-    # Used for automatically building larger HTML tables
     'django-tables2>=1.19,<2.0',
-    # Used for manipulating form field attributes in templates (eg: add
-    # a css class)
-    'django-widget-tweaks>=1.4.1',
 ]
 
 with open(os.path.join(PROJECT_DIR, 'README.rst')) as fh:
