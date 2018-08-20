25615a00f44c59fef8f7df83ba9c09a52ed7d0d1
Lars van de Kerkhof <lars@permanentmarkers.nl>
2018-08-20 13:18:02 +0200

    No need for any of this in the meta package.

--- metapackage/metasetup.py
+++ metapackage/metasetup.py
@@ -83,23 +83,3 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Application Frameworks']
 )
-
-# Show contributing instructions if being installed in 'develop' mode
-if len(sys.argv) > 1 and sys.argv[1] == 'develop':
-    docs_url = 'https://django-oscar.readthedocs.io/en/latest/internals/contributing/index.html'
-    mailing_list = 'django-oscar@googlegroups.com'
-    mailing_list_url = 'https://groups.google.com/forum/?fromgroups#!forum/django-oscar'
-    twitter_url = 'https://twitter.com/django_oscar'
-    msg = (
-        "You're installing Oscar in 'develop' mode so I presume you're thinking\n"
-        "of contributing:\n\n"
-        "(a) That's brilliant - thank you for your time\n"
-        "(b) If you have any questions, please use the mailing list:\n    %s\n"
-        "    %s\n"
-        "(c) There are more detailed contributing guidelines that you should "
-        "have a look at:\n    %s\n"
-        "(d) Consider following @django_oscar on Twitter to stay up-to-date\n"
-        "    %s\n\nHappy hacking!") % (mailing_list, mailing_list_url,
-                                       docs_url, twitter_url)
-    line = '=' * 82
-    print(("\n%s\n%s\n%s" % (line, msg, line)))
