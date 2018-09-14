e1102310dc5f1230df9b695a6f56e0b6f6ce693a
frmdstryr <frmdstryr@protonmail.com>
2018-09-14 16:32:10 -0400

    Make dashboard editor options configurable

--- src/oscar/apps/system/abstract_models.py
+++ src/oscar/apps/system/abstract_models.py
@@ -17,6 +17,39 @@ class AbstractConfiguration(models.Model):
     google_analytics_id = models.CharField(max_length=256, blank=True,
                                            null=True)
     
+    #: Date formats for dashboard and UI
+    shop_date_format = models.CharField(max_length=32, default="yy-mm-dd")
+    shop_time_format = models.CharField(max_length=32, default="hh:ii")
+    shop_datetime_format = models.CharField(
+        max_length=32,
+        default="yy-mm-dd hh:ii"
+    )
+    
+    #: Time for date picker
+    shop_date_picker_minute_step = models.PositiveSmallIntegerField(default=15)
+    
+    #: Dashboard editor settings
+    dashboard_editor_plugins = models.CharField(
+        max_length=255,
+        default="link lists", 
+        blank=True
+    )
+    dashboard_editor_menubar = models.CharField(
+        max_length=255,
+        default="", 
+        blank=True
+    )
+    dashboard_editor_toolbar_layout = models.CharField(
+        max_length=255,
+        default="styleselect | bold italic blockquote | bullist numlist | link",
+        blank=True
+    )
+    dashboard_editor_context_menu = models.CharField(
+        max_length=255,
+        default="copy cut paste link",
+        blank=True
+    )
+    
     class Meta:
         abstract = True
         app_label = 'system'
@@ -26,6 +59,14 @@ class AbstractConfiguration(models.Model):
         return {
             'shop_name': self.shop_name,
             'shop_tagline': self.shop_tagline,
+            'shop_date_format': self.shop_date_format,
+            'shop_time_format': self.shop_time_format,
+            'shop_datetime_format': self.shop_datetime_format,
+            'shop_date_picker_minute_step': self.shop_date_picker_minute_step,
+            'dashboard_editor_plugins': self.dashboard_editor_plugins,
+            'dashboard_editor_menubar': self.dashboard_editor_menubar,
+            'dashboard_editor_toolbar_layout': self.dashboard_editor_toolbar_layout,
+            'dashboard_editor_context_menu': self.dashboard_editor_context_menu,
             'homepage_url': self.homepage_url,
             # Fallback to old settings name for backwards compatibility
             'use_less': self.use_less,
