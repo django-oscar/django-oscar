e88714b2761ee3c4c312794813d0e9cfda0d48e8
Samir Shah <solaris.smoke@gmail.com>
2018-10-13 08:41:55 +0300

    Update several development dependencies.

--- gulpfile.js/subtasks/watch.js
+++ gulpfile.js/subtasks/watch.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
 
 gulp.task('watch', function() {
-    var watch = require('gulp-watch');
-
-    gulp.watch('src/oscar/static/oscar/less/**/*.less', ['less']);
+    gulp.watch('src/oscar/static/oscar/less/**/*.less', gulp.parallel('less'));
 });
