4a40701599d60f19351e962cf54c33fcb5d73ed0
PlayDay <onedaywemay@gmail.com>
2018-12-05 12:22:56 +0200

    (UPDATE) normalise_email func to handle double @ email values

--- src/oscar/apps/customer/utils.py
+++ src/oscar/apps/customer/utils.py
@@ -147,6 +147,6 @@ def normalise_email(email):
     """
     clean_email = email.strip()
     if '@' in clean_email:
-        local, host = clean_email.split('@')
+        local, host = clean_email.rsplit('@', 1)
         return local + '@' + host.lower()
     return clean_email
