43c5db8fbcb81cd7ce84be3f59940ed0c20ea577
Highbiza migration experts <45388312+highbiza@users.noreply.github.com>
2019-03-20 05:27:37 +0100

    Added method to Product manager for filtering on product attributes. (#2888)

--- src/oscar/apps/catalogue/managers.py
+++ src/oscar/apps/catalogue/managers.py
@@ -1,10 +1,91 @@
+from collections import defaultdict
+
 from django.db import models
 from django.db.models import Count
+from django.db.models.constants import LOOKUP_SEP
 
 from oscar.core.decorators import deprecated
 
 
+class AttributeFilter(dict):
+    """
+    Utility class used to implement the filter_by_attributes functionality.
+
+    handles lookups, options and multivalue properties, check the tests for
+    all features.
+    """
+
+    def __init__(self, filter_kwargs):
+        super(AttributeFilter, self).__init__()
+
+        for key, value in filter_kwargs.items():
+            if LOOKUP_SEP in key:
+                field_name, lookup = key.split(LOOKUP_SEP, 1)
+                self[field_name] = (lookup, value)
+            else:
+                self[key] = (None, value)
+
+    def field_names(self):
+        return self.keys()
+
+    def _selector(self, attribute_type):
+        if attribute_type == "option" or attribute_type == "multi_option":
+            return "attribute_values__value_%s__option" % attribute_type
+        else:
+            return "attribute_values__value_%s" % attribute_type
+
+    def _select_value(self, types, lookup, value):
+        _filter = models.Q()
+        for _type in types:
+            sel = self._selector(_type)
+            if lookup is not None:
+                sel = "%s%s%s" % (sel, LOOKUP_SEP, lookup)
+
+            kwargs = dict()
+            kwargs[sel] = value
+            _filter |= models.Q(**kwargs)
+
+        return _filter
+
+    def fast_query(self, attribute_types, queryset):
+        qs = queryset
+        typedict = defaultdict(list)
+
+        for code, attribute_type in attribute_types:
+            typedict[code].append(attribute_type)
+
+        for code, (lookup, value) in self.items():
+            selected_values = self._select_value(typedict[code], lookup, value)
+            if not selected_values:  # if no value clause can be formed, no result can be formed.
+                return queryset.none()
+
+            qs = qs.filter(
+                selected_values,
+                attribute_values__attribute__code=code,
+            )
+
+        return qs
+
+
 class ProductQuerySet(models.query.QuerySet):
+
+    def filter_by_attributes(self, **filter_kwargs):
+        """
+        Allows querying by attribute as if the attributes where fields on the
+        product::
+
+        >>> first_large_shirt = Product.objects.filter_by_attributes(size="Large").first()
+        >>> first_large_shirt.attr.size
+        <AttributeOption: Large>
+        """
+        attribute_filter = AttributeFilter(filter_kwargs)
+
+        ProductAttribute = self.model.attributes.rel.model
+        attribute_types = ProductAttribute.objects.values_list("code", "type").filter(
+            code__in=attribute_filter.field_names()
+        )
+
+        return attribute_filter.fast_query(attribute_types, self)
 
     def base_queryset(self):
         """
---
+++ tests/unit/catalogue/test_product_attributes_query.py
@@ -0,0 +1,119 @@
+from django.test import TestCase
+from oscar.core.loading import get_model
+from django.core.exceptions import ValidationError
+
+Product = get_model("catalogue", "Product")
+
+
+class ProductAttributeQuerysetTest(TestCase):
+    fixtures = ["productattributes"]
+
+    def test_query_multiple_producttypes(self):
+        "We should be able to query over multiple product classes"
+        result = Product.objects.filter_by_attributes(henkie="bah bah")
+        self.assertEqual(result.count(), 2)
+        result1, result2 = list(result)
+
+        self.assertNotEqual(result1.product_class, result2.product_class)
+        self.assertEqual(result1.attr.henkie, result2.attr.henkie)
+
+    def test_further_filtering(self):
+        "The returned queryset should be ready for further filtering"
+        result = Product.objects.filter_by_attributes(henkie="bah bah")
+        photo = result.filter(title__contains="Photo")
+        self.assertEqual(photo.count(), 1)
+
+    def test_empty_results(self):
+        "Empty results are possible without errors"
+        result = Product.objects.filter_by_attributes(doesnotexist=True)
+        self.assertFalse(result.exists(), "querying with bulshit attributes should give no results")
+        result = Product.objects.filter_by_attributes(henkie="zulthoofd")
+        self.assertFalse(result.exists(), "querying with non existing values should give no results")
+        result = Product.objects.filter_by_attributes(henkie=True)
+        self.assertFalse(result.exists(), "querying with wring value type should give no results")
+
+    def test_text_value(self):
+        result = Product.objects.filter_by_attributes(subtitle="superhenk")
+        self.assertTrue(result.exists())
+        result = Product.objects.filter_by_attributes(subtitle="kekjo")
+        self.assertTrue(result.exists())
+        result = Product.objects.filter_by_attributes(subtitle=True)
+        self.assertFalse(result.exists())
+
+    def test_formatted_text(self):
+        html = "<p style=\"margin: 0px; font-stretch: normal; font-size: 12px; line-height: normal; font-family: Helvetica;\">Vivamus auctor leo vel dui. Aliquam erat volutpat. Phasellus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras tempor. Morbi egestas, <em>urna</em> non consequat tempus, <strong>nunc</strong> arcu mollis enim, eu aliquam erat nulla non nibh. Duis consectetuer malesuada velit. Nam ante nulla, interdum vel, tristique ac, condimentum non, tellus. Proin ornare feugiat nisl. Suspendisse dolor nisl, ultrices at, eleifend vel, consequat at, dolor.</p>"  # noqa
+        result = Product.objects.filter_by_attributes(additional_info=html)
+        self.assertTrue(result.exists())
+
+    def test_boolean(self):
+        result = Product.objects.filter_by_attributes(available=True)
+        self.assertTrue(result.exists())
+        result = Product.objects.filter_by_attributes(available=0)
+        self.assertTrue(result.exists())
+        with self.assertRaises(ValidationError):
+            result = Product.objects.filter_by_attributes(available="henk")
+
+    def test_number(self):
+        result = Product.objects.filter_by_attributes(facets=4)
+        self.assertTrue(result.exists())
+        with self.assertRaises(ValueError):
+            result = Product.objects.filter_by_attributes(facets="four")
+
+        result = Product.objects.filter_by_attributes(facets=1)
+        self.assertFalse(result.exists())
+
+    def test_float(self):
+        result = Product.objects.filter_by_attributes(hypothenusa=1.25)
+        self.assertTrue(result.exists())
+        with self.assertRaises(ValueError):
+            result = Product.objects.filter_by_attributes(facets="four")
+
+        result = Product.objects.filter_by_attributes(hypothenusa=1)
+        self.assertFalse(result.exists())
+
+    def test_option(self):
+        result = Product.objects.filter_by_attributes(kind="totalitarian")
+        self.assertTrue(result.exists())
+        result = Product.objects.filter_by_attributes(kind=True)
+        self.assertFalse(result.exists())
+
+        result = Product.objects.filter_by_attributes(kind="omnimous")
+        self.assertFalse(result.exists())
+
+    def test_multi_option(self):
+        result = Product.objects.filter_by_attributes(subkinds="megalomane")
+        self.assertTrue(result.exists())
+        self.assertEqual(result.count(), 2)
+        result = Product.objects.filter_by_attributes(subkinds=True)
+        self.assertFalse(result.exists())
+
+        result = Product.objects.filter_by_attributes(subkinds="omnimous")
+        self.assertFalse(result.exists())
+
+        result = Product.objects.filter_by_attributes(subkinds__contains="om")
+        self.assertTrue(result.exists(), "megalomane conains om")
+        self.assertEqual(result.count(), 2)
+
+    def test_multiple_attributes(self):
+        result = Product.objects.filter_by_attributes(subkinds="megalomane", available=True)
+        self.assertTrue(result.exists())
+
+        result = Product.objects.filter_by_attributes(
+            kind="totalitarian",
+            hypothenusa=1.25,
+            facets=8,
+            subtitle="superhenk",
+            subkinds="megalomane",
+            available=False
+        )
+        self.assertTrue(result.exists())
+
+    def test_lookups(self):
+        result = Product.objects.filter_by_attributes(facets__lte=4)
+        self.assertEqual(result.count(), 1)
+
+        result = Product.objects.filter_by_attributes(facets__lte=8)
+        self.assertEqual(result.count(), 2)
+
+        result = Product.objects.filter_by_attributes(facets__lt=8)
+        self.assertEqual(result.count(), 1)
