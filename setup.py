0b11e40e0f1e2e58d958ac3b175f01f7913acba5
Alexander Gaevsky <sasha@sasha0.ru>
2019-04-17 15:38:15 +0300

    Upgrade to Django 2.2 in the setup requirements.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ sys.path.append(os.path.join(PROJECT_DIR, 'src'))
 from oscar import get_version  # noqa isort:skip
 
 install_requires = [
-    'django>=1.11,<2.2',
+    'django>=1.11,<2.3',
     # PIL is required for image fields, Pillow is the "friendly" PIL fork
     'pillow>=4.0',
     # We use the ModelFormSetView from django-extra-views for the basket page
