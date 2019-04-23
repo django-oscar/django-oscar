5b3cfcef7d7185d4504f05da1448b1c9089dff56
Basil Dubyk <samitnuk.work@gmail.com>
2019-04-23 20:29:39 +0300

    Replace all icons with Font Awesome 5

--- src/oscar/static_src/oscar/js/oscar/dashboard.js
+++ src/oscar/static_src/oscar/js/oscar/dashboard.js
@@ -112,7 +112,7 @@ var oscar = (function(o, $) {
             // Adds error icon if there are errors in the product update form
             $('[data-behaviour="affix-nav-errors"] .tab-pane').each(function(){
                 var productErrorListener = $(this).find('[class*="error"]:not(:empty)').closest('.tab-pane').attr('id');
-                $('[data-spy="affix"] a[href="#' + productErrorListener + '"]').append('<i class="fa fa-info-circle float-right" aria-hidden="true"></i>');
+                $('[data-spy="affix"] a[href="#' + productErrorListener + '"]').append('<i class="fas fa-info-circle float-right"></i>');
             });
 
             o.dashboard.filereader.init();
