0f2c6bf2c822a09ef3760d09e1b5e765b3f28234
Lars van de Kerkhof <lars@permanentmarkers.nl>
2019-05-24 05:02:19 +0200

    Retrieving product options now performs only 1 query. (#3037)
    * Retrieving product options now performs only 1 query.
    
    Also it returns all the product options combined. This makes more sense because
    as the existing tests prove, there is already a way to retrieve the product_class
    options and the product options separately.

--- tests/integration/catalogue/test_options.py
+++ tests/integration/catalogue/test_options.py
@@ -34,3 +34,36 @@ class ProductOptionTests(TestCase):
         product = qs.first()
         self.assertTrue(product.has_options)
         self.assertEquals(product.num_product_options, 1)
+
+    def test_queryset_both(self):
+        "The options attribute on a product should return a queryset containing "
+        "both the product class options and any extra options defined on the"
+        "product"
+        # set up the options on product and product_class
+        self.test_product_has_options_per_product_class()
+        self.test_product_has_options_per_product()
+        self.assertTrue(self.product.has_options, "Options should be present")
+        self.assertEquals(
+            self.product.options.count(), 1,
+            "options attribute should not contain duplicates"
+        )
+        qs = Product.objects.browsable().base_queryset().filter(id=self.product.id)
+        product = qs.first()
+        self.assertEquals(
+            product.num_product_class_options, 1,
+            "num_product_class_options should indicate the product_class option"
+        )
+        self.assertEquals(
+            product.num_product_options, 1,
+            "num_product_options should indicate the number of product options"
+        )
+        self.product_class.options.add(factories.OptionFactory(code="henk"))
+        self.assertEquals(
+            self.product.options.count(), 2,
+            "New product_class options should be immediately visible"
+        )
+        self.product.product_options.add(factories.OptionFactory(code="klaas"))
+        self.assertEquals(
+            self.product.options.count(), 3,
+            "New product options should be immediately visible"
+        )
