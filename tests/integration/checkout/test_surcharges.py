3dde3df6e9379047a061cf579eac7b8b822c128d
Viggo de Vries <viggo@uwkm.nl>
2020-08-18 15:52:24 +0200

    Add shipping charge to the percentage surcharge

--- tests/integration/checkout/test_surcharges.py
+++ tests/integration/checkout/test_surcharges.py
@@ -2,11 +2,14 @@ from decimal import Decimal as D
 
 from django.test import TestCase
 
+from oscar.core import prices
 from oscar.core.loading import get_class
 from oscar.test import factories
 from oscar.test.basket import add_product
 
 SurchargeApplicator = get_class("checkout.applicator", "SurchargeApplicator")
+PercentageCharge = get_class("checkout.surcharges", "PercentageCharge")
+FlatCharge = get_class("checkout.surcharges", "FlatCharge")
 
 
 class TestSurcharges(TestCase):
@@ -20,3 +23,39 @@ class TestSurcharges(TestCase):
 
         self.assertEqual(surcharges.total.excl_tax, D('20.0'))
         self.assertEqual(surcharges.total.incl_tax, D('22.0'))
+
+    def test_percentage_surcharge(self):
+        percentage_surcharge = PercentageCharge(percentage=D(10))
+        add_product(self.basket, D(12))
+        price = percentage_surcharge.calculate(self.basket)
+
+        self.assertEqual(self.basket.total_incl_tax, D(12))
+        self.assertEqual(price.incl_tax, D("1.20"))
+
+    def test_percentage_empty_basket(self):
+        percentage_surcharge = PercentageCharge(percentage=D(10))
+        price = percentage_surcharge.calculate(self.basket)
+
+        self.assertEqual(self.basket.total_incl_tax, D(0))
+        self.assertEqual(price.incl_tax, D(0))
+
+    def test_flat_surcharge(self):
+        flat_surcharge = FlatCharge(excl_tax=D(1), incl_tax=D("1.21"))
+        add_product(self.basket, D(12))
+        price = flat_surcharge.calculate(self.basket)
+
+        self.assertEqual(self.basket.total_incl_tax, D(12))
+        self.assertEqual(price.incl_tax, D("1.21"))
+        self.assertEqual(price.excl_tax, D(1))
+
+    def test_percentage_with_shipping_charge(self):
+        percentage_surcharge = PercentageCharge(percentage=D(4))
+        add_product(self.basket, D(10))
+        shipping_charge = prices.Price(
+            currency=self.basket.currency,
+            excl_tax=D('3.95'), tax=D('1.05'))
+        price = percentage_surcharge.calculate(self.basket, shipping_charge=shipping_charge)
+
+        self.assertEqual(self.basket.total_incl_tax, D(10))
+        self.assertEqual(shipping_charge.incl_tax, D(5))
+        self.assertEqual(price.incl_tax, D("0.6"))
