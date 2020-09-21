08f4228f6f0b67cb31611b50b4dd50988ba13fd5
Alexander Gaevsky <sasha@sasha0.ru>
2020-09-21 17:14:41 +0300

    Implement tests for basket report generator querysets.

--- tests/integration/basket/test_report.py
+++ tests/integration/basket/test_report.py
@@ -4,9 +4,16 @@ from django.test import TestCase
 
 from oscar.apps.basket.reports import (
     OpenBasketReportGenerator, SubmittedBasketReportGenerator)
+from oscar.core.loading import get_model
+from oscar.test.factories import BasketFactory
+
+Basket = get_model('basket', 'Basket')
 
 
 class TestBasketReports(TestCase):
+    def setUp(self) -> None:
+        BasketFactory.create_batch(5, status=Basket.OPEN)
+        BasketFactory.create_batch(6, status=Basket.SUBMITTED)
 
     def test_open_report_doesnt_error(self):
         data = {
@@ -17,6 +24,10 @@ class TestBasketReports(TestCase):
         generator = OpenBasketReportGenerator(**data)
         generator.generate()
 
+    def test_open_report_queryset(self):
+        generator = OpenBasketReportGenerator()
+        assert generator.queryset.count() == 5
+
     def test_submitted_report_doesnt_error(self):
         data = {
             'start_date': datetime.date(2012, 5, 1),
@@ -25,3 +36,7 @@ class TestBasketReports(TestCase):
         }
         generator = SubmittedBasketReportGenerator(**data)
         generator.generate()
+
+    def test_submitted_report_queryset(self):
+        generator = SubmittedBasketReportGenerator()
+        assert generator.queryset.count() == 6
