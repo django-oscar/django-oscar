3dbc1907ae60536fdd25146ad809fed8b6828c8d
Alexander Gaevsky <sasha@sasha0.ru>
2020-09-22 11:06:05 +0300

    Implement tests for date range filtering in the basket report generators.

--- tests/integration/basket/test_report.py
+++ tests/integration/basket/test_report.py
@@ -1,6 +1,8 @@
 import datetime
 
 from django.test import TestCase
+from django.utils.timezone import utc
+from freezegun import freeze_time
 
 from oscar.apps.basket.reports import (
     OpenBasketReportGenerator, SubmittedBasketReportGenerator)
@@ -28,6 +30,17 @@ class TestBasketReports(TestCase):
         generator = OpenBasketReportGenerator()
         assert generator.queryset.count() == 5
 
+    @freeze_time('2020-05-02')
+    def test_open_report_filtering_by_date_range(self):
+        BasketFactory.create(status=Basket.OPEN)
+        data = {
+            'start_date': datetime.date(2020, 5, 1),
+            'end_date': datetime.date(2020, 6, 1),
+            'formatter': 'CSV'
+        }
+        generator = OpenBasketReportGenerator(**data)
+        assert generator.queryset.count() == 1
+
     def test_submitted_report_doesnt_error(self):
         data = {
             'start_date': datetime.date(2012, 5, 1),
@@ -40,3 +53,14 @@ class TestBasketReports(TestCase):
     def test_submitted_report_queryset(self):
         generator = SubmittedBasketReportGenerator()
         assert generator.queryset.count() == 6
+
+    def test_submitted_report_filtering_by_date_range(self):
+        date_submitted = datetime.datetime(2020, 7, 3).replace(tzinfo=utc)
+        BasketFactory.create(status=Basket.SUBMITTED, date_submitted=date_submitted)
+        data = {
+            'start_date': datetime.date(2020, 7, 1),
+            'end_date': datetime.date(2020, 8, 1),
+            'formatter': 'CSV'
+        }
+        generator = SubmittedBasketReportGenerator(**data)
+        assert generator.queryset.count() == 1
