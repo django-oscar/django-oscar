7a9d01232e8c022a0c5fde38ab54f0cc47232cd5
Alexander Gaevsky <sasha@sasha0.ru>
2020-10-02 10:23:24 +0300

    Apply queryset filters after calling get_queryset method in report generator.

--- src/oscar/apps/dashboard/reports/reports.py
+++ src/oscar/apps/dashboard/reports/reports.py
@@ -29,6 +29,7 @@ class ReportGenerator(object):
         formatter_name = '%s_formatter' % kwargs.get('formatter', 'HTML')
         self.formatter = self.formatters[formatter_name]()
         self.queryset = self.get_queryset()
+        self.queryset = self.filter_with_date_range(self.queryset)
 
     def report_description(self):
         return _('%(report_filter)s between %(start_date)s and %(end_date)s') \
@@ -46,8 +47,7 @@ class ReportGenerator(object):
                 "Please define a model_class property on your report generator class, "
                 "or override the qet_queryset() method."
             )
-        queryset = self.model_class._default_manager.all()
-        return self.filter_with_date_range(queryset)
+        return self.model_class._default_manager.all()
 
     def generate(self):
         return self.formatter.generate_response(self.queryset)
