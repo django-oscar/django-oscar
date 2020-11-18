8f694f9a872df271f6b6dce89d6db949e5cb2a4f
Orinda Harrison <harrison@regulusweb.com>
2020-11-18 13:16:12 +0300

    Add 'basket' to 'shipping_discount' method

--- src/oscar/apps/offer/abstract_models.py
+++ src/oscar/apps/offer/abstract_models.py
@@ -343,8 +343,8 @@ class AbstractConditionalOffer(models.Model):
             .aggregate(total=models.Sum('frequency'))
         return aggregates['total'] if aggregates['total'] is not None else 0
 
-    def shipping_discount(self, charge):
-        return self.benefit.proxy().shipping_discount(charge)
+    def shipping_discount(self, charge, basket):
+        return self.benefit.proxy().shipping_discount(charge, basket)
 
     def record_usage(self, discount):
         self.num_applications += discount['freq']
@@ -680,7 +680,7 @@ class AbstractBenefit(BaseOfferMixin, models.Model):
         # We sort lines to be cheapest first to ensure consistent applications
         return sorted(line_tuples, key=operator.itemgetter(0))
 
-    def shipping_discount(self, charge):
+    def shipping_discount(self, charge, basket):
         return D('0.00')
 
 
