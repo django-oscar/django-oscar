81fb5736d27e2f59aaf4198df92642c4cb00a522
Orinda Harrison <harrison@regulusweb.com>
2021-02-09 11:00:27 +0300

    Assert duplicate parents are not returned when search query is shared

--- tests/integration/dashboard/test_catalogue_views.py
+++ tests/integration/dashboard/test_catalogue_views.py
@@ -11,14 +11,26 @@ Product = get_model('catalogue', 'Product')
 class ProductListViewTestCase(TestCase):
     def test_searching_child_product_by_title_returns_parent_product(self):
         self.parent_product = create_product(structure=Product.PARENT, title='Parent', upc='PARENT_UPC')
-        create_product(structure=Product.CHILD, title='Child', parent=self.parent_product, upc='CHILD_UPC',
-                       partner_sku='SKU123')
+        create_product(structure=Product.CHILD, title='Child', parent=self.parent_product, upc='CHILD_UPC')
         view = ProductListView(request=RequestFactory().get('/?title=Child'))
+        assert list(view.get_queryset()) == [self.parent_product]
+
+    def test_searching_child_product_by_title_returns_1_parent_product_if_title_is_partially_shared(self):
+        self.parent_product = create_product(structure=Product.PARENT, title='Shared', upc='PARENT_UPC')
+        create_product(structure=Product.CHILD, title='Shared', parent=self.parent_product, upc='CHILD_UPC')
+        create_product(structure=Product.CHILD, title='Shared1', parent=self.parent_product, upc='CHILD_UPC1')
+        view = ProductListView(request=RequestFactory().get('/?title=Shared'))
         assert list(view.get_queryset()) == [self.parent_product]
 
     def test_searching_child_product_by_upc_returns_parent_product(self):
         self.parent_product = create_product(structure=Product.PARENT, title='Parent', upc='PARENT_UPC')
-        create_product(structure=Product.CHILD, title='Child', parent=self.parent_product, upc='CHILD_UPC',
-                       partner_sku='SKU123')
+        create_product(structure=Product.CHILD, title='Child', parent=self.parent_product, upc='CHILD_UPC')
         view = ProductListView(request=RequestFactory().get('/?upc=CHILD_UPC'))
+        assert list(view.get_queryset()) == [self.parent_product]
+
+    def test_searching_child_product_by_upc_returns_1_parent_product_if_upc_is_partially_shared(self):
+        self.parent_product = create_product(structure=Product.PARENT, title='Parent', upc='PARENT_UPC')
+        create_product(structure=Product.CHILD, title='Child', parent=self.parent_product, upc='CHILD_UPC')
+        create_product(structure=Product.CHILD, title='Child1', parent=self.parent_product, upc='CHILD_UPC1')
+        view = ProductListView(request=RequestFactory().get('/?upc=CHILD'))
         assert list(view.get_queryset()) == [self.parent_product]
