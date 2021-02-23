25de6cc48732fc9a62b8f5e74aac644d7c25d03d
Joseph Wayodi <joseph@regulusweb.com>
2021-02-23 15:33:19 +0300

    Add tests for "oscar_fork_statics" management command

--- src/oscar/management/commands/oscar_fork_statics.py
+++ src/oscar/management/commands/oscar_fork_statics.py
@@ -36,10 +36,10 @@ class Command(BaseCommand):
 
         source = os.path.realpath(
             os.path.join(os.path.dirname(__file__), '../../static'))
-        print("Copying Oscar's static files to %s" % (destination,))
+        self.stdout.write("Copying Oscar's static files to %s" % destination)
         shutil.copytree(source, destination)
 
         # Check if this new folder is in STATICFILES_DIRS
         if destination not in settings.STATICFILES_DIRS:
-            print(("You need to add %s to STATICFILES_DIRS in order for your "
-                   "local overrides to be picked up") % destination)
+            self.stdout.write("You need to add %s to STATICFILES_DIRS in order for your local overrides to be picked "
+                              "up" % destination)
