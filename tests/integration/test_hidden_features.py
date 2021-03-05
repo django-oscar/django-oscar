0e240047b04a4679d3d58bd4e46e2f7ed6e29d03
Orinda Harrison <ajuongharry35@gmail.com>
2021-03-05 11:59:22 +0300

    Update tests to disable features/apps by removing their app configurations from INSTALLED_APPS setting

--- tests/integration/test_hidden_features.py
+++ tests/integration/test_hidden_features.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import reverse
 
 from oscar.test.factories import create_product
@@ -11,13 +12,15 @@ class TestHiddenFeatures(WebTestCase):
         super().setUp()
         self.product = create_product()
         self.wishlists_url = reverse('customer:wishlists-list')
+        self.installed_apps_setting = settings.INSTALLED_APPS.copy()
 
     def test_reviews_enabled(self):
         product_detail_page = self.get(self.product.get_absolute_url())
         self.assertContains(product_detail_page, 'Number of reviews')
 
     def test_reviews_disabled(self):
-        with self.settings(OSCAR_HIDDEN_FEATURES=['reviews']):
+        self.installed_apps_setting.remove('oscar.apps.catalogue.reviews.apps.CatalogueReviewsConfig')
+        with self.settings(INSTALLED_APPS=self.installed_apps_setting):
             product_detail_page = self.get(self.product.get_absolute_url())
             self.assertNotContains(product_detail_page, 'Number of reviews')
 
@@ -28,7 +31,8 @@ class TestHiddenFeatures(WebTestCase):
         self.assertContains(product_detail_page, 'Add to wish list')
 
     def test_wishlists_disabled(self):
-        with self.settings(OSCAR_HIDDEN_FEATURES=['wishlists']):
+        self.installed_apps_setting.remove('oscar.apps.wishlists.apps.WishlistsConfig')
+        with self.settings(INSTALLED_APPS=self.installed_apps_setting):
             account_page = self.get(reverse('customer:profile-view'))
 
             self.assertNotContains(account_page, self.wishlists_url)
