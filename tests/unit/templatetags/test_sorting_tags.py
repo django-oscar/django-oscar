ce95a8f742c18ce4ee2c9b2556d969cf654e85d7
Samir Shah <solaris.smoke@gmail.com>
2021-03-06 06:41:17 +0300

    Simplify code for several old template tags.

---
+++ tests/unit/templatetags/test_sorting_tags.py
@@ -0,0 +1,18 @@
+from django.test import RequestFactory, TestCase
+
+from oscar.templatetags.sorting_tags import anchor
+
+
+class SortingTagsTestCase(TestCase):
+
+    def test_anchor_tag_output(self):
+        ctx = {'request': RequestFactory().get('/')}
+        # No title supplied
+        self.assertEqual(anchor(ctx, 'foo'), '<a href="/?sort=foo">Foo</a>')
+        # Title supplied
+        self.assertEqual(anchor(ctx, 'foo', 'Title'), '<a href="/?sort=foo">Title</a>')
+        # Title supplied with unsafe chars
+        self.assertEqual(anchor(ctx, 'foo', 'Ti&tle'), '<a href="/?sort=foo">Ti&amp;tle</a>')
+        # Reverse sort order
+        ctx = {'request': RequestFactory().get('/?sort=foo&dir=asc')}
+        self.assertEqual(anchor(ctx, 'foo'), '<a href="/?sort=foo&dir=desc">Foo <i class="fas fa-chevron-up"></i></a>')
