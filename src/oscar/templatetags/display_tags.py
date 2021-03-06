5256cc18ea67f3b2e9127216457e366c9a9c2a3c
Samir Shah <solaris.smoke@gmail.com>
2021-03-06 06:41:17 +0300

    Simplify code for several old template tags.

--- src/oscar/templatetags/display_tags.py
+++ src/oscar/templatetags/display_tags.py
@@ -5,41 +5,17 @@ from oscar.core.loading import feature_hidden
 register = template.Library()
 
 
-def get_parameters(parser, token):
-    """
-    {% get_parameters except_field %}
-    """
-
-    args = token.split_contents()
-    if len(args) < 2:
-        raise template.TemplateSyntaxError(
-            "get_parameters tag takes at least 1 argument")
-    return GetParametersNode(args[1].strip())
-
-
-class GetParametersNode(template.Node):
+@register.simple_tag(takes_context=True)
+def get_parameters(context, except_field):
     """
     Renders current get parameters except for the specified parameter
     """
-    def __init__(self, field):
-        self.field = field
+    getvars = context['request'].GET.copy()
+    getvars.pop(except_field, None)
+    if len(getvars.keys()) > 0:
+        return "%s&" % getvars.urlencode()
 
-    def render(self, context):
-        request = context['request']
-        getvars = request.GET.copy()
-
-        if self.field in getvars:
-            del getvars[self.field]
-
-        if len(getvars.keys()) > 0:
-            get_params = "%s&" % getvars.urlencode()
-        else:
-            get_params = ''
-
-        return get_params
-
-
-get_parameters = register.tag(get_parameters)
+    return ''
 
 
 @register.tag()
---
+++ tests/unit/templatetags/test_wishlist_tags.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+from oscar.core.loading import get_model
+from oscar.templatetags.wishlist_tags import wishlists_containing_product
+from oscar.test.factories import ProductFactory, UserFactory, WishListFactory
+
+Wishlist = get_model('wishlists', 'Wishlist')
+
+
+class WishlistTagsTestCase(TestCase):
+
+    def test_wishlists_containing_product(self):
+        p1 = ProductFactory()
+        p2 = ProductFactory()
+        user = UserFactory()
+        wishlist1 = WishListFactory(owner=user)
+        WishListFactory(owner=user)
+        wishlist1.add(p1)
+
+        containing_one = wishlists_containing_product(Wishlist.objects.all(), p1)
+        self.assertEqual(len(containing_one), 1)
+        self.assertEqual(containing_one[0], wishlist1)
+        containing_none = wishlists_containing_product(Wishlist.objects.all(), p2)
+        self.assertEqual(len(containing_none), 0)
