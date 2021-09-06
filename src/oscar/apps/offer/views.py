0b7eabeaf040e3577980ea7226dbb04b86e7e835
Sakshi Uppoor <sakshiuppoor@gmail.com>
2021-09-06 23:29:46 +0530

    Add pagination to RangeDetailView

--- src/oscar/apps/offer/views.py
+++ src/oscar/apps/offer/views.py
@@ -55,6 +55,7 @@ class OfferDetailView(ListView):
 class RangeDetailView(ListView):
     template_name = 'oscar/offer/range.html'
     context_object_name = 'products'
+    paginate_by = settings.OSCAR_PRODUCTS_PER_PAGE
 
     def dispatch(self, request, *args, **kwargs):
         self.range = get_object_or_404(
