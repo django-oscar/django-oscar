f4e4dc51a82d86bd94e29c81824fc8a5a6e904e4
akash232 <akashsuthaar232@gmail.com>
2021-09-14 09:14:33 +0530

    remove DOC strig

--- src/oscar/apps/order/mixins.py
+++ src/oscar/apps/order/mixins.py
@@ -7,15 +7,12 @@ class EventHandlerMixin:
     handler_class = EventHandler
 
     def get_handler_class(self):
-        """Return handler class to use"""
         return self.handler_class
 
     def get_handler(self, user=None, **kwargs):
-        """Return instance of handler"""
         handler_kwargs = self.get_handler_kwargs()
         handler_kwargs.update(**kwargs)
         return self.get_handler_class()(user, **handler_kwargs)
 
     def get_handler_kwargs(self):
-        """Return kwargs arguments for instantiating handler"""
         return {}
