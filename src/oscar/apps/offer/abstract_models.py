b6c5a0813f4f8e07d77459d3de9413af278f6748
gwaidacher <117359695+gwaidacher@users.noreply.github.com>
2023-01-25 15:49:51 +0100

    Update abstract_models.py
    including children

--- src/oscar/apps/offer/abstract_models.py
+++ src/oscar/apps/offer/abstract_models.py
@@ -1011,8 +1011,10 @@ class AbstractRange(models.Model):
         else:
             if self.use_short_query():
                 # we have a simple range with just included products
-                included = self.included_products.values_list('id', flat=True)
-                return Product.objects.filter(id__in=included).distinct()
+                return Product.objects.filter(
+                    Q(includes=self)
+                    | Q(parent__includes=self),  # noqa W503
+                ).distinct()
             selected_products = Product.objects.filter(
                 Q(product_class__classes=self)
                 | Q(includes=self)
