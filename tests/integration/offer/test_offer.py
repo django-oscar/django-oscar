ca4915de55a6b77eea53d382cfa2827455f953d1
Gunther Waidacher <117359695+gwaidacher@users.noreply.github.com>
2023-02-24 13:16:26 +0100

    Create test_offer.py

---
+++ tests/integration/offer/test_offer.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+from oscar.test.factories import create_product
+from oscar.test.factories.offer import ConditionalOfferFactory
+
+
+class TestOffer(TestCase):
+
+    def test_non_public_product_not_in_offer(self):
+        offer = ConditionalOfferFactory()
+        product = create_product(is_public=False)
+        offer.condition.range.add_product(product)
+        self.assertFalse(product in offer.products())
