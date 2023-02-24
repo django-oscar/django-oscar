35313f3bbf3538bd985eba77a2e51a8b8ffdb228
Gunther Waidacher <117359695+gwaidacher@users.noreply.github.com>
2023-02-24 14:54:34 +0100

    fix removed empty line

--- tests/integration/offer/test_offer.py
+++ tests/integration/offer/test_offer.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 
 from oscar.apps.offer.views import RangeDetailView
-
 from oscar.test.factories import create_product
 from oscar.test.factories.offer import ConditionalOfferFactory
 
