064cf52613d03ef88871f6d156d96fb9f2dd98f5
Gunther Waidacher <117359695+gwaidacher@users.noreply.github.com>
2023-02-24 12:56:36 +0100

    fix import

--- tests/integration/offer/test_offer.py
+++ tests/integration/offer/test_offer.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from oscar.test.factories import create_product
-from tests.factories.offer import ConditionalOfferFactory
+from test.factories.offer import ConditionalOfferFactory
 
 
 class TestOffer(TestCase):
