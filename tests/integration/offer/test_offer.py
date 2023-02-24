bbd92c0f1187af712d78a73e4335b15d79cede8d
Gunther Waidacher <117359695+gwaidacher@users.noreply.github.com>
2023-02-24 12:41:03 +0100

    fix added self

--- tests/integration/offer/test_offer.py
+++ tests/integration/offer/test_offer.py
@@ -5,7 +5,7 @@ from tests.factories.offer import ConditionalOfferFactory
 
 class TestOffer(TestCase):
 
-    def test_non_public_product_not_in_offer():
+    def test_non_public_product_not_in_offer(self):
         offer = ConditionalOfferFactory()
         product = create_product(is_public=False)
         offer.condition.range.add_product(product)
