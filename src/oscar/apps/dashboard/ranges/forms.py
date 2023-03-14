159100aad97d8114b7c3a9b47f9f6143893d638e
Gunther Waidacher <117359695+gwaidacher@users.noreply.github.com>
2023-03-14 15:37:43 +0100

    Update forms.py
    Form for excluded_products

--- src/oscar/apps/dashboard/ranges/forms.py
+++ src/oscar/apps/dashboard/ranges/forms.py
@@ -86,3 +86,42 @@ class RangeProductForm(forms.Form):
 
     def get_duplicate_skus(self):
         return self.duplicate_skus
+
+
+class RangeExcludedProductForm(RangeProductForm):
+
+    def clean_query(self):
+        raw = self.cleaned_data['query']
+        if not raw:
+            return raw
+
+        # Check that the search matches some products
+        ids = set(UPC_SET_REGEX.findall(raw))
+        products = self.range.excluded_products.all()
+        existing_skus = set(products.values_list(
+            'stockrecords__partner_sku', flat=True))
+        existing_upcs = set(products.values_list('upc', flat=True))
+        existing_ids = existing_skus.union(existing_upcs)
+        new_ids = ids - existing_ids
+
+        if len(new_ids) == 0:
+            raise forms.ValidationError(
+                _("The products with SKUs or UPCs matching %s are already in"
+                  " this range") % (', '.join(ids)))
+
+        self.products = Product._default_manager.filter(
+            Q(stockrecords__partner_sku__in=new_ids)
+            | Q(upc__in=new_ids))
+        if len(self.products) == 0:
+            raise forms.ValidationError(
+                _("No products exist with a SKU or UPC matching %s")
+                % ", ".join(ids))
+
+        found_skus = set(self.products.values_list(
+            'stockrecords__partner_sku', flat=True))
+        found_upcs = set(self.products.values_list('upc', flat=True))
+        found_ids = found_skus.union(found_upcs)
+        self.missing_skus = new_ids - found_ids
+        self.duplicate_skus = existing_ids.intersection(ids)
+
+        return raw
