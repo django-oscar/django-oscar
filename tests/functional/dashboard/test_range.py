3107fd81400e9513562d93784840c79e64b02902
gwaidacher <gw@acat.cc>
2023-03-16 14:04:07 +0100

    fix test_upload_excluded_file_with_skus

--- tests/functional/dashboard/test_range.py
+++ tests/functional/dashboard/test_range.py
@@ -6,8 +6,7 @@ from django.urls import reverse
 from webtest.forms import Upload
 
 from oscar.apps.dashboard.ranges import forms
-from oscar.apps.offer.models import (
-    Range, RangeProductExcludedFileUpload, RangeProductFileUpload)
+from oscar.apps.offer.models import Range, RangeProductFileUpload
 from oscar.test.factories import create_product
 from oscar.test.testcases import WebTestCase
 
@@ -106,12 +105,12 @@ class RangeProductViewTest(WebTestCase):
         excluded_products = self.range.excluded_products.all()
         self.assertEqual(len(excluded_products), 1)
         self.assertTrue(self.product3 in excluded_products)
-        range_product_excluded_file_upload = RangeProductExcludedFileUpload.objects.get()
-        self.assertEqual(range_product_excluded_file_upload.range, self.range)
-        self.assertEqual(range_product_excluded_file_upload.num_new_skus, 1)
-        self.assertEqual(range_product_excluded_file_upload.status,
-                         RangeProductExcludedFileUpload.PROCESSED)
-        self.assertEqual(range_product_excluded_file_upload.size, 3)
+        range_product_file_upload = RangeProductFileUpload.objects.get()
+        self.assertEqual(range_product_file_upload.range, self.range)
+        self.assertEqual(range_product_file_upload.num_new_skus, 1)
+        self.assertEqual(range_product_file_upload.status,
+                         RangeProductFileUpload.PROCESSED)
+        self.assertEqual(range_product_file_upload.size, 3)
 
     def test_dupe_skus_warning(self):
         self.range.add_product(self.product3)
