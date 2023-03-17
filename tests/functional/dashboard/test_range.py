1b3219b4434fe1fa7d6028b8fdc4a5de5b5b764b
gwaidacher <gw@acat.cc>
2023-03-17 11:53:41 +0100

    query form 3

--- tests/functional/dashboard/test_range.py
+++ tests/functional/dashboard/test_range.py
@@ -145,7 +145,7 @@ class RangeProductViewTest(WebTestCase):
     def test_dupe_excluded_skus_warning(self):
         self.range.excluded_products.add(self.product3)
         range_products_page = self.get(self.url2)
-        form = range_products_page.forms[1]
+        form = range_products_page.forms[2]
         form['query'] = '456'
         response = form.submit()
         self.assertEqual(list(response.context['messages']), [])
