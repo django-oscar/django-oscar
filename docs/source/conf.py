de5b1cf2ceb4e7358106992acbfbd4accdd2291e
Viggo de Vries <viggo@highbiza.nl>
2023-09-04 15:34:55 +0200

    Always use the sphinx_rtd_theme (#4165)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -126,14 +126,10 @@ issues_github_path = 'django-oscar/django-oscar'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-# Use RTD theme locally
-html_theme = 'default'
-if not on_rtd:
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
