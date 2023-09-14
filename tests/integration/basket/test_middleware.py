2ea3c50be432c0aeec5c6c4ca4175e9651e39b39
gwaidacher <gw@acat.cc>
2023-09-14 13:40:29 +0200

    add product to cookie basket

--- tests/integration/basket/test_middleware.py
+++ tests/integration/basket/test_middleware.py
@@ -1,15 +1,17 @@
 from decimal import Decimal as D
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.messages import get_messages
+from django.core.signing import Signer
 from django.http import HttpResponse
 from django.test import TestCase
 from django.test.client import RequestFactory
 from oscar.apps.basket import middleware
 from oscar.core.compat import get_user_model
+from oscar.core.loading import get_class
 from oscar.test.basket import add_product
 
 User = get_user_model()
-
+AccountAuthView = get_class("customer.views", "AccountAuthView")
 
 class TestBasketMiddleware(TestCase):
     @staticmethod
@@ -46,13 +48,32 @@ class TestBasketMiddleware(TestCase):
         self.assertIn("oscar_open_basket", request.cookies_to_delete)
 
     def test_merged_basket_message(self):
-        basket = self.middleware.get_basket(self.request)
+        # add product to anonymous user's basket
+        basket = self.request.basket
         add_product(basket, D("12.00"), 1)
+        basket.save()
+        # set cookie_basket
+        basket_hash = Signer().sign(basket.id)
+        request_factory = RequestFactory()
+        request_factory.cookies["oscar_open_basket"] = basket_hash
+        request = request_factory.get("/")
+        request.user = AnonymousUser()
+        request.cookies_to_delete = []
+        cookie_basket = self.middleware.get_cookie_basket(
+            "oscar_open_basket", request, None
+        )
+        self.assertEqual(basket, cookie_basket)
         # create User
         username, email, password = "lucy", "lucy@example.com", "password"
-        self.request.user = User.objects.create_user(username, email, password)
-        basket = self.middleware.get_basket(self.request)
-        messages = get_messages(self.request)
+        request = request_factory.get("/")
+        request.user = User.objects.create_user(username, email, password)
+        # login as registered user
+        data = {"username": "lucy@example.com", "password": password}
+        request = request_factory.post("customer:login", data=data)
+        view = AccountAuthView.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, 302)
+        messages = get_messages(request)
         message = (
             "We have merged 1 items from a previous session to "
             "your basket. Its content has changed."
