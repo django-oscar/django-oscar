d105381a3ba34ce53fff6a2e2be2df3a6c33ded9
gwaidacher <gw@acat.cc>
2023-09-15 11:33:06 +0200

    BasketView.as_view()

--- tests/functional/basket/test_middleware.py
+++ tests/functional/basket/test_middleware.py
@@ -2,7 +2,6 @@ from decimal import Decimal as D
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.http import HttpResponse
-from django.urls import reverse
 from oscar.apps.basket import middleware, views
 from oscar.core.compat import get_user_model
 from oscar.test import factories
@@ -20,13 +19,13 @@ class BasketMiddlewareTest(WebTestCase):
 
     def setUp(self):
         self.middleware = middleware.BasketMiddleware(self.get_response_for_test)
-        self.url = reverse("basket:summary")
         self.request = RequestFactory().get("/")
         self.request.user = AnonymousUser()
         self.middleware(self.request)
         self.user = factories.UserFactory()
 
     def test_merged_basket_message(self):
+        # add product to anonymous user's basket
         basket = self.middleware.get_basket(self.request)
         add_product(basket, D("5.00"), 1)
         cookie_basket = self.middleware.get_cookie_basket(
@@ -34,16 +33,16 @@ class BasketMiddlewareTest(WebTestCase):
         )
         self.assertEqual(basket, cookie_basket)
         self.assertEqual(cookie_basket.lines.count(), 1)
-
+        # get hash from cookie_basket and set cookie in new request
         basket_hash = self.middleware.get_basket_hash(basket.id)
 
         request = RequestFactory().get(self.url, user=self.user)
         request.COOKIES[settings.OSCAR_BASKET_COOKIE_OPEN] = basket_hash
-
-        view = views.BasketView(request=request)
+        # call BasketView and check messages from response.context
+        view = views.BasketView.as_view()
         response = view(request)
         messages = list(response.context["messages"])
-        self.assertEqual(messages.length(), 1)
+        self.assertEqual(len(messages), 1)
         message = (
             "We have merged 1 items from a previous session to "
             "your basket. Its content has changed."
