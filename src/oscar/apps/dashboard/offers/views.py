f8b8d33cd374193de49f97a741c66b5b0f3b2461
Vallabh <tiwarivallabh27@gmail.com>
2023-10-09 13:13:19 +0530

    Fixes #3844 suspend offer doesn't display with is_active 'NO'

--- src/oscar/apps/dashboard/offers/views.py
+++ src/oscar/apps/dashboard/offers/views.py
@@ -95,7 +95,11 @@ class OfferListView(ListView):
                 )
                 self.search_filters.append(_("Is active"))
             else:
-                qs = qs.filter(Q(end_datetime__lt=now) | Q(start_datetime__gt=now))
+                qs = qs.filter(
+                    (Q(start_datetime__lte=now) | Q(start_datetime__isnull=True))
+                    & (Q(end_datetime__gte=now) | Q(end_datetime__isnull=True)),
+                    status=ConditionalOffer.SUSPENDED,
+                )
                 self.search_filters.append(_("Is inactive"))
         if offer_type:
             qs = qs.filter(offer_type=offer_type)
