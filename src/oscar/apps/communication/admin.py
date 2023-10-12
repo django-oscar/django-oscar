8005263b8e0ad0c1bea784d41c4a2dabc67e0710
Islam Mesha <eng.islammesha@gmail.com>
2023-10-12 03:09:24 +0300

    Register the Notification model to Django admin

--- src/oscar/apps/communication/admin.py
+++ src/oscar/apps/communication/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 from oscar.core.loading import get_model
 
-CommunicationEventType = get_model("communication", "CommunicationEventType")
 Email = get_model("communication", "Email")
-
+Notification = get_model("communication", "Notification")
+CommunicationEventType = get_model("communication", "CommunicationEventType")
 
 admin.site.register(Email)
+admin.site.register(Notification)
 admin.site.register(CommunicationEventType)
