7a13d0869b68a701938c5bbca0e79877249ba75e
Christian Clauss <cclauss@me.com>
2024-01-22 12:12:44 +0100

    Should STORAGES be defined in a .env file?

--- src/oscar/templatetags/history_tags.py
+++ src/oscar/templatetags/history_tags.py
@@ -37,7 +37,7 @@ def get_back_button(context):
     if not request:
         raise Exception("Cannot get request from context")
 
-    referrer = request.headers.get("referer", None)
+    referrer = request.META.get("HTTP_REFERER", None)
     if not referrer:
         return None
 
