788f051ea9aaac55d5ed9bf4dc251a536e8fd2bb
Viggo de Vries <viggo@highbiza.nl>
2024-01-26 09:38:35 +0100

    3.2.3rc3 release + set final release date

--- src/oscar/__init__.py
+++ src/oscar/__init__.py
@@ -1,5 +1,5 @@
 # Use 'alpha', 'beta', 'rc' or 'final' as the 4th element to indicate release type.
-VERSION = (3, 2, 3, "alpha", 1)
+VERSION = (3, 2, 3, "rc", 3)
 
 
 def get_short_version():
@@ -13,7 +13,7 @@ def get_version():
         version = "%s.%s" % (version, VERSION[2])
 
     if VERSION[3] != "final":
-        mapping = {"alpha": "a", "beta": "b", "rc": "c"}
+        mapping = {"alpha": "a", "beta": "b", "rc": "rc"}
         version = "%s%s" % (version, mapping[VERSION[3]])
         if len(VERSION) == 5:
             version = "%s%s" % (version, VERSION[4])
