9c6cc48e166f5bc68447242063a9577ee5ba2df0
maziar-dandc <53433222+maziar-dandc@users.noreply.github.com>
2024-06-25 11:18:16 +0400

    Missed django 4.2 upgrade DeleteView override fix (#4316)
    * missed django 4.2 upgrade DeleteView override fix
    
    * maintain older django versions compatibility - thanks to @samar-hassan
    following the same compat method as https://github.com/django-oscar/django-oscar/pull/4098
    
    ---------
    
    Co-authored-by: MaziyarMK <maziyarmalakouti@gmail.com>

--- src/oscar/apps/dashboard/catalogue/views.py
+++ src/oscar/apps/dashboard/catalogue/views.py
@@ -493,6 +493,9 @@ class ProductDeleteView(PartnerProductFilterMixin, generic.DeleteView):
             ctx["title"] = _("Delete product?")
         return ctx
 
+    def post(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
+
     def delete(self, request, *args, **kwargs):
         # We override the core delete method and don't call super in order to
         # apply more sophisticated logic around handling child products.
