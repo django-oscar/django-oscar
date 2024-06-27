b4a9528b85b8b75ffe5550a3dbd0e79a987451a8
Julie Rymer <chadys@hotmail.fr>
2024-06-27 09:47:23 +0200

    fix: remove communication unused config files (#4282)
    Co-authored-by: Julie Rymer <jrymer@biru.sh>

--- src/oscar/apps/communication/config.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class CommunicationConfig(AppConfig):
-    label = "communication"
-    name = "oscar.apps.communication"
-    verbose_name = _("Communication")
