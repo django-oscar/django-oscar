dab7e051c7af2a707a984d038b9ace26d3ae70f7
Gijs Bal <gijsb@highbiza.nl>
2024-11-28 11:26:22 +0100

    put line and line_created in attributes

--- src/oscar/apps/basket/views.py
+++ src/oscar/apps/basket/views.py
@@ -359,7 +359,7 @@ class BasketAddView(FormView):
     def form_valid(self, form):
         offers_before = self.request.basket.applied_offers()
 
-        self.request.basket.add_product(
+        self.line, self.line_created = self.request.basket.add_product(
             form.product, form.cleaned_data["quantity"], form.cleaned_options()
         )
 
