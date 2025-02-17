343ee075d784d672273077ff5f10723b145a5e3c
Viggo de Vries <viggo@highbiza.nl>
2025-02-17 15:36:34 +0100

    update test migrations

--- tests/_site/apps/catalogue/migrations/0030_auto_20250217_1247.py
+++ tests/_site/apps/catalogue/migrations/0030_auto_20250217_1247.py
@@ -4,10 +4,7 @@ from django.db import migrations, connection
 
 def create_materialized_view(apps, schema_editor):
     """Creates a materialized view for PostgreSQL."""
-    print("Ja ik ga dinges aanmaken jeweeet", connection.vendor)
     if connection.vendor == "postgresql":
-        print("JAAA IK MAAK M AAN !!!@" * 30)
-        
         with connection.cursor() as cursor:
             cursor.execute("""
                 CREATE MATERIALIZED VIEW IF NOT EXISTS catalogue_product_category_hierarchy AS
@@ -24,6 +21,11 @@ def create_materialized_view(apps, schema_editor):
                     ON child_categories.path LIKE parent_categories.path || '%'
                 JOIN catalogue_product p ON p.id = pc.product_id;
                 
+            """)
+            
+            cursor.execute("""
+                CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS catalogue_product_category_hierarchy_idx 
+                ON catalogue_product_category_hierarchy (product_id, category_id);
             """)
 
 
@@ -42,6 +44,7 @@ def drop_materialized_view(apps, schema_editor):
     if connection.vendor == "postgresql":
         with connection.cursor() as cursor:
             cursor.execute("DROP MATERIALIZED VIEW IF EXISTS catalogue_product_category_hierarchy;")
+            cursor.execute("DROP INDEX IF EXISTS catalogue_product_category_hierarchy_idx;")
 
 
 class Migration(migrations.Migration):
@@ -53,9 +56,4 @@ class Migration(migrations.Migration):
 
     operations = [
          migrations.RunPython(create_materialized_view, drop_materialized_view),
-         migrations.RunSQL(
-             sql="CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS catalogue_product_category_hierarchy_idx "
-                 "ON catalogue_product_category_hierarchy (product_id, category_id);",
-             reverse_sql="DROP INDEX IF EXISTS catalogue_product_category_hierarchy_idx;",
-         ),
      ]
