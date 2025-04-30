89ac1c155a9f5e382f6ac8364fe044d77cef8415
Joey Jurjens <joeyjurjens@gmail.com>
2025-04-30 22:17:28 +0200

    Replace join query with CTE for the range category lookup.

--- src/oscar/apps/catalogue/expressions.py
+++ src/oscar/apps/catalogue/expressions.py
@@ -1,36 +1,59 @@
 from django.db.models.expressions import Subquery
 
-EXPAND_UPWARDS_CATEGORY_QUERY = """
-(SELECT "CATALOGUE_CATEGORY_JOIN"."id" FROM "catalogue_category" AS "CATALOGUE_CATEGORY_BASE"
-LEFT JOIN "catalogue_category" AS "CATALOGUE_CATEGORY_JOIN" ON (
-    "CATALOGUE_CATEGORY_BASE"."path" LIKE "CATALOGUE_CATEGORY_JOIN"."path" || '%%%%'
-    AND "CATALOGUE_CATEGORY_BASE"."depth" >= "CATALOGUE_CATEGORY_JOIN"."depth"
-)
-WHERE "CATALOGUE_CATEGORY_BASE"."id" IN (%(subquery)s))
-"""
-
-
-EXPAND_DOWNWARDS_CATEGORY_QUERY = """
-(SELECT "CATALOGUE_CATEGORY_JOIN"."id" FROM "catalogue_category" AS "CATALOGUE_CATEGORY_BASE"
-LEFT JOIN "catalogue_category" AS "CATALOGUE_CATEGORY_JOIN" ON (
-    "CATALOGUE_CATEGORY_JOIN"."path" LIKE "CATALOGUE_CATEGORY_BASE"."path" || '%%%%'
-    AND "CATALOGUE_CATEGORY_BASE"."depth" <= "CATALOGUE_CATEGORY_JOIN"."depth"
-)
-WHERE "CATALOGUE_CATEGORY_BASE"."id" IN (%(subquery)s))
-"""
-
 
 # pylint: disable=abstract-method
 class ExpandUpwardsCategoryQueryset(Subquery):
-    template = EXPAND_UPWARDS_CATEGORY_QUERY
+    def __init__(self, queryset, **kwargs):
+        self.queryset = queryset
+        super().__init__(queryset, **kwargs)
+
+    def as_sql(self, compiler, connection, template=None, **extra_context):
+        inner_sql, inner_params = compiler.compile(self.queryset.query)
+
+        sql = f"""
+        (WITH RECURSIVE category_hierarchy AS (
+            SELECT "id", "path", "depth" FROM "catalogue_category" WHERE "id" IN ({inner_sql})
+            UNION ALL
+            SELECT "parent"."id", "parent"."path", "parent"."depth"
+            FROM "catalogue_category" AS "parent"
+            JOIN category_hierarchy AS "child" ON (
+                "child"."path" LIKE "parent"."path" || '%%'
+                AND "parent"."depth" < "child"."depth"
+            )
+        )
+        SELECT DISTINCT "id" FROM category_hierarchy)
+        """
+        return sql, inner_params
 
     def as_sqlite(self, compiler, connection):
-        return super().as_sql(compiler, connection, self.template[1:-1])
+        sql, params = self.as_sql(compiler, connection)
+        return sql[1:-1], params
 
 
 # pylint: disable=abstract-method
 class ExpandDownwardsCategoryQueryset(Subquery):
-    template = EXPAND_DOWNWARDS_CATEGORY_QUERY
+    def __init__(self, queryset, **kwargs):
+        self.queryset = queryset
+        super().__init__(queryset, **kwargs)
+
+    def as_sql(self, compiler, connection, template=None, **extra_context):
+        inner_sql, inner_params = compiler.compile(self.queryset.query)
+
+        sql = f"""
+        (WITH RECURSIVE category_hierarchy AS (
+            SELECT "id", "path", "depth" FROM "catalogue_category" WHERE "id" IN ({inner_sql})
+            UNION ALL
+            SELECT "child"."id", "child"."path", "child"."depth"
+            FROM "catalogue_category" AS "child"
+            JOIN category_hierarchy AS "parent" ON (
+                "child"."path" LIKE "parent"."path" || '%%'
+                AND "child"."depth" > "parent"."depth"
+            )
+        )
+        SELECT DISTINCT "id" FROM category_hierarchy)
+        """
+        return sql, inner_params
 
     def as_sqlite(self, compiler, connection):
-        return super().as_sql(compiler, connection, self.template[1:-1])
+        sql, params = self.as_sql(compiler, connection)
+        return sql[1:-1], params
